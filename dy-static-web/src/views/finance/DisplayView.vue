bf444061e86e8f9398b3b0e993331f8176990167
Mika <mika@eagrp.net>
2023-06-21 13:53:46 +0800

    upd style

--- dy-static-web/src/views/finance/DisplayView.vue
+++ dy-static-web/src/views/finance/DisplayView.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="submitMessage.length > 0" class="inner-cont">
     <div  class="submit-message">
-    <div class="line"><span>银行名称：</span> <span class="info" ref="subMsg0">{{submitMessage[0]}}</span><button @blur="blurCode" @click="copyMessage('0')" class="common-btn">{{ copybtntxt0 }}</button></div>
-    <div class="line"><span>银行账号：</span> <span class="info" ref="subMsg1">{{submitMessage[1]}}</span><button @blur="blurCode" @click="copyMessage('1')" class="common-btn">{{ copybtntxt1 }}</button></div>
-    <div class="line"><span>银行卡号：</span> <span class="info" ref="subMsg2">{{submitMessage[2]}}</span><button @blur="blurCode" @click="copyMessage('2')" class="common-btn">{{ copybtntxt2 }}</button></div>
-    <div class="line"><span>存款金额：</span> <span class="info" ref="subMsg3">{{submitMessage[3]}}</span><button @blur="blurCode" @click="copyMessage('3')" class="common-btn">{{ copybtntxt3 }}</button></div>
+    <div class="linebox"><span>银行名称：</span> <span class="info" ref="subMsg0">{{submitMessage[0]}}</span><button @blur="blurCode" @click="copyMessage('0')" class="common-btn">{{ copybtntxt0 }}</button></div>
+    <div class="linebox"><span>银行账号：</span> <span class="info" ref="subMsg1">{{submitMessage[1]}}</span><button @blur="blurCode" @click="copyMessage('1')" class="common-btn">{{ copybtntxt1 }}</button></div>
+    <div class="linebox"><span>银行卡号：</span> <span class="info" ref="subMsg2">{{submitMessage[2]}}</span><button @blur="blurCode" @click="copyMessage('2')" class="common-btn">{{ copybtntxt2 }}</button></div>
+    <div class="linebox"><span>存款金额：</span> <span class="info" ref="subMsg3">{{submitMessage[3]}}</span><button @blur="blurCode" @click="copyMessage('3')" class="common-btn">{{ copybtntxt3 }}</button></div>
   </div>
   </div>
   <div v-else id="renderArea">
@@ -153,7 +153,7 @@ onMounted(async () => {
   align-items: flex-start;
   flex-direction: column;
   color:#000000;
-  .line {
+  .linebox {
     display: flex;
     gap: 10px;
     justify-content: space-between;
