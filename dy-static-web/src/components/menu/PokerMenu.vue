a974ac261c9937dd4727c15e18bc2ee5fc8e58ca
Keng Hong <kenghongho@eagrp.net>
2023-06-23 19:28:03 +0800

    update fishing AG gameCode. fix poker menu

--- dy-static-web/src/components/menu/PokerMenu.vue
+++ dy-static-web/src/components/menu/PokerMenu.vue
@@ -31,8 +31,8 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     data: () => ({
         navigations: [
-        { code: "DT", image: "tx", label: "大唐", slogan: "赛事最多 玩法新颖" , gameCode: '' },
-        { code: "KYDY", image: "ky", label: "开元", slogan: "崭新玩法 感受精彩", gameCode: 'ky_lobby' },
+        { code: "DT", image: "tx", label: "大唐", slogan: "赛事最多 玩法新颖" , gameCode: '', percentage: "0"},
+        { code: "KYDY", image: "ky", label: "开元", slogan: "崭新玩法 感受精彩", gameCode: 'ky_lobby', percentage: "-232px"},
         ],
     }),
 })
