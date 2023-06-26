3eb9fcac64f79f88e4ed77e7dc98d0c38f7f5dc6
jack <jack@eagrp.net>
2023-06-27 00:12:04 +0800

    打开转账

--- dy-static-h5/src/pages/account/RecordsView.vue
+++ dy-static-h5/src/pages/account/RecordsView.vue
@@ -27,11 +27,11 @@ const transitList = ref([
     icon: 'withdraw',
     name: '提款记录'
   },
-  // {
-  //   code: 'transfer',
-  //   icon: 'transfer',
-  //   name: '转账记录'
-  // },
+  {
+    code: 'transfer',
+    icon: 'transfer',
+    name: '转账记录'
+  },
   {
     code: 'promo',
     icon: 'deposit',
