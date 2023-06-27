8fab2c8bb66da12df951c65178ea6812569f4c64
FE/gibsern <gibsern@eagrp.net>
2023-06-27 15:37:39 +0800

    feat: added support for env for h2 envirnoment variable

--- .shared/api/endpoint/bonus.js
+++ .shared/api/endpoint/bonus.js
@@ -1,4 +1,5 @@
-const ns = `${process.env.VUE_APP_RST_API}bonus`
+const domain = process.env.VUE_APP_RST_API || process.env.RST_API
+const ns = `${domain}bonus`
 
 export default {
     claim: `${ns}/claim`,
