e9217f755e2cadc97c2ae8c9ee2fcafcb76b4ea2
Keng Hong <kenghongho@eagrp.net>
2023-07-01 09:32:31 +0800

    add cnzz

--- dy-static-web/src/views/layouts/MainLayout.vue
+++ dy-static-web/src/views/layouts/MainLayout.vue
@@ -5,7 +5,7 @@
     </keep-alive>
     <router-view class="router-view" v-slot="{ Component }">
       <!-- <transition name="component-fade" mode="out-in"> -->
-        <component :is="Component" />
+      <component :is="Component" />
       <!-- </transition> -->
     </router-view>
     <common-footer />
@@ -25,6 +25,59 @@ export default defineComponent({
     CommonFooter,
     CommonSidebar
   },
+  mounted() {
+    this.loadTrackingScript();
+  },
+  methods: {
+    loadTrackingScript() {
+      const currentDomain = window.location.hostname;
+      const currentPort = window.location.port;
+      const currentDomainWithPort = `${currentDomain}:${currentPort}`;
+      console.log("Domain:", currentDomainWithPort);
+
+      // Determine the tracking script URL based on the current domain
+      let trackingScriptUrl = "";
+      switch (currentDomainWithPort) {
+        case "www.dy86353.com":
+        case "www.dy80252.com":
+          trackingScriptUrl =
+            "https://s9.cnzz.com/z_stat.php?id=1281279658&web_id=1281279658";
+          break;
+        case "www.dy93113.com":
+        case "www.dy96108.com":
+          trackingScriptUrl =
+            "https://s9.cnzz.com/z_stat.php?id=1280864521&web_id=1280864521";
+          break;
+        case "www.dy18178.com:8765":
+        case "www.dy50122.com:8765":
+        case "www.dy52506.com:8765":
+        case "www.dy53976.com:8765":
+        case "www.dy70679.com:8765":
+        case "www.dy73953.com:8765":
+          trackingScriptUrl = "https://s4.cnzz.com/z_stat.php?id=1281277587&web_id=1281277587";
+          break;
+        case "www.dy52373.com:8765":
+        case "www.dy67892.com:8765":
+        case "www.dy93828.com:8765":
+        case "www.dy37378.com:8765":
+        case "www.dy35567.com:8765":
+        case "www.dy25952.com:8765":
+        case "www.dy87265.com:8765":
+        case "www.dy29892.com:8765":
+        case "www.dy38885.com:8765":
+        case "www.dy78299.com:8765":
+          trackingScriptUrl = "https://s4.cnzz.com/z_stat.php?id=1281277587&web_id=1281277587";
+          break;
+        default:
+          return; // Don't load the tracking script for other domains
+      }
+
+      const script = document.createElement("script");
+      script.src = trackingScriptUrl;
+      script.type = "text/javascript";
+      document.body.appendChild(script);
+    }
+  },
   setup() {
     return {};
   },
@@ -33,6 +86,6 @@ export default defineComponent({
 
 <style scoped>
 .router-view {
-    min-height: calc(100vh - 310px);
+  min-height: calc(100vh - 310px);
 }
 </style>
