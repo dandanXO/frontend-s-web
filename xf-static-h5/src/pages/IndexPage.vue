1a490287a3bc05eef423c6002ebf8e4dea686e18
WeiSeng <weiseng@eagrp.net>
2023-07-05 14:56:43 +0800

    reset android files

--- xf-static-h5/src/pages/IndexPage.vue
+++ xf-static-h5/src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Jolly88" src="~assets/logo.png" style="width: 200px" />
+    <img alt="兴发" src="~assets/logo.png" style="width: 200px" />
   </q-page>
 </template>
 
