d5925951215833eccfbb7762e61bd0a919debbd3
Wong Ws <newplayer992@gmail.com>
2023-07-06 00:01:06 +0800

    check ios 2

--- xf-static-h5/src/pages/AccountPage.vue
+++ xf-static-h5/src/pages/AccountPage.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="pro-details">
         <span class="nickname-span">{{ store.nickName }}</span>
-        <span v-if="store.nickName=='npr100'">&nbsp;1.0.1</span>
+        <span v-if="store.nickName=='npr100'">&nbsp;1.0.2</span>
         <q-badge class="vip-badge-item" color="orange" text-color="black" :label="store.vip"/>
 
         <!--        <span v-if="vipLevel === 1"-->
