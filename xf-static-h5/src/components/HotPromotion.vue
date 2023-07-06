22a1771ac17451568bd93a2a697850043262b3ca
darren <darren@eagrp.net>
2023-07-06 16:06:22 +0800

    update welcome task

--- xf-static-h5/src/components/HotPromotion.vue
+++ xf-static-h5/src/components/HotPromotion.vue
@@ -9,7 +9,11 @@
     <TigerCardPromo v-if="!isCommonPromo && list.redirectUrl === 'dy1-tiger-card'" />
     <GoldenEggPromo v-if="!isCommonPromo && list.redirectUrl === 'goldenegg'" />
     <HongBaoYuPromo v-if="!isCommonPromo && list.redirectUrl === 'hongbaoyu'" />
-
+    <WelcomeTaskPromo
+        v-if="
+        !isCommonPromo && list.redirectUrl === 'welcomenewuser' && store.token
+      "
+    />
     <InviteFriendPromo
         v-if="list.redirectUrl === 'invitefriend' && !isCommonPromo"
     />
@@ -217,6 +221,7 @@ import ClaimPromo from "../components/hotpromo/claimPromo.vue";
 import TigerCardPromo from "../components/hotpromo/tigercard/tigerCardPromo.vue";
 import GoldenEggPromo from "../components/hotpromo/goldenegg/goldenEggPromo.vue";
 import HongBaoYuPromo from "../components/hotpromo/hongbaoyu/HongBaoYu.vue";
+import WelcomeTaskPromo from "../components/hotpromo/welcometask/welcomeTaskPromo.vue";
 import InviteFriendPromo from "../components/hotpromo/invitefriend/inviteFriendPromo.vue";
 
 export default defineComponent({
@@ -228,6 +233,7 @@ export default defineComponent({
     TigerCardPromo,
     GoldenEggPromo,
     HongBaoYuPromo,
+    WelcomeTaskPromo,
     InviteFriendPromo
   },
   props: {
