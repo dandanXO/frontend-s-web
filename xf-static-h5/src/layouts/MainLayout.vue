0f1c7344323408e17bfa930333c03bc68cc6653e
WeiSeng <weiseng@eagrp.net>
2023-07-07 14:37:08 +0800

    XF Dy new LiveChat

--- xf-static-h5/src/layouts/MainLayout.vue
+++ xf-static-h5/src/layouts/MainLayout.vue
@@ -106,7 +106,7 @@
           <img class="hover" src="../assets/images/index/menu/promo-icon-hover.png">
           优惠
         </q-route-tab>
-        <q-route-tab to="/liveChat" name="live">
+        <q-route-tab to="/liveChat" id="cs-web-id" name="live">
           <img class="inactive" src="../assets/images/index/menu/livechat-icon.png">
           <img class="hover" src="../assets/images/index/menu/livechat-icon-hover.png">
           客服
