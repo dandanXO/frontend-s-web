e141126b7ee8d4d32faf4a32bf807d151c672a45
leo <leo.nam@outlook.com>
2023-07-07 16:27:04 +0800

    member platform

---
+++ tf-manager-web/src/api/member-platform.js
@@ -0,0 +1,18 @@
+import { ContentType, Method } from "axios-mapper";
+import https from "../utils/https";
+
+export const getMemberPlatformList = (query) => {
+  return https().request("/member-platform", Method.GET, query, ContentType.form);
+};
+
+export const getPlatformList = (query) => {
+  return https().request("/member-platform/platform-list", Method.GET, query, ContentType.form);
+};
+
+export const manualRegister = (row) => {
+  return https().request("/member-platform/manual", Method.GET, row, ContentType.form);
+};
+
+export const deleteMemberPlatform = (ids) => {
+  return https().request(`/member-platform?_method=DELETE`, Method.POST, { ids: ids.join(",") }, ContentType.form);
+};
