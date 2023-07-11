de38ea2bbdd42aea885be33d6a03c1531e278bf2
Mika <mika@eagrp.net>
2023-07-11 13:33:17 +0800

    hide agent for now

--- xf-static-web/src/views/AboutView.vue
+++ xf-static-web/src/views/AboutView.vue
@@ -224,9 +224,9 @@ export default defineComponent({
 <style lang="scss">
 .about-container {
   // min-height: 1062px;
-  min-height: calc(100vh - 330px);
+    min-height: calc(100vh - 330px);
     height: 570px;
-  background: url(../assets/about/abtbg.jpg)no-repeat left top;
+    background: url(../assets/about/abtbg.jpg)no-repeat left top;
     background-position: -80px 0px;
     overflow: auto;
     scrollbar-width: none;
