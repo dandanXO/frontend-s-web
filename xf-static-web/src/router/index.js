235c9de2f3fb41133181c8853ecc2e36f03aa907
Keng Hong <kenghongho@eagrp.net>
2023-07-17 13:41:10 +0800

    create maintenance pages for xf/dy

--- xf-static-web/src/router/index.js
+++ xf-static-web/src/router/index.js
@@ -74,6 +74,11 @@ const routes = [
                 component: () => import(/* webpackChunkName: "Game" */ '../views/VIPView.vue')
             },
             {
+                path: "/maintenance",
+                name: "maintenance",
+                component: () => import(/* webpackChunkName: "Maintenance" */ '../views/MaintenanceView.vue')
+            },
+            {
                 path: "/agent/:affiliateCode",
                 name: "agentCode",
                 component: () => {},
