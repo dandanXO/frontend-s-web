949cd749fde801f6b6412641069b2268fea4bc0e
Keng Hong <kenghongho@eagrp.net>
2023-07-18 15:16:55 +0800

    fix: mobile login location reload

--- xf-static-h5/src/stores/index.js
+++ xf-static-h5/src/stores/index.js
@@ -105,6 +105,7 @@ export const userStore = defineStore("userStore", {
             return api.post("/member/mobileLogin", string).then((ret) => {
                 if (ret.code === 0) {
                     SessionStorage.set("TOKEN", ret.data);
+                    location.reload();
                 } else {
                     Notify.create({
                         color: "negative",
