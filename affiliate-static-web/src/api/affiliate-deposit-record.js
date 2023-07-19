036590788a2bb5806498ad977ef19c4ad4555800
darren <darren@eagrp.net>
2023-07-19 15:24:38 +0800

    add affiliate deposit

--- affiliate-static-web/src/api/affiliate-deposit-record.js
+++ affiliate-static-web/src/api/affiliate-deposit-record.js
@@ -1,6 +1,10 @@
 import { ContentType, Method } from "axios-mapper";
 import https from "../utils/https";
 
-export const getMemberDepositRecords = (id, query) => {
+export const getDepositRecords = (id, query) => {
   return https().request(`/affiliateDepositRecord/${id}`, Method.GET, query, ContentType.form);
+};
+
+export const getMemberDepositRecords = (id, query) => {
+  return https().request(`/affiliateDepositRecord/${id}/member`, Method.GET, query, ContentType.form);
 };
