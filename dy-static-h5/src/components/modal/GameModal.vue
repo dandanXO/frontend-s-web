a3778143ae85d05e5d26d397c69a4a744943a92a
Mika <mika@eagrp.net>
2023-07-20 15:21:35 +0800

    remove query on slots

--- dy-static-h5/src/components/modal/GameModal.vue
+++ dy-static-h5/src/components/modal/GameModal.vue
@@ -259,15 +259,16 @@ const closeDialog = () => {
   visible.value = false;
   src.value = ""
   AppFullscreen.exit()
-  router.replace({query: {}});
   window.screen.orientation.lock('portrait');
+  const query = { ...route.query }
+  delete query.inGame
+  router.replace({ query })
 }
 const open = (gameName, platformCode, gameCode, gameType) => {
       //
-      router.push({query: {inGame: true}});
+      router.push({query: { ...route.query, inGame: true}});
       AppFullscreen.request();
       window.screen.orientation.unlock();
-
       localStorage.removeItem("isOpenFromAccount");
       localStorage.removeItem("isBacked");
       // window.addEventListener(
