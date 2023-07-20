13233c6be56527dc2e9ce14eac6de45fec2feb79
Mika <mika@eagrp.net>
2023-07-20 11:56:08 +0800

    update home banner width.

--- dy-static-new-h5/src/pages/HomePage.vue
+++ dy-static-new-h5/src/pages/HomePage.vue
@@ -875,7 +875,7 @@
 
   <q-dialog width="100%" v-model="isFirstView">
     <q-card
-        style="width: 90%; max-width: 500px; margin: 0 auto"
+        style="width: 70%; max-width: 340px; margin: 0 auto"
         class="bg-white text-black"
     >
       <q-card-section class="q-mb-md">
