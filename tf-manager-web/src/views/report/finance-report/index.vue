f245b49cb6b2d4d17c910eac1e20419ae6d49f4c
leo <leo.nam@outlook.com>
2023-07-27 17:34:22 +0800

    财务报表

---
+++ tf-manager-web/src/views/report/finance-report/index.vue
@@ -0,0 +1,373 @@
+<template>
+  <div class="roles-main">
+    <div class="header-container">
+      <div class="search">
+        <el-date-picker
+          v-model="request.recordTime"
+          format="DD/MM/YYYY"
+          value-format="YYYY-MM-DD"
+          size="small"
+          type="daterange"
+          range-separator=":"
+          :start-placeholder="t('fields.startDate')"
+          :end-placeholder="t('fields.endDate')"
+          style="width: 250px"
+          :shortcuts="shortcuts"
+          :disabled-date="disabledDate"
+          :editable="false"
+          :clearable="false"
+        />
+
+        <el-select
+          v-model="request.siteId"
+          size="small"
+          :placeholder="t('fields.site')"
+          class="filter-item"
+          style="width: 120px; margin-left: 5px"
+        >
+          <el-option
+            v-for="item in siteList.list"
+            :key="item.id"
+            :label="item.siteName"
+            :value="item.id"
+          />
+        </el-select>
+        <el-button
+          style="margin-left: 20px"
+          icon="el-icon-search"
+          size="mini"
+          type="success"
+          @click="loadFinanceReport()"
+        >
+          {{ t('fields.search') }}
+        </el-button>
+        <el-button
+          icon="el-icon-refresh"
+          size="mini"
+          type="warning"
+          @click="resetQuery()"
+        >
+          {{ t('fields.reset') }}
+        </el-button>
+        <el-button
+          icon="el-icon-download"
+          size="mini"
+          type="primary"
+          v-permission="['sys:report:finance:report:export']"
+          @click="exportExcel"
+          style="float: right;"
+        >
+          {{ t('fields.exportToExcel') }}
+        </el-button>
+      </div>
+    </div>
+
+    <el-table
+      :data="page.records"
+      ref="table"
+      row-key="id"
+      size="small"
+      highlight-current-row
+      v-loading="page.loading"
+      :empty-text="t('fields.noData')"
+      style="margin-top:20px;overflow-x:scroll;width:100%"
+      :row-style="{width: '100px'}"
+      height="550"
+    >
+      <el-table-column
+        style="width:80px"
+        v-for="column in page.columns"
+        :key="column.label"
+        :prop="column.prop"
+        :label="column.label"
+      />
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted, reactive, ref } from 'vue'
+import moment from 'moment'
+import { getFinanceReport } from '../../../api/report-centre'
+import { getSiteListSimple } from '../../../api/site'
+import { useStore } from '../../../store'
+import { TENANT } from '../../../store/modules/user/action-types'
+import { useI18n } from 'vue-i18n'
+import * as XLSX from 'xlsx'
+
+const { t } = useI18n()
+const startDate = new Date()
+startDate.setDate(startDate.getDate())
+const defaultStartDate = convertDate(startDate)
+const defaultEndDate = convertDate(new Date())
+const store = useStore()
+const LOGIN_USER_TYPE = computed(() => store.state.user.userType)
+const site = ref(null)
+
+const siteList = reactive({
+  list: [],
+})
+
+const page = reactive({
+  records: [],
+  columns: [],
+  loading: false,
+})
+
+const request = reactive({
+  size: 30,
+  current: 1,
+  siteId: null,
+  recordTime: [defaultStartDate, defaultEndDate],
+})
+
+function resetQuery() {
+  request.recordTime = [defaultStartDate, defaultEndDate]
+  request.siteId = site.value ? site.value.id : 1
+}
+
+async function loadFinanceReport() {
+  page.loading = true
+  const requestCopy = { ...request }
+  const query = {}
+  Object.entries(requestCopy).forEach(([key, value]) => {
+    if (value) {
+      query[key] = value
+    }
+  })
+  if (request.recordTime !== null) {
+    if (request.recordTime.length === 2) {
+      query.recordTime = request.recordTime.join(',')
+    }
+  }
+  const { data: ret } = await getFinanceReport(query)
+  console.log(ret)
+  page.records = ret.financeReportItemVOS
+  page.columns = ret.financeReportColumnVOS
+
+  console.log(page.columns)
+
+  page.loading = false
+}
+
+async function loadSites() {
+  const { data: site } = await getSiteListSimple()
+  siteList.list = site
+}
+
+function convertDate(date) {
+  return moment(date).format('YYYY-MM-DD')
+}
+
+const shortcuts = [
+  {
+    text: t('fields.today'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .startOf('day')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.yesterday'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .subtract(1, 'days')
+          .startOf('day')
+          .format('x')
+      )
+      end.setTime(
+        moment(end)
+          .subtract(1, 'days')
+          .endOf('day')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.thisWeek'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .startOf('isoWeek')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.lastWeek'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .subtract(1, 'weeks')
+          .startOf('isoWeek')
+          .format('x')
+      )
+      end.setTime(
+        moment(end)
+          .subtract(1, 'weeks')
+          .endOf('isoWeek')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.thisMonth'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .startOf('month')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.lastMonth'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .subtract(1, 'months')
+          .startOf('month')
+          .format('x')
+      )
+      end.setTime(
+        moment(end)
+          .subtract(1, 'months')
+          .endOf('month')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.last3Months'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .subtract(2, 'months')
+          .startOf('month')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+  {
+    text: t('fields.last6Months'),
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(
+        moment(start)
+          .subtract(5, 'months')
+          .startOf('month')
+          .format('x')
+      )
+      return [start, end]
+    },
+  },
+]
+
+function disabledDate(time) {
+  return (
+    time.getTime() <
+      moment(new Date())
+        .subtract(5, 'months')
+        .startOf('month')
+        .format('x') || time.getTime() > new Date().getTime()
+  )
+}
+
+async function exportExcel() {
+  page.loading = true
+  const requestCopy = { ...request }
+  const query = {}
+  Object.entries(requestCopy).forEach(([key, value]) => {
+    if (value) {
+      query[key] = value
+    }
+  })
+  if (request.recordTime !== null) {
+    if (request.recordTime.length === 2) {
+      query.recordTime = request.recordTime.join(',')
+    }
+  }
+  const { data: ret } = await getFinanceReport(query)
+  const header = []
+  const data = []
+
+  const exportData = ret.financeReportColumnVOS
+
+  exportData.forEach(item => {
+    header.push(item.label);
+  })
+
+  ret.financeReportItemVOS.forEach(item => {
+    data.push(item.data);
+  })
+
+  const headerarray = []
+  headerarray.push(header);
+  pushRecordToData(data, headerarray)
+  console.log(headerarray);
+  const ws = XLSX.utils.aoa_to_sheet(headerarray)
+  const wb = XLSX.utils.book_new()
+  wb.SheetNames.push('Record')
+  wb.Sheets.Record = ws
+  XLSX.writeFile(wb, t('reportName.Finance_Report') + '.xlsx')
+
+  page.loading = false
+}
+
+function pushRecordToData(records, exportData) {
+  const data = records.map(record =>
+    Object.values(record).map(item => (!item || item === '' ? '-' : item))
+  )
+  exportData.push(...data)
+}
+
+onMounted(async () => {
+  await loadSites()
+
+  if (LOGIN_USER_TYPE.value === TENANT.value) {
+    site.value = siteList.list.find(
+      s => s.siteName === store.state.user.siteName
+    )
+    request.siteId = site.value.id
+  } else {
+    request.siteId = 1
+  }
+  await loadFinanceReport()
+})
+</script>
+
+<style>
+.el-table .cell {
+  word-break: break-word;
+  text-overflow: none;
+}
+.warning-row {
+  color: #ff0000;
+}
+</style>
