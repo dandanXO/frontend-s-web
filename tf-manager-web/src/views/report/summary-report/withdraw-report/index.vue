ea369ded3203efe97ba2b42dd43c00719b394be7
martin <martin@eagrp.net>
2023-08-02 17:33:36 +0800

    Update summary report export button permission

--- tf-manager-web/src/views/report/summary-report/withdraw-report/index.vue
+++ tf-manager-web/src/views/report/summary-report/withdraw-report/index.vue
@@ -24,7 +24,7 @@
           icon="el-icon-download"
           size="mini"
           type="primary"
-          v-permission="['sys:report:summary:active:export']"
+          v-permission="['sys:report:summary:withdraw:export']"
           @click="exportExcel"
         >
           {{ t('fields.exportToExcel') }}
