f220fe7df16ad40e1d497bd22caae69906f17d8c
leo <leo.nam@outlook.com>
2023-08-25 17:32:12 +0800

    update

--- tf-manager-web/src/views/report/site-member-report/index.vue
+++ tf-manager-web/src/views/report/site-member-report/index.vue
@@ -214,7 +214,7 @@
           $
           <span
             v-formatter="{
-              data: scope1.row.profit,
+              data: scope1.row.firstDeposit,
               type: 'money',
             }"
           />
@@ -283,7 +283,7 @@ const totalPage = reactive({
 })
 
 const request = reactive({
-  size: 30,
+  size: 20,
   current: 1,
   siteId: null,
   loginName: null,
