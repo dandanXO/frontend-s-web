e7907669e3787f57f371ff93c88d057828469be5
WeiSeng <weiseng@eagrp.net>
2023-10-20 11:32:21 +0800

    Fixed THA 1004 translate

--- tha-static-h5/src/i18n/en.js
+++ tha-static-h5/src/i18n/en.js
@@ -441,6 +441,7 @@ export default {
     1000: "Login name or password error",
     1001: "Account is closed",
     1002: "Account is for affiliates",
+    1004: "Username can only contains numbers and letters",
     1300: "Amount must be a number greater than 0",
     1301: "This card is not supported",
     1302: "This currency is not supported",
