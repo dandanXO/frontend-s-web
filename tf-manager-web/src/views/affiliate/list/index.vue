e0dce35b2cac73033333cd090325c7802adc0d2e
karchuan <karchuan@caspen-tech.com>
2023-10-25 11:57:38 +0800

    feat: affiliate module 添加时区

--- tf-manager-web/src/views/affiliate/list/index.vue
+++ tf-manager-web/src/views/affiliate/list/index.vue
@@ -284,7 +284,7 @@
         <el-table-column prop="regTime" :label="t('fields.registerTime')" width="150">
           <template #default="scope">
             <span v-if="scope.row.regTime === null">-</span>
-            <span v-if="scope.row.regTime !== null" v-formatter="{data: scope.row.regTime,formatter: 'YYYY/MM/DD HH:mm:ss',type: 'date'}" />
+            <span v-if="scope.row.regTime !== null" v-formatter="{data: scope.row.regTime,timeZone: siteTimeZone.timeZone,type: 'date'}" />
           </template>
         </el-table-column>
         <el-table-column
@@ -361,6 +361,9 @@ const table = ref(null);
 const siteList = reactive({
   list: []
 });
+const siteTimeZone = reactive({
+  timeZone: null,
+})
 const freezeType = reactive({
   list: [
     { key: 1, name: "NORMAL", value: "NORMAL" },
@@ -578,6 +581,10 @@ async function loadAffiliates() {
 
   page.pages = result.data.pages;
   page.records = result.data.records;
+
+  var siteSelected = siteList.list.find(e => e.id === request.siteId)
+  siteTimeZone.timeZone = siteSelected.timeZone;
+
   page.loading = false;
 }
 
