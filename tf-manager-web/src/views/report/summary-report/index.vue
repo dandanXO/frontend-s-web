4d423efb8b6a42806a804851cf08129d7d2561c3
leo <leo.nam@outlook.com>
2023-10-31 13:17:27 +0800

    fix: 首页报表不显示首存， 活跃人数 fix: 更改人均首存 -> 人均充值

--- tf-manager-web/src/views/report/summary-report/index.vue
+++ tf-manager-web/src/views/report/summary-report/index.vue
@@ -147,7 +147,7 @@
       </el-table-column>
       <el-table-column
         prop="fdAverage"
-        :label="t('fields.ftdAverage')"
+        :label="t('fields.depositAverage')"
         width="120"
       >
         <template #default="scope1">
