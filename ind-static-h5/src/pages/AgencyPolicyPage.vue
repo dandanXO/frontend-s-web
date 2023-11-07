8f8146c678ce93581b59e016e9bbb939982051c8
ChoonLoong <choonloongatwork@gmail.com>
2023-11-07 16:53:56 +0800

    fix: IND - missing props for SwiperNav

--- ind-static-h5/src/pages/AgencyPolicyPage.vue
+++ ind-static-h5/src/pages/AgencyPolicyPage.vue
@@ -1,15 +1,13 @@
 <template>
-  <SwiperNav :slideList="slideList" :onSlideClick="onSlideClick" :isActiveSlide="isActiveSlide"></SwiperNav>
+  <SwiperNav :slideList="slideList" :slideListPath="slideListPath" :isActiveSlide="isActiveSlide"></SwiperNav>
   <AgencyPolicy></AgencyPolicy>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 import SwiperNav from "../components/SwiperNav.vue";
 import AgencyPolicy from "../components/earn-money/AgencyPolicy.vue";
 
-const router = useRouter();
 let slideList = ref(["Agency Policy", "My Member"]);
 let slideListPath = ref(["/agency-policy", "/earn-money"]);
 let currentSlide = ref(slideList.value[0]);
@@ -17,10 +15,5 @@ let currentSlide = ref(slideList.value[0]);
 const isActiveSlide = (e) => {
   if (e === currentSlide.value) return true;
   return false;
-};
-const onSlideClick = (e, i) => {
-  if (e === currentSlide.value) return;
-  router.push(slideListPath.value[i]);
-  currentSlide.value = e;
 };
 </script>
