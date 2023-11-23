ba1be0a081e1db81308515ad44339a379f6ccab2
vincey <vincey@eagrp.net>
2023-11-23 12:03:51 +0800

    Revert "Update"
    This reverts commit 8d9dedcf45303da5eb728063e68f89a8203e98f8.

--- tf-manager-web/src/views/site-management/game/index.vue
+++ tf-manager-web/src/views/site-management/game/index.vue
@@ -687,7 +687,7 @@ const gameLabel = reactive({
   list: [
     { key: 1, displayName: 'NEW', value: 'NEW' },
     { key: 2, displayName: 'HOT', value: 'HOT' },
-    { key: 3, displayName: 'REC', value: 'REC' },
+    { key: 3, displayName: 'RECOMMEND', value: 'RECOMMEND' },
     { key: 4, displayName: 'LIST', value: 'LIST' },
   ],
 })
