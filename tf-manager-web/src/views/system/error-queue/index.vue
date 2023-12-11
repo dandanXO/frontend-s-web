b19fb2f2235792d3e982886ef5a4fe0b3deee16a
yijing <yijing@eagrp.net>
2023-12-11 15:26:49 +0800

    fix: error queue json add indent

--- tf-manager-web/src/views/system/error-queue/index.vue
+++ tf-manager-web/src/views/system/error-queue/index.vue
@@ -209,6 +209,7 @@ async function removeMessagelist() {
 }
 
 function syntaxHighlight(json) {
+  json = JSON.stringify(JSON.parse(json), undefined, 2);
   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
     var cls = 'number';
