920611668bfcf0b0b651ada2f879aef3c9e01e14
WeiSeng <weiseng@eagrp.net>
2023-12-12 17:57:18 +0800

    feat: DY H5 inbox tabs & readall deleteAll

--- dy-static-h5/src/components/MailComponent.vue
+++ dy-static-h5/src/components/MailComponent.vue
@@ -6,19 +6,19 @@
     </q-inner-loading>
     <div v-if="!loading">
       <q-infinite-scroll @load="onLoad" :offset="150">
-        <div v-if="truncatedList.length" class="q-pa-sm quick-btn-container">
-          <button class="read-all-btn" @click="readAllMessage()">
+        <div v-if="lists[type].length" class="q-pa-sm quick-btn-container">
+          <button class="read-all-btn" @click="readAllMessage(type)">
             <q-icon name="mail_outline" />
             <div>全部已读</div>
           </button>
-          <button class="del-all-btn" @click="deleteAllMessage()">
+          <button class="del-all-btn" @click="deleteAllMessage(type)">
             <q-icon name="delete" />
             <div>全部删除</div>
           </button>
         </div>
 
         <q-card
-          v-for="(det, n) in truncatedList"
+          v-for="(det, n) in lists[type]"
           :key="n"
           class="q-pa-sm mail-card"
           :class="{ active: isSelectedMail === det.title }"
@@ -47,17 +47,34 @@
             </div>
           </div>
           <div v-else class="q-pa-md" style="text-align: center">
-            {{ truncatedList.length === 0 ? "暂无数据" : "暂无更多数据了" }}
+            {{ lists[type].length === 0 ? "暂无数据" : "暂无更多数据了" }}
           </div>
         </template>
       </q-infinite-scroll>
     </div>
   </div>
+
+  <q-dialog width="100%" v-model="isDeleteAllModal">
+    <q-card style="width: 100%; padding: 20px" class="text-black">
+      <q-card-section class="q-mb-md text-center" style="flex-direction: column">
+        <strong>温馨提示</strong>
+        <br />
+        <br />
+        确认删除信息？
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn @click="confirmDeleteAll(type)" label="确认" color="dyblue" />
+        <q-btn @click="isDeleteAllModal = false" label="取消" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref, reactive } from "vue";
 import moment from "moment";
+import { useQuasar } from "quasar";
+import { api } from "boot/axios";
 
 export default defineComponent({
   props: {
@@ -78,19 +95,31 @@ export default defineComponent({
       default: function () {
         return "";
       }
+    },
+    type: {
+      type: String,
+      default: function () {
+        return "NOTIFICATION";
+      }
     }
   },
   setup(props, { emit }) {
-    const truncatedList = ref([]);
-    const comList = ref({});
+    const $q = useQuasar();
+    const isDeleteAllModal = ref(false);
+    const lists = reactive({
+      NOTIFICATION: [],
+      ACTIVITY: [],
+      ANNOUNCEMENT: [],
+      PAYMENT: [],
+      ALL: []
+    });
+    const comList = ref([]);
     const onLoad = (index, done) => {
       comList.value = props.list;
       setTimeout(() => {
         if (comList.value.length) {
-          var slicedArray = comList.value.splice(0, 6);
-          slicedArray.forEach((element) => {
-            truncatedList.value.push(element);
-          });
+          var slicedArray = comList.value.splice(0, 10);
+          lists[props.type] = mergeArrays(lists[props.type], slicedArray);
           done();
         }
       }, 200);
@@ -100,9 +129,52 @@ export default defineComponent({
       isSelectedMail.value = mail.title;
     };
 
-    const readAllMessage = () => {};
+    const readAllMessage = (type) => {
+      api
+        .post("/session/inbox/readAll", {
+          type: type
+        })
+        .then((res) => {
+          if (res.code === 0) {
+            $q.notify({
+              message: "全部消息已读",
+              type: "positive",
+              position: "top",
+              icon: "check_circle_outline"
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
 
-    const deleteAllMessage = () => {};
+    const confirmDeleteAll = (type) => {
+      api
+        .post("/session/inbox/deleteAll", {
+          type: type
+        })
+        .then((res) => {
+          isDeleteAllModal.value = false;
+          if (res.code === 0) {
+            $q.notify({
+              message: "已删除全部消息",
+              type: "positive",
+              position: "top",
+              icon: "check_circle_outline"
+            });
+            lists[type] = [];
+          }
+        })
+        .catch((error) => {
+          isDeleteAllModal.value = false;
+          console.log(error);
+        });
+    };
+
+    const deleteAllMessage = (type) => {
+      isDeleteAllModal.value = true;
+    };
 
     onMounted(() => {
       onLoad;
@@ -112,15 +184,46 @@ export default defineComponent({
         return moment(ts).format("YYYY-MM-DD HH:mm:ss");
       },
       onLoad,
-      truncatedList,
+      lists,
       comList,
       selectMail,
       isSelectedMail,
       readAllMessage,
-      deleteAllMessage
+      deleteAllMessage,
+      isDeleteAllModal,
+      confirmDeleteAll
     };
   }
 });
+
+function mergeArrays(array1, array2) {
+  // 创建一个空数组来存储结果
+  const mergedArray = [];
+
+  // 将第一个数组的元素添加到合并数组中
+  for (const item of array1) {
+    // 检查合并数组中是否已存在相同ID的元素
+    const existingItem = mergedArray.find((i) => i.id === item.id);
+
+    if (!existingItem) {
+      // 如果合并数组中不存在相同ID的元素，则添加
+      mergedArray.push(item);
+    }
+  }
+
+  // 将第二个数组的元素添加到合并数组中
+  for (const item of array2) {
+    // 检查合并数组中是否已存在相同ID的元素
+    const existingItem = mergedArray.find((i) => i.id === item.id);
+
+    if (!existingItem) {
+      // 如果合并数组中不存在相同ID的元素，则添加
+      mergedArray.push(item);
+    }
+  }
+
+  return mergedArray;
+}
 </script>
 
 <style scoped lang="scss">
@@ -143,6 +246,7 @@ export default defineComponent({
   .read-all-btn {
     background: $primary;
   }
+
   .del-all-btn {
     background: $secondary;
   }
@@ -151,20 +255,24 @@ export default defineComponent({
 .q-infinite-scroll {
   padding-top: 14px;
 }
+
 .table-data {
   font-size: 16px;
   display: flex;
   justify-content: flex-start;
   gap: 30px;
   margin: 12px 0 10px;
+
   .label {
     flex: 1;
   }
+
   .desc {
     flex: 3;
     word-break: break-all;
   }
 }
+
 .buttons {
   text-align: right;
 }
