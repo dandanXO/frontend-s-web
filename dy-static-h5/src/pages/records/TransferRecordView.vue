f31243bb4b02e7c7b8193bad22f3a3437766ff2c
WeiSeng <weiseng@eagrp.net>
2023-12-14 18:44:23 +0800

    feat: DY H5 History table add pagination

--- dy-static-h5/src/pages/records/TransferRecordView.vue
+++ dy-static-h5/src/pages/records/TransferRecordView.vue
@@ -31,18 +31,25 @@ export default defineComponent({
 
     var endDate = moment().format("YYYY-MM-DD");
     var startDate = moment().add(-7, "days").format("YYYY-MM-DD");
+    var current = ref(1);
+    var maxPage = ref(0);
 
     const loadNewData = () => {
-      startDate = moment(startDate).add(-7, "days").format("YYYY-MM-DD");
-      console.log(startDate);
+      if(maxPage.value > current.value){
+        current.value++;
+      }else {
+        current.value = 1;
+        endDate = moment(startDate).add(-1, "days").format("YYYY-MM-DD");
+        console.log(endDate);
 
-      endDate = moment(endDate).add(-7, "days").format("YYYY-MM-DD");
-      console.log(endDate);
+        startDate = moment(endDate).add(-7, "days").format("YYYY-MM-DD");
+        console.log(startDate);
 
-      if (startDate <= moment().add(-30, "days").format("YYYY-MM-DD")) {
-        console.log("mor than 3 months");
-        isEnded.value = true;
-        return;
+        if (endDate <= moment().add(-29, "days").format("YYYY-MM-DD")) {
+          console.log("mor than 3 months");
+          isEnded.value = true;
+          return;
+        }
       }
       loadDepositTable(false);
     };
@@ -56,17 +63,21 @@ export default defineComponent({
 
       let paramData = {
         "startDate": startDate,
-        "endDate": endDate
+        "endDate": endDate,
+        "size": 10,
+        "current": current.value
       };
-      var apiKey = apiUrl + "_" + startDate + "_" + endDate;
+      var apiKey = apiUrl + "_" + startDate + "_" + endDate + "_" + current.value;
       console.log(apiKey);
-
-      cached.get(apiKey, () => api.get(apiUrl, {
+      cached.get(apiKey, () =>
+          api.get(apiUrl, {
           params: paramData
         }),
         {expired_value: 30}
       ).then((res) => {
         console.log(res);
+
+        maxPage.value = res.pages;
 
         if (isNew) {
           visible.value = false;
@@ -110,6 +121,7 @@ export default defineComponent({
 
     ]);
     onMounted(() => {
+      current.value = 1;
       loadDepositTable();
     });
 
@@ -123,6 +135,4 @@ export default defineComponent({
   }
 });
 </script>
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
