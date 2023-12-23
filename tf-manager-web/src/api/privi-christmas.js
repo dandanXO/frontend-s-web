63538c21a7c22e176b23165fb5fc82fd5d2ea495
karchuan <karchuan@caspen-tech.com>
2023-12-23 13:46:32 +0800

    change id to promocode

--- tf-manager-web/src/api/privi-christmas.js
+++ tf-manager-web/src/api/privi-christmas.js
@@ -1,12 +1,12 @@
 import https from "@/utils/https";
 import { ContentType, Method } from "axios-mapper";
 
-export const getWinnerList = (id) => {
-  return https().request("/privi-christmas/queryWinner", Method.GET, { id: id }, ContentType.form);
+export const getWinnerList = (code) => {
+  return https().request("/privi-christmas/queryWinner", Method.GET, { promoCode: code }, ContentType.form);
 };
 
-export const calculateWinner = (id) => {
-  return https().request("/privi-christmas/calculateWinner", Method.POST, { id: id }, ContentType.form);
+export const calculateWinner = (code) => {
+  return https().request("/privi-christmas/calculateWinner", Method.POST, { promoCode: code }, ContentType.form);
 };
 
 export const getParticipants = (form) => {
