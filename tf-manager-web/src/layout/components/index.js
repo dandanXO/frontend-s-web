b3ed770fa588e47714f3f7088bd67a79e4749936
yijing <yijing@eagrp.net>
2024-01-11 16:44:30 +0800

    feat: real time withdraw apply update

--- tf-manager-web/src/layout/components/index.js
+++ tf-manager-web/src/layout/components/index.js
@@ -1,4 +1,4 @@
 
 export { default as AppMain } from './AppMain.vue'
-export { default as Navbar } from './navigation_bar/Index.vue'
 export { default as Sidebar } from './side_bar/Index.vue'
+export { default as Navbar } from './navigation_bar/Index.vue'
