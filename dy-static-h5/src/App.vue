a330469b9ba5123bfaad9d4ba31dc9630986403c
WeiSeng <weiseng@eagrp.net>
2024-01-12 16:56:12 +0800

    feat: DY add timer for memberStatistics

--- dy-static-h5/src/App.vue
+++ dy-static-h5/src/App.vue
@@ -12,6 +12,7 @@ import { userStore } from "src/stores";
 import { cached } from "boot/cache";
 import { useRouter } from "vue-router";
 import { isAndroid, isHuaweiPhone } from "boot/utils";
+import axios from "axios";
 
 export default defineComponent({
   name: "App",
@@ -112,6 +113,32 @@ export default defineComponent({
         }
       });
     };
+
+    const getOnlineStatApi = async () => {
+      // console.log("Ok Online.");
+      const fpPromise = FingerprintJS.load();
+
+      const fp = await fpPromise;
+      const result = await fp.get();
+      const excludes = { value: ["timezone", "timeZoneOffset"] };
+      const allComponents = { ...result.components };
+      excludes.value.forEach((element) => {
+        delete allComponents[element];
+      });
+      const sidParam = FingerprintJS.hashComponents(allComponents);
+      const way = "h5";
+
+      if (sidParam) {
+        const res = await axios.get("https://memsta.eatrhaquke.com/memberStatistics/submit", {
+          params: {
+            way: way,
+            sid: sidParam,
+            siteCode: "dy2"
+          }
+        });
+      }
+    };
+
     onMounted(() => {
       checkSID();
       // initCsWeb();
@@ -119,6 +146,9 @@ export default defineComponent({
       if (isAndroid() && !isHuaweiPhone()) {
         window.screen.orientation.lock("portrait");
       }
+
+      setTimeout(getOnlineStatApi, 2000);
+      setInterval(getOnlineStatApi, 60000);
     });
   }
   /*test*/
--- dy-static-web/src/App.vue
+++ dy-static-web/src/App.vue
@@ -6,6 +6,7 @@
 import { defineComponent, onMounted } from "vue";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 import { memberAccessLog } from "@/api/index/login";
+import axios from "axios";
 
 export default defineComponent({
   setup() {
@@ -21,9 +22,11 @@ export default defineComponent({
           delete allComponents[element];
         });
         const sidParam = FingerprintJS.hashComponents(allComponents);
+        console.log(sidParam);
+
         const obj = {
           identifier: sidParam,
-          affiliateCode: affiliateItem,
+          affiliateCode: affiliateItem
         };
         memberAccessLog(obj).then((res) => {
           if (res.code === 0) {
@@ -32,10 +35,39 @@ export default defineComponent({
         });
       })();
     };
+
+    const getOnlineStatApi = async () => {
+      // console.log("Ok Online.");
+      const fpPromise = FingerprintJS.load();
+
+      const fp = await fpPromise;
+      const result = await fp.get();
+      const excludes = { value: ["timezone", "timeZoneOffset"] };
+      const allComponents = { ...result.components };
+      excludes.value.forEach((element) => {
+        delete allComponents[element];
+      });
+      const sidParam = FingerprintJS.hashComponents(allComponents);
+      const way = "web";
+
+      if (sidParam) {
+        const res = await axios.get("https://memsta.eatrhaquke.com/memberStatistics/submit", {
+          params: {
+            way: way,
+            sid: sidParam,
+            siteCode: "dy2"
+          }
+        });
+      }
+    };
+
     onMounted(() => {
       checkSID();
+
+      setTimeout(getOnlineStatApi, 2000);
+      setInterval(getOnlineStatApi, 60000);
     });
-  },
+  }
 });
 </script>
 <style lang="scss">
