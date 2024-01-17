c43b21b042b974bc8f3bc7b0259fdf5a1e55ca3c
cody <code@eagrp.net>
2024-01-17 20:44:21 +0800

    竞猜赛事提交

--- tf-manager-web/src/views/promotion-display/game-quiz/question/index.vue
+++ tf-manager-web/src/views/promotion-display/game-quiz/question/index.vue
@@ -680,7 +680,8 @@ const uiControl = reactive({
     { key: 1, displayName: 'gameQuiz.questions.2', value: 'gameQuiz.questions.2' },
     { key: 2, displayName: 'gameQuiz.questions.3', value: 'gameQuiz.questions.3' },
     { key: 3, displayName: 'gameQuiz.questions.4', value: 'gameQuiz.questions.4' },
-    { key: 4, displayName: 'gameQuiz.questions.5', value: 'gameQuiz.questions.5' }
+    { key: 4, displayName: 'gameQuiz.questions.5', value: 'gameQuiz.questions.5' },
+    { key: 5, displayName: 'gameQuiz.questions.6', value: 'gameQuiz.questions.6' }
   ]
 });
 const page = reactive({
@@ -1027,7 +1028,7 @@ function populateChoice() {
     } else if (question.key === 3) {
       choiceTwo.value[0].value = t('gameQuiz.answers.' + form.gameType + '.killSignal')
       choiceTwo.value[1].value = t('gameQuiz.answers.' + form.gameType + '.killDouble')
-    } else if (question.key === 4) {
+    } else if (question.key === 4 || question.key === 5) {
       choiceTwo.value[0].value = form.homeTeam
       choiceTwo.value[1].value = form.awayTeam
     }
@@ -1044,7 +1045,7 @@ function populateChoice() {
     } else if (question.key === 3) {
       choiceThree.value[0].value = t('gameQuiz.answers.' + form.gameType + '.killSignal')
       choiceThree.value[1].value = t('gameQuiz.answers.' + form.gameType + '.killDouble')
-    } else if (question.key === 4) {
+    } else if (question.key === 4 || question.key === 5) {
       choiceThree.value[0].value = form.homeTeam
       choiceThree.value[1].value = form.awayTeam
     }
