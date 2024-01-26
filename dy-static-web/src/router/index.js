b51b636ddb8f45eaa0a631831bf81949aaf6abfd
WeiSeng <weiseng@eagrp.net>
2024-01-26 23:55:46 +0800

    feat: DY hotfix invite page

--- dy-static-web/src/router/index.js
+++ dy-static-web/src/router/index.js
@@ -105,7 +105,7 @@ const routes = [
       },
       {
         path: "/privilege/hongbaoyu",
-        name: "invite",
+        name: "hongbaoyu",
         component: () => import("../views/HongBaoYu2024.vue")
       },
       {
