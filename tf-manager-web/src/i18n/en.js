3bf2981014633712d7af3f5b376fb918fe2ad82a
leo <leo.nam@outlook.com>
2024-01-29 17:57:29 +0800

    feat: 語言

--- tf-manager-web/src/i18n/en.js
+++ tf-manager-web/src/i18n/en.js
@@ -1882,6 +1882,7 @@ export default {
     'Add Ads Popout': 'Add Ads Popout',
     'Edit Ads Popout': 'Edit Ads Popout',
     'Red Packet Rain': 'Red Packet Rain',
+    'Red Packet Rain List': 'Red Packet Rain List',
     Lottery: 'Lottery',
     'Lottery Record List': 'Lottery Record List',
     'Lottery Result List': 'Lottery Result List',
