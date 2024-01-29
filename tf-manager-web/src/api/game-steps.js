856be4d328eba7be4cc0cd647a08a977b486b9e6
darren <darren@eagrp.net>
2024-01-29 12:24:32 +0800

    feat: 添加后台查询闯关记录

--- tf-manager-web/src/api/game-steps.js
+++ tf-manager-web/src/api/game-steps.js
@@ -9,6 +9,10 @@ export const updateGameStepsParam = (siteId, param) => {
   return https().request(`/game-steps?_method=PUT`, Method.POST, { siteId: siteId, param: param }, ContentType.form);
 };
 
+export const getGameStepsRecords = (gameSteps) => {
+  return https().request("/game-steps/records", Method.GET, gameSteps, ContentType.form);
+};
+
 export const getSiteWithPromo = () => {
   return https().request("/game-steps/sites", Method.GET);
 }
