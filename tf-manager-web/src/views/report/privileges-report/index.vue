d6f3b0ec1e9b0dc0dc92b29435cb2207ff56268f
SHIAN\dcshi <dcshian85@hotmail.com>
2024-02-29 17:56:39 +0800

    Fixed privilege expand use name instead of using id

--- tf-manager-web/src/views/report/privileges-report/index.vue
+++ tf-manager-web/src/views/report/privileges-report/index.vue
@@ -218,13 +218,13 @@ async function loadDaily(row, expandedRows) {
       }
     }
 
-    dailyquery.id = row.privilegeId
+    dailyquery.name = row.name
     dailyquery.siteId = row.siteId
 
     const { data: ret } = await getDailyReport(dailyquery)
     page.records.forEach((temp, index) => {
       // 找到当前点击的行，把动态获取到的数据赋值进去
-      if (temp.id === row.id) {
+      if (temp.name === row.name) {
         page.records[index].ruleItemData = ret.records
         console.log(page.records)
       }
