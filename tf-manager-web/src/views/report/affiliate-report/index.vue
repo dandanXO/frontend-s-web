072093fce8c4683eec6705967a6cd23c02e3b399
SHIAN\dcshi <dcshian85@hotmail.com>
2024-03-06 15:38:56 +0800

    update affiliate report child dropdown key

--- tf-manager-web/src/views/report/affiliate-report/index.vue
+++ tf-manager-web/src/views/report/affiliate-report/index.vue
@@ -31,8 +31,18 @@
           :editable="false"
           :clearable="false"
         />
-        <el-input v-model="request.loginName" size="small" style="width: 200px; margin-left: 5px" :placeholder="t('fields.loginName')" />
-        <el-input v-model="request.affiliateCode" size="small" style="width: 200px; margin-left: 5px" :placeholder="t('fields.affiliateCode')" />
+        <el-input
+          v-model="request.loginName"
+          size="small"
+          style="width: 200px; margin-left: 5px"
+          :placeholder="t('fields.loginName')"
+        />
+        <el-input
+          v-model="request.affiliateCode"
+          size="small"
+          style="width: 200px; margin-left: 5px"
+          :placeholder="t('fields.affiliateCode')"
+        />
         <el-select
           v-model="request.affiliateLevel"
           size="small"
@@ -71,13 +81,19 @@
           type="primary"
           v-permission="['sys:report:summary:affiliate:export']"
           @click="requestExportExcel"
-        >{{ t('fields.requestExportToExcel') }}
+        >
+          {{ t('fields.requestExportToExcel') }}
         </el-button>
       </div>
     </div>
 
-    <el-dialog :title="t('fields.exportToExcel')" v-model="uiControl.messageVisible" append-to-body width="500px"
-               :close-on-click-modal="false" :close-on-press-escape="false"
+    <el-dialog
+      :title="t('fields.exportToExcel')"
+      v-model="uiControl.messageVisible"
+      append-to-body
+      width="500px"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
     >
       <span>{{ t('message.requestExportToExcelDone1') }}</span>
       <router-link :to="`/site-management/download-manager`">
@@ -91,7 +107,7 @@
     <el-table
       :data="page.records"
       ref="table"
-      row-key="id"
+      row-key="affiliateId"
       size="small"
       highlight-current-row
       v-loading="page.loading"
@@ -102,41 +118,110 @@
       show-summary
       :summary-method="populateReportTotal"
     >
-      <el-table-column prop="recordTime" :label="t('fields.recordTime')" align="center" min-width="140" fixed="left" />
-      <el-table-column prop="loginName" :label="t('fields.loginName')" align="center" min-width="110" fixed="left">
+      <el-table-column
+        prop="recordTime"
+        :label="t('fields.recordTime')"
+        align="center"
+        min-width="140"
+        fixed="left"
+      />
+      <el-table-column
+        prop="loginName"
+        :label="t('fields.loginName')"
+        align="center"
+        min-width="110"
+        fixed="left"
+      >
         <template #default="scope" v-if="hasPermission(['sys:member:detail'])">
-          <router-link :to="`/affiliate/details/${scope.row.affiliateId}?site=${request.siteId}`">
+          <router-link
+            :to="
+              `/affiliate/details/${scope.row.affiliateId}?site=${request.siteId}`
+            "
+          >
             <el-link type="primary">{{ scope.row.loginName }}</el-link>
           </router-link>
         </template>
       </el-table-column>
-      <el-table-column prop="affiliateCode" :label="t('fields.affiliateCode')" align="center" min-width="100" />
-      <el-table-column prop="affiliateLevel" :label="t('fields.affiliateLevel')" align="center" min-width="120">
+      <el-table-column
+        prop="affiliateCode"
+        :label="t('fields.affiliateCode')"
+        align="center"
+        min-width="100"
+      />
+      <el-table-column
+        prop="affiliateLevel"
+        :label="t('fields.affiliateLevel')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
           {{ t('affiliate.level.' + scope.row.affiliateLevel) }}
         </template>
       </el-table-column>
-      <el-table-column prop="commissionModel" :label="t('fields.commissionModel')" align="center" min-width="130">
+      <el-table-column
+        prop="commissionModel"
+        :label="t('fields.commissionModel')"
+        align="center"
+        min-width="130"
+      >
         <template #default="scope">
-          <el-tag v-if="scope.row.commissionModel === 'NORMAL'" type="success">{{ t('affiliate.commissionModel.' + scope.row.commissionModel) }}</el-tag>
-          <el-tag v-else type="primary">{{ t('affiliate.commissionModel.' + scope.row.commissionModel) }}</el-tag>
+          <el-tag v-if="scope.row.commissionModel === 'NORMAL'" type="success">
+            {{ t('affiliate.commissionModel.' + scope.row.commissionModel) }}
+          </el-tag>
+          <el-tag v-else type="primary">
+            {{ t('affiliate.commissionModel.' + scope.row.commissionModel) }}
+          </el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="commission" :label="t('fields.commission')" width="100">
+      <el-table-column
+        prop="commission"
+        :label="t('fields.commission')"
+        width="100"
+      >
         <template #default="scope">
           <span v-if="scope.row.commission === null">0.00 %</span>
-          <span v-if="scope.row.commission !== null">{{ (scope.row.commission * 100).toFixed(2) }} %</span>
+          <span v-if="scope.row.commission !== null">
+            {{ (scope.row.commission * 100).toFixed(2) }} %
+          </span>
         </template>
       </el-table-column>
-      <el-table-column prop="registerMemberCount" :label="t('fields.registerCount')" align="center" min-width="110" />
-      <el-table-column prop="totalRegisterMemberCount" :label="t('fields.totalRegisterCount')" align="center" min-width="140" />
-      <el-table-column prop="ftdMemberCount" :label="t('fields.ftdCount')" align="center" min-width="100" />
-      <el-table-column prop="depositAmount" :label="t('fields.depositAmount')" align="center" min-width="120">
+      <el-table-column
+        prop="registerMemberCount"
+        :label="t('fields.registerCount')"
+        align="center"
+        min-width="110"
+      />
+      <el-table-column
+        prop="totalRegisterMemberCount"
+        :label="t('fields.totalRegisterCount')"
+        align="center"
+        min-width="140"
+      />
+      <el-table-column
+        prop="ftdMemberCount"
+        :label="t('fields.ftdCount')"
+        align="center"
+        min-width="100"
+      />
+      <el-table-column
+        prop="depositAmount"
+        :label="t('fields.depositAmount')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.depositAmount, type: 'money'}" />
+          $
+          <span
+            v-formatter="{data: scope.row.depositAmount, type: 'money'}"
+          />
         </template>
       </el-table-column>
-      <el-table-column prop="depositMemberCount" :label="t('fields.depositMemberCount')" align="center" min-width="150">
+      <el-table-column
+        prop="depositMemberCount"
+        :label="t('fields.depositMemberCount')"
+        align="center"
+        min-width="150"
+      >
         <template #default="scope">
           <router-link
             :to="
@@ -149,30 +234,72 @@
           </router-link>
         </template>
       </el-table-column>
-      <el-table-column prop="withdrawAmount" :label="t('fields.withdrawAmount')" align="center" min-width="120">
+      <el-table-column
+        prop="withdrawAmount"
+        :label="t('fields.withdrawAmount')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.withdrawAmount, type: 'money'}" />
+          $
+          <span
+            v-formatter="{data: scope.row.withdrawAmount, type: 'money'}"
+          />
         </template>
       </el-table-column>
-      <el-table-column prop="withdrawMemberCount" :label="t('fields.withdrawMemberCount')" align="center" min-width="160" />
-      <el-table-column prop="bet" :label="t('fields.bet')" align="center" min-width="120">
+      <el-table-column
+        prop="withdrawMemberCount"
+        :label="t('fields.withdrawMemberCount')"
+        align="center"
+        min-width="160"
+      />
+      <el-table-column
+        prop="bet"
+        :label="t('fields.bet')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.bet, type: 'money'}" />
+          $
+          <span v-formatter="{data: scope.row.bet, type: 'money'}" />
         </template>
       </el-table-column>
-      <el-table-column prop="payout" :label="t('fields.payout')" align="center" min-width="120">
+      <el-table-column
+        prop="payout"
+        :label="t('fields.payout')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.payout, type: 'money'}" />
+          $
+          <span v-formatter="{data: scope.row.payout, type: 'money'}" />
         </template>
       </el-table-column>
-      <el-table-column prop="bonus" :label="t('fields.bonus')" align="center" min-width="120">
+      <el-table-column
+        prop="bonus"
+        :label="t('fields.bonus')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.bonus, type: 'money'}" />
+          $
+          <span v-formatter="{data: scope.row.bonus, type: 'money'}" />
         </template>
       </el-table-column>
-      <el-table-column prop="companyProfit" :label="t('fields.companyProfit')" align="center" min-width="120">
+      <el-table-column
+        prop="companyProfit"
+        :label="t('fields.companyProfit')"
+        align="center"
+        min-width="120"
+      >
         <template #default="scope">
-          $ <span v-formatter="{data: scope.row.bet - scope.row.payout, type: 'money'}" />
+          $
+          <span
+            v-formatter="{
+              data: scope.row.bet - scope.row.payout,
+              type: 'money',
+            }"
+          />
         </template>
       </el-table-column>
     </el-table>
@@ -190,13 +317,18 @@
 <script setup>
 import { computed, onMounted, reactive, ref } from 'vue'
 import moment from 'moment'
-import { getAffiliateChildReport, getAffiliateReport, getAffiliateReportExport, getAffiliateReportTotal } from '../../../api/report-affiliate'
+import {
+  getAffiliateChildReport,
+  getAffiliateReport,
+  getAffiliateReportExport,
+  getAffiliateReportTotal,
+} from '../../../api/report-affiliate'
 import { getSiteListSimple } from '../../../api/site'
 import { useStore } from '../../../store'
 import { TENANT } from '../../../store/modules/user/action-types'
 import { useI18n } from 'vue-i18n'
 import { hasPermission } from '../../../utils/util'
-import { getShortcuts } from "@/utils/datetime";
+import { getShortcuts } from '@/utils/datetime'
 
 const { t } = useI18n()
 const startDate = new Date()
@@ -212,17 +344,29 @@ const siteList = reactive({
 const uiControl = reactive({
   messageVisible: false,
   affiliateLevel: [
-    { key: 1, displayName: t('affiliate.level.AFFILIATE'), value: "AFFILIATE" },
-    { key: 2, displayName: t('affiliate.level.SUPER_AFFILIATE'), value: "SUPER_AFFILIATE" },
-    { key: 3, displayName: t('affiliate.level.MASTER_AFFILIATE'), value: "MASTER_AFFILIATE" },
-    { key: 4, displayName: t('affiliate.level.CHIEF_AFFILIATE'), value: "CHIEF_AFFILIATE" }
+    { key: 1, displayName: t('affiliate.level.AFFILIATE'), value: 'AFFILIATE' },
+    {
+      key: 2,
+      displayName: t('affiliate.level.SUPER_AFFILIATE'),
+      value: 'SUPER_AFFILIATE',
+    },
+    {
+      key: 3,
+      displayName: t('affiliate.level.MASTER_AFFILIATE'),
+      value: 'MASTER_AFFILIATE',
+    },
+    {
+      key: 4,
+      displayName: t('affiliate.level.CHIEF_AFFILIATE'),
+      value: 'CHIEF_AFFILIATE',
+    },
   ],
   colors: [
     { color: '#f56c6c', percentage: 30 },
     { color: '#e6a23c', percentage: 70 },
-    { color: '#5cb87a', percentage: 100 }
-  ]
-});
+    { color: '#5cb87a', percentage: 100 },
+  ],
+})
 
 const page = reactive({
   pages: 0,
@@ -238,24 +382,30 @@ const request = reactive({
   recordTime: [defaultDate, defaultDate],
   siteId: null,
   affiliateCode: null,
-  affiliateLevel: null
+  affiliateLevel: null,
 })
 
-const shortcuts = getShortcuts(t);
+const shortcuts = getShortcuts(t)
 function convertDate(date) {
   return moment(date).format('YYYY-MM-DD')
 }
 
 function disabledDate(time) {
-  return time.getTime() < moment(new Date()).subtract(2, 'months').startOf('month').format('x') || time.getTime() > new Date().getTime();
+  return (
+    time.getTime() <
+      moment(new Date())
+        .subtract(2, 'months')
+        .startOf('month')
+        .format('x') || time.getTime() > new Date().getTime()
+  )
 }
 
 function resetQuery() {
-  request.loginName = null;
-  request.affiliateCode = null;
-  request.affiliateLevel = null;
-  request.recordTime = [defaultDate, defaultDate];
-  request.siteId = site.value ? site.value.id : siteList.list[0].id;
+  request.loginName = null
+  request.affiliateCode = null
+  request.affiliateLevel = null
+  request.recordTime = [defaultDate, defaultDate]
+  request.siteId = site.value ? site.value.id : siteList.list[0].id
 }
 
 function checkQuery() {
@@ -271,12 +421,12 @@ function checkQuery() {
       query.recordTime = request.recordTime.join(',')
     }
   }
-  return query;
+  return query
 }
 
 async function loadAffiliateReport(withTotal) {
   page.loading = true
-  const query = checkQuery();
+  const query = checkQuery()
   const { data: ret } = await getAffiliateReport(query)
   page.pages = ret.pages
   page.records = ret.records
@@ -288,25 +438,25 @@ async function loadAffiliateReport(withTotal) {
 
 async function loadAffiliateReportTotal(query) {
   const { data: total } = await getAffiliateReportTotal(query)
-  page.total = total;
+  page.total = total
 }
 
 async function load(tree, treeNode, resolve) {
-  const query = {};
-  query.recordTime = tree.recordTime;
-  query.superiorAffiliateId = tree.affiliateId;
-  query.siteId = request.siteId;
-  const { data: children } = await getAffiliateChildReport(query);
-  resolve(children);
+  const query = {}
+  query.recordTime = tree.recordTime
+  query.superiorAffiliateId = tree.affiliateId
+  query.siteId = request.siteId
+  const { data: children } = await getAffiliateChildReport(query)
+  resolve(children)
 }
 
 async function requestExportExcel() {
-  const query = checkQuery();
-  query.requestBy = store.state.user.name;
-  query.requestTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-  const { data: ret } = await getAffiliateReportExport(query);
+  const query = checkQuery()
+  query.requestBy = store.state.user.name
+  query.requestTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+  const { data: ret } = await getAffiliateReportExport(query)
   if (ret) {
-    uiControl.messageVisible = true;
+    uiControl.messageVisible = true
   }
 }
 
@@ -321,7 +471,7 @@ function changePage(page) {
 }
 
 function populateReportTotal(param) {
-  const { columns } = param;
+  const { columns } = param
   const total = []
   columns.forEach((column, index) => {
     if (index === 0) {
@@ -333,9 +483,9 @@ function populateReportTotal(param) {
     Object.entries(totalCopy).forEach(([key, value]) => {
       if (key === column.property && value !== null) {
         if (key === 'depositAmount' || key === 'withdrawAmount') {
-          total[index] = '$' + value.toFixed(2);
+          total[index] = '$' + value.toFixed(2)
         } else {
-          total[index] = value;
+          total[index] = value
         }
       }
     })
@@ -347,8 +497,10 @@ onMounted(async () => {
   await loadSites()
   request.siteId = siteList.list[0].id
   if (LOGIN_USER_TYPE.value === TENANT.value) {
-    site.value = siteList.list.find(s => s.siteName === store.state.user.siteName);
-    request.siteId = site.value.id;
+    site.value = siteList.list.find(
+      s => s.siteName === store.state.user.siteName
+    )
+    request.siteId = site.value.id
   }
   await loadAffiliateReport(true)
 })
