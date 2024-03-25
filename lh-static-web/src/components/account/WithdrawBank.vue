863bef062a64c1f6522e9bfbc54403358105c312
Mika <mika@eagrp.net>
2024-03-25 15:27:14 +0800

    feat: [LH,XF,DY] update SZPAY

--- lh-static-web/src/components/account/WithdrawBank.vue
+++ lh-static-web/src/components/account/WithdrawBank.vue
@@ -320,6 +320,9 @@ export default defineComponent({
         } else if (selectedCode === "OKPAY") {
           min = 16;
           max = 16;
+        } else if (selectedCode === "SZPAY") {
+          min = 11;
+          max = 11;
         }
       }
       if (v === "") {
--- xf-static-h5/src/pages/account/WithdrawBankView.vue
+++ xf-static-h5/src/pages/account/WithdrawBankView.vue
@@ -793,6 +793,8 @@ export default defineComponent({
           return (val.length > 33 && val.length < 35) || '长度应为34个字符'
         } else if(selectedCode === 'OKPAY') {
           return (val.length > 15 && val.length < 17) || '长度应为16个字符'
+        } else if(selectedCode === 'SZPAY') {
+          return (val.length > 10 && val.length < 12) || '长度应为11个字符'
         }
       } else {
         var selectedBankCode = null
