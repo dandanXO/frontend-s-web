779e6bc4eada070228589213ede26a6e17d1360b
Kai Wen <kaiwen@caspen-tech.com>
2024-04-09 15:27:46 +0800

    fix: [ind] Replace 55Ace text to 789F

--- ind-static-v2/quasar.config.js
+++ ind-static-v2/quasar.config.js
@@ -172,8 +172,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `55Ace`,
-        short_name: `55Ace`,
+        name: `789F`,
+        short_name: `789F`,
         description: `APP`,
         display: "standalone",
         orientation: "portrait",
@@ -218,7 +218,7 @@ module.exports = configure(function (ctx) {
     capacitor: {
       hideSplashscreen: true,
       // (Optional) If not present, will look for package.json > name
-      appName: "55Ace", // string
+      appName: "789F", // string
       backButtonExit: "*"
     },
 
