f76cc28b6620077d454e893dc54c1a698adb1b41
WeiSeng <weiseng@eagrp.net>
2024-04-11 10:53:42 +0800

    feat: VNM Web fix 0411

--- vnm-static-web/src/api/platform/platform.js
+++ vnm-static-web/src/api/platform/platform.js
@@ -40,11 +40,19 @@ export function launchSessionGame(
   { gameCode = null, isMobile = false } = {}
 ) {
   var way = null;
+  var gameheader= {};
+  if(platform === 'PG'){
+    gameheader =  {
+      'Cache-Control': 'no-cache, no-store, must-revalidate'
+    }
+  }
+
   if (getDevice() === "MOBILE") {
     way = getMobileOS();
   }
   return server.REST.get(`/session/launch?_time=${new Date().getTime()}`, {
-    params: { platform, gameCode, isMobile, way }
+    params: { platform, gameCode, isMobile, way },
+    headers: gameheader
   });
 }
 
