1ab25f2af3cd3eda65d340ab564d3ec490b75dde
WeiSeng <weiseng@eagrp.net>
2024-04-17 08:36:14 +0800

    feat: Vnm H5 fix USDT estimate amount

--- vnm-static-h5/src/pages/finance/WithdrawView.vue
+++ vnm-static-h5/src/pages/finance/WithdrawView.vue
@@ -191,10 +191,12 @@
                 {{ store.currency.value }}
               </span>
             </div>
-            <div class="q-mt-sm" style="display: flex; justify-content: center; align-items: center">
+            <div class="q-mt-sm" style="display: flex; justify-content: center; align-items: center; color: #17cd27;">
               <span style="flex: 1">{{ $t("lang.withdraw_estimatedarrival") }}：</span>
               <span style="flex: 3" class="bg-neontb text-neontb q-pa-sm">
-                {{ (withdrawInfo.amount / selectedWithdrawalMethod.exchangeRate).toFixed(2) }}
+                {{  selectedWithdrawalMethod && (withdrawInfo.amount < selectedWithdrawalMethod.withdrawMin || (withdrawInfo.amount / selectedWithdrawalMethod.exchangeRate - 1).toFixed(2) < 0)
+                ? "0.00"
+                : (withdrawInfo.amount / selectedWithdrawalMethod.exchangeRate - 1).toFixed(2) }}
                 USDT
               </span>
             </div>
