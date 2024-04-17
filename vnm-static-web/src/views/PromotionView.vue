685bf4e6efdf2c1121d53bc06d2a73c610a105d5
Keng Hong <kenghongho@eagrp.net>
2024-04-17 09:46:48 +0800

    feat: [vnm h5 / web] update promo label

--- vnm-static-web/src/views/PromotionView.vue
+++ vnm-static-web/src/views/PromotionView.vue
@@ -270,15 +270,15 @@ export default defineComponent({
     const getPromoLabel = (labelType) => {
   switch (labelType) {
     case 0:
-      return "NEW Mới nhất";
+      return "Mới nhất";
     case 1:
-      return "HOT Phổ biến";
+      return "Hot";
     case 3:
-      return "RECOMMEND Đề xuất";
+      return "Đề xuất";
     case 4:
-      return "DAILY Hàng ngày";
+      return "Hàng ngày";
     case 5:
-      return "NEWBIE Mới";
+      return "Thành viên mới";
     case 6:
       return "TIME Thời gian giới hạn";
     default:
