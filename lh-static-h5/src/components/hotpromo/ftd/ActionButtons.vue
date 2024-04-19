8a3aee6c54a58f33150eda47cb6ad2188328324e
Kai Wen <kaiwen@caspen-tech.com>
2024-04-19 17:38:14 +0800

    fix: [lh h5] Remove 领取奖金 button, when redirect to Deposit page pass query param for default privilege selection

--- lh-static-h5/src/components/hotpromo/ftd/ActionButtons.vue
+++ lh-static-h5/src/components/hotpromo/ftd/ActionButtons.vue
@@ -3,72 +3,50 @@
         <div class="action-button" @click="goToDeposit">
             点击{{ props.tab }}
         </div>
-        <div class="action-button" @click="claimBonus">
-            领取奖金
-        </div>
     </div>
 </template>
 
 <script setup>
-import { eventapi } from "boot/axios";
 import './styles.css';
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
 
-const $q = useQuasar();
-const qs = require("qs");
 const router = useRouter();
 
 const goToDeposit = () => {
+    const promoCode = getPromoCode();
+
   if(window.location.pathname === "/promotion"){
-    document.location.href = "app://deposit";
+    document.location.href = `app://deposit?privilegeCode=${promoCode}`;
   }else{
-    router.push("/finance/deposit");
+    router.push(`/finance/deposit?privilegeCode=${promoCode}`);
   }
 }
 
-const claimBonus = () => {
-    const params = {}
-
+const getPromoCode = () => {
     if(props.type === '体育') {
-        params.promoCode = 'lh1-sport-ftd';
+        return 'lh1-sport-ftd';
     } else if(props.type === '电竞') {
-        params.promoCode = 'lh1-esport-ftd';
+        return 'lh1-esport-ftd';
     } else if(props.type === '真人' && props.tab === '首存') {
-        params.promoCode = 'lh1-live-ftd';
+        return 'lh1-live-ftd';
     } else if(props.type === '真人' && props.tab === '复存') {
-        params.promoCode = 'lh1-live-ftd-2';
+        return 'lh1-live-ftd-2';
     } else if(props.type === '电子' && props.tab === '首存') {
-        params.promoCode = 'lh1-slot-ftd';
+        return 'lh1-slot-ftd';
     } else if(props.type === '电子' && props.tab === '复存') {
-        params.promoCode = 'lh1-slot-ftd-2';
+        return 'lh1-slot-ftd-2';
     } else if(props.type === '棋牌' && props.tab === '首存') {
-        params.promoCode = 'lh1-poker-ftd';
+        return 'lh1-poker-ftd';
     } else if(props.type === '棋牌' && props.tab === '复存') {
-        params.promoCode = 'lh1-poker-ftd-2';
+        return 'lh1-poker-ftd-2';
     } else if(props.type === '捕鱼' && props.tab === '首存') {
-        params.promoCode = 'lh1-fishing-ftd';
+        return 'lh1-fishing-ftd';
     } else if(props.type === '捕鱼' && props.tab === '复存') {
-        params.promoCode = 'lh1-fishing-ftd-2';
+        return 'lh1-fishing-ftd-2';
     }
 
-    if(params.promoCode) {
-        eventapi.post('/first-deposit/claim',
-            qs.stringify({
-                promoCode: params.promoCode
-            })
-        ).then((res) => {
-            const { code, data } = res;
-            if (code === 0) {
-                $q.notify({
-                    message: data,
-                    type: "positive",
-                    position: "top",
-                    icon: "check_circle_outline"
-                });
-            }
-        });
-    }
+    return '';
 }
 
 const props = defineProps(['tab', 'type']);
