95106af0534373ec932a23308b51b5de6a36082f
WeiSeng <weiseng@eagrp.net>
2024-04-24 16:37:43 +0800

    feat: MANAGER change height

--- tf-manager-web/src/views/member-withdraw-record/withdraw-review/index.vue
+++ tf-manager-web/src/views/member-withdraw-record/withdraw-review/index.vue
@@ -189,7 +189,7 @@
       size="small"
       highlight-current-row
       v-loading="page.loading"
-      height="500"
+      height="calc(100vh - 210px)"
       :header-cell-style="{background: 'lightgray'}"
       :empty-text="t('fields.noData')"
       :summary-method="getSummaries"
