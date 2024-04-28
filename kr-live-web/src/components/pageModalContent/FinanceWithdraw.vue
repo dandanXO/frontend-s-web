a5a1a414ddf21b277cb84a2ba9e2f0e8fa17e4c0
Eudora-hsj <eudora.hsj@gmail.com>
2024-04-29 07:28:48 +0800

    feat. dialogPages(deposit/withdraw/notify/service) - withdraw

--- kr-live-web/src/components/pageModalContent/FinanceWithdraw.vue
+++ kr-live-web/src/components/pageModalContent/FinanceWithdraw.vue
@@ -1,6 +1,24 @@
 <template>
 <div class="modal-body-wrap">
   <q-card-section class="modal-body-content">
+    <form class="content-form">
+      <p>
+        <label>보유금액</label>
+        <input />
+      </p>
+      <p>
+        <label>환전암호</label>
+        <input />
+      </p>
+      <p>
+        <label>출금금액</label>
+        <input />
+      </p>
+      <p class="option-btns">
+        <q-btn v-for="(item, index) in countOptions" :key="index" size="md" class="" :label="item + '원'"></q-btn>
+        <q-btn size="md" class="active" label="정정하기"></q-btn>
+        </p>
+    </form>
   </q-card-section>
   <q-card-actions  class="modal-body-buttons" align="center">
     <q-btn class="form-button blue" label="입금하기"></q-btn>
@@ -9,6 +27,11 @@
 </template>
 
 <script setup id="FinanceWithdraw">
+
+import { ref } from "vue";
+
+const countOptions = ref([1,5,10,50,100]);
+
 </script>
 
 <style lang="scss" scoped>
@@ -42,4 +65,52 @@
     }
   }
 }
+
+.modal-body-content {
+  .content-form {
+    p {
+      margin-top: 20px;
+
+    }
+    label {
+      margin-bottom: 10px;
+      display: block;
+      font-size: 14px;
+      color: #fff;
+
+    }
+    label, input {
+      width: 100%;
+    }
+    input {
+      font-size: 14px;
+      border-radius: 3px;
+      border: 1px solid #5C5C5C;
+      line-height: 40px;
+      color: #fff;
+      background: #212121;
+      padding: 5px 15px;
+    }
+
+    .option-btns {
+      :deep(.q-btn) {
+        width: 100px;
+        height: 40px;
+        margin-right: 15px;
+        color: #fff;
+        font-size: 14px;
+        border-radius: 3px;
+        padding: 5px 15px;
+        margin-top: 20px;
+        background: #18324A;
+        &.active {
+          background: #237BFF;
+        }
+      }
+    }
+  }
+
+}
+
+
 </style>
