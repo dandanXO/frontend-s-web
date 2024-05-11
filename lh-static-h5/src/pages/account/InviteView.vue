2b2a2c987af4bfdd24308ded46dd8eb0c983b419
cody <code@eagrp.net>
2024-05-11 12:26:44 +0800

    唤醒链接开放给正式

--- lh-static-h5/src/pages/account/InviteView.vue
+++ lh-static-h5/src/pages/account/InviteView.vue
@@ -67,7 +67,7 @@
       </div>
     </div>
 
-    <div class="personal-content-box" v-if="store.memberType==='TEST' || store.memberType==='PROMO_TEST'">
+    <div class="personal-content-box">
       <div class="shadow-box" id="summon-share">
         <div class="qr-title">唤醒分享</div>
         <div class="title-top-line1">
