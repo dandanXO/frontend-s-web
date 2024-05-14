7caf8679d6e6c6abb67128e9d5df0c9d0b999b7d
Mika <mika@eagrp.net>
2024-05-14 17:47:55 +0800

    feat: [tf manager web] update kr

--- tf-manager-web/src/layout/components/navigation_bar/Index.vue
+++ tf-manager-web/src/layout/components/navigation_bar/Index.vue
@@ -36,6 +36,7 @@
         <el-option key="1" value="en">en</el-option>
         <el-option key="2" value="zh">zh</el-option>
         <el-option key="3" value="th">th</el-option>
+        <el-option key="4" value="kr">kr</el-option>
       </el-select>
       <el-dropdown
         class="avatar-container right-menu-item hover-effect"
