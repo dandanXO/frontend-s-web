0eabd5677e9cc71c55d978d4493af19f1351d26c
Mika <mika@eagrp.net>
2024-05-20 20:17:03 +0800

    feat: [lh xf dy] update BLB pay

--- xf-static-web/src/views/account/WithdrawBankView.vue
+++ xf-static-web/src/views/account/WithdrawBankView.vue
@@ -334,6 +334,12 @@ export default defineComponent({
         } else if (selectedCode === 'OKPAY') {
           min = 16;
           max = 16;
+        } else if(selectedCode === 'BLBPAY') {
+          min = 32;
+          max = 32;
+        } else if(selectedCode === 'JDPAY') {
+          min = 34;
+          max = 34;
         } else if (selectedCode === 'SZPAY') {
           // if (!/^\d+$/.test(v)) {
           //   return Promise.reject('请输入数字人民币使用的手机号');
