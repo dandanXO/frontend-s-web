01f38bfac55d43c426a03d40acb306a36f6121ae
Kai Wen <kaiwen@caspen-tech.com>
2024-05-21 11:19:08 +0800

    fix: [kr] Separate GameCategory into its own component

---
+++ kr-live-web/src/components/game/GameCategory.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="category-list">
+        <div :class="`category ${category.name} ${props.selectedCategory === category.name ? 'active' : ''}`" v-for="(category, categoryIndex) in categoryList" :key="categoryIndex" 
+            @click="() => props.onClickGameCategory(category.name, categoryIndex)">
+            <div class="category-labels">
+                <div class="category-kr-label">{{ category.label }}</div>
+                <div class="category-en-label">{{ category.enLabel }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const categoryList = [
+    { name: "live", label: "라이브카지노", enLabel: "LIVE CASINO" },
+    { name: "slots", label: "슬롯게임", enLabel: "SLOT GAME" },
+    { name: "esport", label: "E-스포츠", enLabel: "ESPORTS" },
+    { name: "sport", label: "스포츠", enLabel: "SPORTS" },
+    { name: "casual", label: "미니게임", enLabel: "MINI GAME" }
+    // { name: "fish", label: "낚시 게임" }
+];
+
+const props = defineProps(['onClickGameCategory', 'selectedCategory']);
+</script>
+
+<style lang="scss" scoped>
+.category-list {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+    justify-content: center;
+}
+.category {
+    margin: 5px;
+    filter: grayscale(1);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 220px;
+    height: 80px;
+    white-space: nowrap;
+    cursor: pointer;
+    transition: background 0.3s ease-in-out;
+    overflow: hidden;
+    border: 1px solid #5dd8ff;
+    transform: skewX(-16deg);
+    border-radius: 10px;
+    position: relative;
+
+    .category-labels {
+        position: absolute;
+        top: 50%;
+        left: 55%;
+        transform: translate(-50%, -50%) skewX(16deg);
+        width: 200px;
+        overflow: auto;
+
+        .category-kr-label {
+            color: #01d9ab;
+            font-size: 24px;
+            line-height: 35px;
+            font-weight: bold;
+        }
+
+        .category-en-label {
+            font-size: 10px;
+            color: #fff;
+        }
+    }
+
+    &.slots {
+        background: url('../../assets/home/menu/menu-slots.png') no-repeat center center;
+        background-size: 120% 120%;
+    }
+
+    &.live {
+        background: url('../../assets/home/menu/menu-live.png') no-repeat center center;
+        background-size: 120% 120%;
+    }
+
+    &.esport {
+        background: url('../../assets/home/menu/menu-esport.png') no-repeat center center;
+        background-size: 120% 120%;
+    }
+
+    &.sport {
+        background: url('../../assets/home/menu/menu-sport.png') no-repeat center center;
+        background-size: 120% 120%;
+    }
+
+    &.casual {
+        background: url('../../assets/home/menu/menu-casual.png') no-repeat center center;
+        background-size: 120% 120%;
+    }
+
+    &:hover, &.active {
+        filter: grayscale(0);
+        background-size: 130% 130%;
+    }
+}
+</style>
