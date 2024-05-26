4ef6cfae8e467578bd794e91cd2fc030c7b5e283
dan <dan@gmail.com>
2024-05-26 17:04:33 +0800

    feat: lh-lpl-summer24 H5

--- lh-static-web/src/components/HotPromotion.vue
+++ lh-static-web/src/components/HotPromotion.vue
@@ -122,7 +122,7 @@ import UpgradeHongBao from "../components/hotpromo/upgradeHongBao/UpgradeHongBao
 import HongBaoYu2024 from "../components/hotpromo/hongbaoyu2024/HongBaoYu2024.vue";
 import AsianCup2024 from "../components/hotpromo/asian-cup-2024/AsianCup2024.vue";
 import BasketballHot from "../components/hotpromo/basketball-hot/BasketballHot.vue";
-import LPLSummer from "../components/hotpromo/lpl-summer/LPLSummer.vue";
+import LPLSummer from "../components/hotpromo/lpl-summer/LPLSummer.vue"; 
 import Cny2024Promo from "../components/hotpromo/cny2024/Cny2024Promo.vue";
 import BbDacha2024Promo from "../components/hotpromo/bbdacha2024/BbDacha2024Promo.vue";
 import CnyStepGame2024Promo from "../components/hotpromo/cnystepgame2024/CnyStepGame2024Promo.vue";
@@ -144,7 +144,7 @@ import { ElMessage } from "element-plus";
 import { userStore } from "@/store";
 import moment from "moment";
 
-export default defineComponent({
+export default defineComponent({     
   name: "HotPromo",
   order: 1,
   // setup: (props, { emit }) => {},
