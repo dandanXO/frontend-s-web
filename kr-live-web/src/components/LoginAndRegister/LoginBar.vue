ff85f6f1f3f2934bb820b897f5d5db0464ce3696
Kai Wen <kaiwen@caspen-tech.com>
2024-05-27 16:11:06 +0800

    fix: [kr] Add unlink button for Bank Card List

--- kr-live-web/src/components/LoginAndRegister/LoginBar.vue
+++ kr-live-web/src/components/LoginAndRegister/LoginBar.vue
@@ -43,7 +43,7 @@
                 <img :src="require('../../assets/icon/sidebar-icon-withdraw.svg')" alt="" />
                 <div class="info-text">출금</div>
               </router-link>
-              <div
+              <!-- <div
                 class="sidebar-section-item"
               >
                 <img :src="require('../../assets/icon/sidebar-icon-1.svg')" alt="" />
@@ -54,7 +54,7 @@
               >
                 <img :src="require('../../assets/icon/sidebar-icon-2.svg')" alt="" />
                 <div class="info-text">알전환</div>
-              </div>
+              </div> -->
             </div>
             <div class="separator"/>
             <div class="icon-section-label">메뉴</div>
