7df8aabbfb55faa7be211d7b488424be7ea5f2e6
karchuan <karchuan@caspen-tech.com>
2024-05-28 15:02:02 +0800

    retrieve bet ratio for affiliate profile info

--- xf-lh-dy-affiliate-web/src/views/personal/index.vue
+++ xf-lh-dy-affiliate-web/src/views/personal/index.vue
@@ -253,8 +253,7 @@
                 {{ affInfo.affiliateCode }}
               </el-form-item>
             </el-row>
-
-            <el-row class="info" v-if="store.state.user.siteCode !== 'KRW'">
+            <el-row class="info">
               <el-icon color="#7D8792">
                 <Icon :icon="barCodeScanner20Filled" class="stats-icon" />
               </el-icon>
@@ -263,6 +262,19 @@
                 label="Platform Commission"
               >
                 {{ affInfo.revenueShare }}
+              </el-form-item>
+            </el-row>
+            <el-row class="info" v-if="affInfo.shareRatio !== null && affInfo.shareRatio.length > 0">
+              <el-icon color="#7D8792">
+                <Icon :icon="documentPercent20Filled" class="stats-icon" />
+              </el-icon>
+              <el-form-item
+                style="display: flex"
+                :label="t('fields.shareRatio')"
+              >
+                <div v-for="item in affInfo.shareRatio" :key="item.code">
+                  <el-row>{{ t('affiliateShareRatio.' + item.code) }} : {{ item.value }}</el-row>
+                </div>
               </el-form-item>
             </el-row>
           </el-form>
@@ -679,6 +691,7 @@ const affInfo = reactive({
   downlineMember: 0,
   commission: 0,
   revenueShare: 0,
+  shareRatio: [],
 })
 
 const eForm = reactive({
