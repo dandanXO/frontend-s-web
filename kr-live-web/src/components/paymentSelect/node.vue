5ca6ee025a619e20af48b09c6ee68a901650b09a
Kai Wen <kaiwen@caspen-tech.com>
2024-05-29 17:23:15 +0800

    fix: [kr] Improve Deposit form ui

--- kr-live-web/src/components/paymentSelect/node.vue
+++ kr-live-web/src/components/paymentSelect/node.vue
@@ -278,7 +278,7 @@ $node-color: #dd4645;
   .payment-method-item {
     text-align: center;
     border-radius: 6px;
-    border: solid 1px #484460;
+    border: 2px solid #4b4b4b;
     color: #ffffff;
     cursor: pointer;
     padding: 20px 35px;
@@ -336,7 +336,7 @@ $node-color: #dd4645;
       text-align: center;
       padding: 10px 8px;
       cursor: pointer;
-      background: #2b2b4b;
+      background: #252e43;
       box-shadow: 6px 6px #161b23;
 
       &:hover {
