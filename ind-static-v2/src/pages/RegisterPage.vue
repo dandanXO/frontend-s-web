057b3d455c7e4a6a790a9ce2f43032c30d16778d
Mika <mika@eagrp.net>
2024-05-30 18:03:15 +0800

    fix: [indv2] update

--- ind-static-v2/src/pages/RegisterPage.vue
+++ ind-static-v2/src/pages/RegisterPage.vue
@@ -43,7 +43,7 @@
                   v-model="regForm.password"
                   lazy-rules
                   :type="isPwd ? 'password' : 'text'"
-                  :rules="[(val) => (val.length >= 6 && val.length <= 11) || 'The characters of password must be between 6 and 11']"
+                  :rules="[(val) => (val.length >= 6) || 'The characters of password must be above 6']"
                   color="white"
                   outlined
                   label-color="brand"
