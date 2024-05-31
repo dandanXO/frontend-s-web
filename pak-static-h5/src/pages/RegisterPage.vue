41ff739a9f5ff6e5e5bbfd7bf2917c65ae112ed0
Keng Hong <kenghongho@eagrp.net>
2024-05-31 17:42:18 +0800

    feat: [pak h5] register phone number validation

--- pak-static-h5/src/pages/RegisterPage.vue
+++ pak-static-h5/src/pages/RegisterPage.vue
@@ -32,8 +32,7 @@
                   v-model="regForm.loginName"
                   :rules="[
                     (val) => (val && val.length > 0) || 'Please insert Phone number',
-                    (val) => (val && val.length === 11) || 'The phone number must have 11 digits',
-                    (val) => val.startsWith('03') || 'The phone number must start with \'03\''
+                    (val) => (val && val.length >= 10 && val.length <= 11 ) || 'The phone number must have 10 to 11 digits'
                   ]"
                   color="green"
                   outlined
