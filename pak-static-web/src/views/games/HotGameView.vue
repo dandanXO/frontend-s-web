34732ef9ab6316723cd258eca1bdb99437e65d8f
Sam <sam@flex-df.com>
2024-06-07 18:00:29 +0800

    feat: [pak web] Change hot game page api

--- pak-static-web/src/views/games/HotGameView.vue
+++ pak-static-web/src/views/games/HotGameView.vue
@@ -12,7 +12,7 @@
             <div class="game-slot animate__animated animate__fadeInRight" v-for="game in games" :key="game.id">
               <a @click="openGame(game, game.platform, game.code)">
                 <div class="slot-img">
-                  <el-image :src="loadGameIcon(`${game.code.toLowerCase()}.png`)" lazy>
+                  <el-image :src="loadGameIcon(game.icon)" lazy>
                     <template #placeholder>
                       <img src="@/assets/images/platform/game/default.png" />
                     </template>
@@ -36,21 +36,24 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import GameModal from "@/components/modal/GameModal";
-import { getHotGames } from "@/api/platform/platform";
+import { getPlatformHotGames } from "@/api/platform/platform";
+
+const imgGamesURL = process.env.VUE_APP_IMAGE_CDN + "/game/";
 
 const games = ref([]);
 const platformGame = ref(null);
 
 const loadGameIcon = (path) => {
   try {
-    return require(`@/assets/images/platform/game/hot/${path}`);
+    return `${imgGamesURL}${path}`;
   } catch (e) {
     return `${require("@/assets/images/platform/game/default.png")}`;
   }
 };
 
 const loadGames = () => {
-  getHotGames().then((res) => (games.value = res));
+  // getHotGames().then((res) => (games.value = res));
+  getPlatformHotGames().then((res) => (games.value = res));
 };
 
 const openGame = (item, platformCode, gameCode) => {
