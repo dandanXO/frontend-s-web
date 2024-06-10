3fd5863ae4f58933ff892ca22df3fdd16e73d0b8
WeiSeng <weiseng@eagrp.net>
2024-06-10 14:45:46 +0800

    feat: LH add lh869 to global

--- lh-static-web/src/utils/request.js
+++ lh-static-web/src/utils/request.js
@@ -11,7 +11,7 @@ const evtArray = process.env.VUE_APP_EVT_API.split(",");
 const crArray = process.env.VUE_APP_CR_API.split(",");
 
 console.log(window.location.hostname);
-const globalLinks = ["lh318", "lh165", "lh765", "lh730", "lh971", "lh835"];
+const globalLinks = ["lh318", "lh165", "lh765", "lh730", "lh971", "lh835", "lh869"];
 const isGlobalLH = globalLinks.some((link) => window.location.hostname.includes(link));
 
 const specialLinks= ["lh93371", "lh76390", "lh30553", "lh13179", "lh36909", "lh97969", "lh09903", "lh97100"];
