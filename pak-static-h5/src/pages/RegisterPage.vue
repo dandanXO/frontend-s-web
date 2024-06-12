da75f48679ea7eb74d2268afb9f2a2f6758e32aa
WeiSeng <weiseng@eagrp.net>
2024-06-12 18:46:31 +0800

    feat: PAK h5 rm adjust package

--- pak-static-h5/src/pages/RegisterPage.vue
+++ pak-static-h5/src/pages/RegisterPage.vue
@@ -244,8 +244,6 @@ import { useRoute, useRouter } from "vue-router";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 import { userStore } from "stores/index";
 import qs from "qs";
-import { Adjust, AdjustEvent } from "@awesome-cordova-plugins/adjust";
-import AdjustWeb from "@adjustcom/adjust-web-sdk";
 // import PrimaryButton from "../components/auth/PrimaryButton.vue";
 import InputField from "../components/auth/InputField.vue";
 import InputRowGrid from "../components/auth/InputRowGrid.vue";
