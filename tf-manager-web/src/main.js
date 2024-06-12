bd160da2ab333f42d9a1408076b95b9ae2faa548
WeiSeng <weiseng@eagrp.net>
2024-06-12 14:18:00 +0800

    feat: AFFILIATE/MANAGER favicon title

--- tf-manager-web/src/main.js
+++ tf-manager-web/src/main.js
@@ -129,8 +129,8 @@ if (siteId === 'dy') {
   link.href = '/favicon.ico';
   title.innerText = 'TF88';
 } else if (siteId === 'krw') {
-  link.href = '/favicon.ico';
-  title.innerText = 'Bet2Won';
+  link.href = '/kr-favicon.ico';
+  title.innerText = 'City8';
 } else if (siteId === 'iw2') {
   link.href = '/iw2-favicon.ico';
   title.innerText = '789F';
