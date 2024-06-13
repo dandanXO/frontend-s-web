77638f7e36ee96b0062d4737ced8f548e5fd74df
Mika <mika@eagrp.net>
2024-06-13 15:57:23 +0800

    fix: [lhdyxf aff] fix page and permission

--- xf-lh-dy-affiliate-web/src/views/Forbidden.vue
+++ xf-lh-dy-affiliate-web/src/views/Forbidden.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <h1>403 - Forbidden</h1>
-    <p>You do not have permission to access this page.</p>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'Forbidden'
-}
-</script>
-<style scoped>
-  h1 {
-    color: red;
+    <div class="forbidden-page">
+      <h1>403 - Forbidden</h1>
+      <p>You don't have permission to access this page.</p>
+      <router-link to="/">Go to Home</router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Forbidden'
   }
-</style>
+  </script>
+  
+  <style scoped>
+  .forbidden-page {
+    text-align: center;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  
+  .forbidden-page h1 {
+    font-size: 3em;
+    margin-bottom: 20px;
+  }
+  
+  .forbidden-page p {
+    font-size: 1.5em;
+    margin-bottom: 20px;
+  }
+  
+  .forbidden-page a {
+    text-decoration: none;
+    color: #42b983;
+    font-weight: bold;
+  }
+  </style>
