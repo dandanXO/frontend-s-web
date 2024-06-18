f6e1408e8f2d1fa9af48e4bdd2552a7d8b72e3e0
WeiSeng <weiseng@eagrp.net>
2024-06-18 19:07:11 +0800

    feat:LH Modal css

--- lh-static-web/src/components/home/HomeBanner.vue
+++ lh-static-web/src/components/home/HomeBanner.vue
@@ -127,6 +127,8 @@ const homePopupType = ref("");
 const homePopupId = ref(0);
 
 const clickHomePopupImg = (urlString)=>{
+  isImportantAnnoucementModal.value= false;
+
   const openPattern = /^\/open\/(.*)/;
   if (urlString.match(openPattern)) {
     const extractedUrl = urlString.match(openPattern)[1];
