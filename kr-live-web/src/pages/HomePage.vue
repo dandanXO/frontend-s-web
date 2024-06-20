40ae8ed466cac9e63962511e04487daa01759c4d
Mika <mika@eagrp.net>
2024-06-20 19:51:46 +0800

    fix; [kr web ] link

--- kr-live-web/src/pages/HomePage.vue
+++ kr-live-web/src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <LangToggle />
 
     <q-page-sticky position="bottom-right" :offset="[50, 150]" style="z-index:999999">
-      <q-btn padding="none" round  color="primary" style="display: flex; justify-content: center; align-items: center;"><router-link to="https://t.me/city88888"><img style="display:block; width: 60px;" src="../assets/images/index/telegram.svg"></router-link></q-btn>
+      <q-btn padding="none" round  color="primary" style="display: flex; justify-content: center; align-items: center;"><a href="https://t.me/city88888" target="_blank"><img style="display:block; width: 60px;" src="../assets/images/index/telegram.svg"></a></q-btn>
     </q-page-sticky>
     <RollingText />
 
