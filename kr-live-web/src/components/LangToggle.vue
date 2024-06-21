4bfa1bbd8cda0b7c53c5fdd63f5c2b9fb7b0a717
Kai Wen <kaiwen@caspen-tech.com>
2024-06-21 16:30:25 +0800

    fix: [kr] Add language toggle for 'TEST' memberType users

--- kr-live-web/src/components/LangToggle.vue
+++ kr-live-web/src/components/LangToggle.vue
@@ -1,14 +1,17 @@
 <template>
-  <q-page-sticky position="bottom-right" :offset="[50, 250]" style="z-index:999999">
-    <q-btn fab v-if="showLangToggle" color="primary" @click="toggleLang">{{ lang }}</q-btn>
+  <q-page-sticky position="bottom-right" :offset="[40, 230]" style="z-index:999999" v-if="memberType === 'TEST'">
+    <q-btn class="floating" fab color="primary" @click="toggleLang">{{ lang }}</q-btn>
   </q-page-sticky>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { i18nStore } from "src/router/language";
+import { userStore } from "src/stores";
+import { storeToRefs } from "pinia";
 
-const showLangToggle = ref(process.env.NODE_ENV === "development");
+const store = userStore();
+const { memberType } = storeToRefs(store);
 const { setLanguage, languageVal } = i18nStore();
 
 const lang = ref(languageVal);
