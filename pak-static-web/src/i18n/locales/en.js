3a43e2ab12854c7bc77d4c8c1ad532c8b3c330af
Mika <mika@eagrp.net>
2024-06-21 18:00:55 +0800

    fix:[pak web]update pak promo with types

--- pak-static-web/src/i18n/locales/en.js
+++ pak-static-web/src/i18n/locales/en.js
@@ -760,6 +760,16 @@ export default {
       }
     }
   },
+  promo: {
+    all: 'ALL',
+    earn: 'EARN',
+    hot: 'HOT',
+    new_user: 'NEW USER',
+    sports: 'SPORTS',
+    live: 'LIVE',
+    slot: 'SLOT',
+    vip: 'VIP'
+  },
   error: {
     101: "Platform not found",
     102: "Platform is closed",
