7859970761a1d0069a1c4b4005897c3163a626ae
WeiSeng <weiseng@eagrp.net>
2024-07-01 21:30:11 +0800

    feat: LH change url env

--- lh-static-h5/src/boot/axios.js
+++ lh-static-h5/src/boot/axios.js
@@ -11,30 +11,33 @@ const evtArray = Object.values(process.env.EVT_API);
 const crtArray = Object.values(process.env.CR_API);
 
 console.log(window.location.hostname);
-const globalLinks= ["lh050","lh068","lh131","lh165","lh318","lh338","lh360","lh537","lh556","lh730","lh739","lh765","lh768","lh835","lh866","lh869","lh887","lh970","lh971","lh988"];
+const globalLinks=   ["lh050.","lh068.","lh131.","lh165.","lh318.","lh338.","lh360.","lh537.","lh556.","lh730.","lh739.","lh765.","lh768.","lh835.","lh866.","lh869.","lh887.","lh970.","lh971.","lh988."];
 const isGlobalLH = globalLinks.some(link => window.location.hostname.includes(link));
 
-const specialLinks= ["lh75561","lh77331","lh79669", "lh93371", "lh76390", "lh30553", "lh13179","lh36791", "lh36909", "lh97969", "lh09903", "lh97100", "lh89737", "lh36987", "lh59376", "lh60108", "lh63133", "lh67319", "lh69166"];
-const isSpecialLH = specialLinks.some((link) => window.location.hostname.includes(link));
+// const specialLinks= ["lh75561","lh77331","lh79669", "lh93371", "lh76390", "lh30553", "lh13179","lh36791", "lh36909", "lh97969", "lh09903", "lh97100", "lh89737", "lh36987", "lh59376", "lh60108", "lh63133", "lh67319", "lh69166"];
+// const isSpecialLH = specialLinks.some((link) => window.location.hostname.includes(link));
 
-if (isSpecialLH){
-  var rstSpecialArray = ["https://apodnbo0tl.anipoius54d.com", "https://ap2gh538tl.se17xiasedy.com"];
-  var evtSpecialArray = ["https://prk46vfitl.111z35h0mt.com", "https://prkuo09ctl.1rqrhcll8p.com"];
-  var crtSpecialArray =["https://cauomdoptl.baw7xptuqr1.com", "https://caaukstntl.ectuu384q0h.com"];
+// if (isSpecialLH){
+//   var rstSpecialArray = ["https://apodnbo0tl.anipoius54d.com", "https://ap2gh538tl.se17xiasedy.com"];
+//   var evtSpecialArray = ["https://prk46vfitl.111z35h0mt.com", "https://prkuo09ctl.1rqrhcll8p.com"];
+//   var crtSpecialArray =["https://cauomdoptl.baw7xptuqr1.com", "https://caaukstntl.ectuu384q0h.com"];
+//
+//   var rstApi = getInitApi(rstSpecialArray, "LH_H5_RST_URL");
+//   var evtApi = getInitApi(evtSpecialArray, "LH_H5_EVT_URL");
+//   var crtApi = getInitApi(crtSpecialArray, "LH_H5_CRT_URL");
+//
+//   var cdnSpecialArray =["https://url9jr173tl.acj39bv80x.com", "https://url847fkttl.b5chotsxy0.com"];
+//
+//   var cdnApi = cdnSpecialArray[getRndInteger(0, cdnSpecialArray.length)];
+//   localStorage.setItem("IMAGE_CDN", cdnApi);
+//
+// }
+if (isGlobalLH) {
+  console.log("Is Global");
 
-  var rstApi = getInitApi(rstSpecialArray, "LH_H5_RST_URL");
-  var evtApi = getInitApi(evtSpecialArray, "LH_H5_EVT_URL");
-  var crtApi = getInitApi(crtSpecialArray, "LH_H5_CRT_URL");
-
-  var cdnSpecialArray =["https://url9jr173tl.acj39bv80x.com", "https://url847fkttl.b5chotsxy0.com"];
-
-  var cdnApi = cdnSpecialArray[getRndInteger(0, cdnSpecialArray.length)];
-  localStorage.setItem("IMAGE_CDN", cdnApi);
-
-}else if (isGlobalLH) {
-  var rstApi = "https://apc2ttgdgl.grsib6dfily.com";
-  var evtApi = "https://pr5z5egdgl.grsib6dfily.com";
-  var crtApi = "https://cad5kegdgl.grsib6dfily.com";
+  var rstApi = "https://aptvpnubglgl.conoibue6er.com";
+  var evtApi = "https://przl4oufglgl.anpoxuaq9ae.com";
+  var crtApi = "https://caxlzwt2glgl.inc8ozys5we.com";
 
   localStorage.setItem("LH_H5_RST_URL", rstApi);
   localStorage.setItem("LH_H5_EVT_URL",evtApi);
