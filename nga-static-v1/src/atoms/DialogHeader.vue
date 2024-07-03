171c914bddf40c273ed6fc2824e07b130a2e51ef
Keng Hong <kenghongho@eagrp.net>
2024-07-03 17:34:55 +0800

    feat: [nga v1] dev environment setup for Nigeria v1

---
+++ nga-static-v1/src/atoms/DialogHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-card-section class="row items-center q-pb-none dialog-header">
+    <div class="text-h5 dialog-title">{{ title }}</div>
+    <q-space></q-space>
+    <img class="close-btn" src="../assets/images/index/close-btn.png" alt="" v-close-popup />
+  </q-card-section>
+</template>
+
+<script setup>
+const props = defineProps(["title"]);
+</script>
+
+<style lang="scss" scoped>
+.dialog-header {
+  position: relative;
+  text-align: center;
+  margin: 0 0 1rem 0;
+
+  .dialog-title {
+    width: 100%;
+    text-align: center;
+  }
+
+  .close-btn {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 2rem;
+    height: 2rem;
+  }
+}
+</style>
