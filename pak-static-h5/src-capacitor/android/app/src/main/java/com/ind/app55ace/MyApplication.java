487eccf16a494a484c7be2f136f3657b93b87cb6
WeiSeng <weiseng@eagrp.net>
2024-07-05 18:41:29 +0800

    feat: PAK h5 cloudwise installed

--- pak-static-h5/src-capacitor/android/app/src/main/java/com/ind/app55ace/MyApplication.java
+++ pak-static-h5/src-capacitor/android/app/src/main/java/com/ind/app55ace/MyApplication.java
@@ -7,7 +7,7 @@ import com.cloudwise.agent.app.CWSDK;
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
-        CWSDK.withAppKey("wS0n2SF8WRCb0fAkjFLvksRizrsrej3Y0IvAwUH4o9t5kzvIslkyrWuCslA3sd7L", "https://apm-int.cloudwise.com")
+        CWSDK.withAppKey("wS0n2SF8WRCb0fAkjFLvksRizrsrej3Y4XuCglHI0M9oZME**mvyF3eC**5bskBqMp", "https://apm-int.cloudwise.com")
                 .enableLogging(true) //打开logcat日志开关
                 .start(getApplicationContext());
         super.onCreate();
