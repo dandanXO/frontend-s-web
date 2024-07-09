5e5afc0a45dcb737a155102f323fcd5b2275aa72
karchuan <karchuan@caspen-tech.com>
2024-07-09 17:58:20 +0800

    add deposit withdraw page

---
+++ kr-affiliate-web/src/views/settlement-management/deposit-withdraw/index.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="page-container">
+    <div class="panel-item">
+      <div>
+        <el-form @submit.prevent>
+          <el-row :gutter="20">
+            <el-col :span="4">
+              <el-form-item :label="t('fields.type') + ' :'">
+                <el-select
+                  clearable
+                  v-model="request.type"
+                  size="small"
+                  :placeholder="t('fields.type')"
+                  class="filter-item"
+                  style="width: 120px; margin-left: 5px"
+                >
+                  <el-option :label="t('fields.all')" value="" />
+                  <el-option :label="t('fields.deposit')" value="DEPOSIT" />
+                  <el-option :label="t('fields.withdraw')" value="WITHDRAW" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item :label="t('fields.loginName') + ' :'">
+                <el-input v-model="request.loginName" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="10">
+              <el-form-item :label="t('fields.recordTime') + ' :'">
+                <el-date-picker
+                  v-model="request.recordTime"
+                  format="DD/MM/YYYY"
+                  value-format="YYYY-MM-DD"
+                  size="normal"
+                  type="daterange"
+                  range-separator=":"
+                  :start-placeholder="t('fields.startDate')"
+                  :end-placeholder="t('fields.endDate')"
+                  :shortcuts="shortcuts"
+                  :disabled-date="disabledDate"
+                  :editable="false"
+                  :clearable="false"
+                  :default-time="defaultTime"
+                  style="width: 100%;"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-button
+                style="margin-left: 20px"
+                icon="el-icon-search"
+                size="normal"
+                type="success"
+                @click="loadRecords()"
+              >
+                {{ t('fields.search') }}
+              </el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+      <div style="overflow:scroll">
+        <table cellpadding="0" cellspacing="0" border class="custom-table">
+          <thead>
+            <tr>
+              <th scope="col">{{ t('fields.date') }}</th>
+              <th scope="col">{{ t('fields.secondCol') }}</th>
+              <th scope="col">{{ t('fields.thirdCol') }}</th>
+              <th scope="col">{{ t('fields.fourthCol') }}</th>
+              <th scope="col">{{ t('fields.fifthCol') }}</th>
+              <th scope="col">{{ t('fields.sixthCol') }}</th>
+              <th scope="col">{{ t('fields.seventhCol') }}</th>
+              <th scope="col">{{ t('fields.eightCol') }}</th>
+            </tr>
+          </thead>
+          <tbody v-if="page.loading || page.records.length === 0">
+            <tr>
+              <td colspan="11">
+                <Loading v-if="page.loading" />
+                <emptyComp v-else-if="page.records.length === 0" />
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-else-if="page.records.length > 0">
+            <tr v-for="item in page.records" :key="item.id">
+              <td :data-label="t('fields.date')">
+                {{ item.date }}
+              </td>
+              <td class="bgGreen textGreen" :data-label="t('fields.secondCol')">
+                {{ formatMoney(item.secondCol) }}
+              </td>
+              <td class="bgRed textRed" :data-label="t('fields.thirdCol')">
+                {{ formatMoney(item.thirdCol) }}
+              </td>
+              <td class="bgYellow textRed" :data-label="t('fields.fourthCol')">
+                {{ formatMoney(item.fourthCol) }}
+              </td>
+              <td class="bgYellow textGreen" :data-label="t('fields.fifthCol')">
+                {{ formatMoney(item.fifthCol) }}
+              </td>
+              <td class="bgYellow" :data-label="t('fields.sixthCol')">
+                {{ formatMoney(item.sixthCol) }}
+              </td>
+              <td class="bgYellow" :data-label="t('fields.seventhCol')">
+                {{ formatMoney(item.seventhCol) }}
+              </td>
+              <td class="bgRed textRed" :data-label="t('fields.eightCol')">
+                {{ formatMoney(item.eightCol) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { useI18n } from 'vue-i18n'
+import Loading from '@/components/loading/Loading.vue'
+import { formatMoney } from '@/utils/format-money'
+import { getDepositWithdrawRecord } from '@/api/affiliate-report'
+import moment from "moment";
+
+const { t } = useI18n()
+
+const page = reactive({
+  pages: 0,
+  records: [],
+  loading: false,
+})
+
+const request = reactive({
+  size: 20,
+  current: 1,
+  recordTime: [convertStartDate(new Date()), convertDate(new Date())],
+  loginName: null,
+  type: '',
+})
+
+function convertDate(date) {
+  return moment(date).endOf('day').format('YYYY-MM-DD');
+}
+
+function convertStartDate(date) {
+  return moment(date).startOf('day').format('YYYY-MM-DD');
+}
+
+async function loadRecords() {
+  page.loading = true;
+  const requestCopy = { ...request }
+  const query = {}
+  Object.entries(requestCopy).forEach(([key, value]) => {
+    if (value) {
+      query[key] = value
+    }
+  })
+  query.recordTime = query.recordTime.join(',')
+  const { data: ret } = await getDepositWithdrawRecord(query)
+  page.records = ret.records
+  page.pages = ret.pages
+  page.total = ret.total
+  page.loading = false
+}
+
+</script>
+
+<style lang="scss" scoped></style>
