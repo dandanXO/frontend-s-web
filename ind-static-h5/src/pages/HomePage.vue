6ffe78f7bd7df063690ef8e251583f1220828531
Sam <sam@flex-df.com>
2024-07-10 12:18:47 +0800

    feat: [ind v2/h5] Fix promo list not displaying

--- ind-static-h5/src/pages/HomePage.vue
+++ ind-static-h5/src/pages/HomePage.vue
@@ -1077,8 +1077,6 @@ import WithdrawalModal from "../components/modal/WithdrawalModal.vue";
 import DepositComponent from "../components/depositComponent.vue";
 import KYCGuestForm from "../components/KYCGuestForm.vue";
 import KYCUserForm from "../components/KYCUserForm.vue";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 import { Swiper, SwiperSlide } from "swiper/vue";
 // import { ref, onMounted, onUnmounted } from 'vue';
@@ -1857,7 +1855,6 @@ onMounted(() => {
   loadJILIFishGameList();
   loadJDBFishGameList();
   loadCustomerAddress();
-  AOS.init();
   SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
   if (Platform.is.android && Platform.is.capacitor) {
