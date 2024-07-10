58a3884e68a8a8d86c80c3d2e173b38d426f6311
WeiSeng <weiseng@eagrp.net>
2024-07-10 18:55:38 +0800

    feat: NGa change api

--- nga-static-v1/src/pages/HomePage.vue
+++ nga-static-v1/src/pages/HomePage.vue
@@ -1961,8 +1961,9 @@ const loadHotGameList = () => {
   cached
     .get(key2, () =>
       api
-        .get("/member/hot", {
+        .get("/sitePlatformAndGamesByLabel", {
           params: {
+            gameLabel: "HOT",
             device: regDevice
           }
         })
