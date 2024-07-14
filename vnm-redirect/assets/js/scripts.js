1e4c10075389b6ad250db22ce6c18c4b5b50c16e
WeiSeng <weiseng@eagrp.net>
2024-07-14 21:38:43 +0800

    feat: VNM add redirect link

--- vnm-redirect/assets/js/scripts.js
+++ vnm-redirect/assets/js/scripts.js
@@ -1,7 +1,19 @@
 const isWaitClick = "false";
 const urlLists = ["https://p886866.com", "https://p886866.com"];
 const randNum = Math.floor(Math.random() * 2) + 1;
-const next = urlLists[randNum - 1];
+var next = urlLists[randNum - 1];
+var hostname = window.location.hostname;
+console.log(hostname);
+if (
+  hostname.indexOf("ft609.cc") > -1 ||
+  hostname.indexOf("ro396.cc") > -1 ||
+  hostname.indexOf("pt505.cc") > -1 ||
+  hostname.indexOf("ai007.cc") > -1 ||
+  hostname.indexOf("qa289.cc") > -1
+) {
+  next = "https://vb88123.com/register";
+}
+
 let timeoutId;
 if (isWaitClick === "false") {
   timeoutId = setTimeout(() => {
@@ -9,9 +21,6 @@ if (isWaitClick === "false") {
   }, 3000);
 }
 const existsContent = "An ninh trang web";
-
-var hostname = window.location.hostname;
-console.log(hostname);
 
 const handleClick = () => {
   // show loading
