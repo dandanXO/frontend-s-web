f048115d4819f8164748a9958c18154a8c97e7de
Kai Wen <kaiwen@caspen-tech.com>
2024-07-14 20:19:27 +0800

    fix: [lh web, lh h5] Amend promo text

--- lh-static-web/src/components/hotpromo/olympic-checkin/OlympicCheckin.vue
+++ lh-static-web/src/components/hotpromo/olympic-checkin/OlympicCheckin.vue
@@ -6,7 +6,7 @@
      <div class="checkin-header__date">(2024年07月14日~2024年07月24日)</div>
    </div>
    <div class="checkin-content">
-     活动期间，登录期间即可免费打卡一次打次数越高对应档位彩金越高；
+    活动期间，当日任意存款一笔即可参与打卡一次，打次数越高对应档位彩金越高；
    </div>
    <div class="checkin-section">
      <div class="items">
