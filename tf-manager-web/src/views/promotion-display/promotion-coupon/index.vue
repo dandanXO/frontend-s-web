33b94168e7cd3549cf6485c8d63666b92a281196
Vincey <vincey@eagrp.net>
2024-07-26 11:00:10 +0800

    upd: pak-promotion-deposit-coupon

--- tf-manager-web/src/views/promotion-display/promotion-coupon/index.vue
+++ tf-manager-web/src/views/promotion-display/promotion-coupon/index.vue
@@ -23,6 +23,20 @@
           style="width: 200px"
           :placeholder="t('fields.loginName')"
         />
+        <el-date-picker
+          v-model="request.recordTimeBetween"
+          format="DD/MM/YYYY"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          size="small"
+          type="daterange"
+          :start-placeholder="t('fields.startDate')"
+          :end-placeholder="t('fields.endDate')"
+          style="width: 280px; margin-left: 10px;"
+          :shortcuts="shortcuts"
+          :editable="false"
+          :clearable="false"
+          :default-time="defaultTime"
+        />
         <el-select
           clearable
           v-model="request.status"
@@ -66,12 +80,20 @@
             <el-tag type="info" size="mini">{{ scope.row.status }}</el-tag>
           </template>
         </el-table-column>
-        <el-table-column prop="recordTime" :label="t('fields.recordTime')" min-width="160">
+        <el-table-column prop="recordTime" :label="t('fields.recordTime')" min-width="200">
           <template #default="scope">
             <span v-if="scope.row.recordTime === null">-</span>
-            <span v-if="scope.row.recordTime !== null">{{ scope.row.recordTime }}</span>
+            <!-- eslint-disable -->
+            <span
+              v-if="scope.row.recordTime !== null"
+              v-formatter="{
+                data: scope.row.recordTime,
+                timeZone: timeZone,
+                type: 'date',
+              }"
+            />
           </template>
-        </el-table-column>
+      </el-table-column>
       </el-table>
       <el-pagination class="pagination"
                      @current-change="changePage"
@@ -90,6 +112,10 @@ import { onMounted, reactive } from "vue";
 import { getCouponRecords } from "../../../api/privilege-coupon";
 import { useI18n } from "vue-i18n";
 import { getSiteListSimple } from "../../../api/site";
+import { getShortcuts } from "@/utils/datetime";
+import moment from "moment";
+import { formatInputTimeZone } from '@/utils/format-timeZone'
+let timeZone = null
 
 const { t } = useI18n();
 const uiControl = reactive({
@@ -110,11 +136,26 @@ const siteList = reactive({
   list: []
 });
 
+const defaultTime = [
+  new Date(2000, 1, 1, 0, 0, 0),
+  new Date(2000, 1, 1, 23, 59, 59),
+];
+const shortcuts = getShortcuts(t);
+
+function convertDate(date) {
+  return moment(date).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+}
+
+function convertStartDate(date) {
+  return moment(date).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+}
+
 const request = reactive({
   size: 30,
   current: 1,
   status: null,
   loginName: null,
+  recordTimeBetween: [convertStartDate(new Date()), convertDate(new Date())],
   siteId: null
 });
 
@@ -123,15 +164,38 @@ function resetQuery() {
   request.loginName = null;
 }
 
-async function loadRecords() {
-  page.loading = true;
-  const requestCopy = { ...request };
-  const query = {};
+function checkQuery() {
+  const requestCopy = { ...request }
+  const query = {}
   Object.entries(requestCopy).forEach(([key, value]) => {
     if (value) {
-      query[key] = value;
+      query[key] = value
     }
-  });
+  })
+  timeZone = siteList.list.find(e => e.id === request.siteId).timeZone
+  if (request.recordTimeBetween !== null) {
+    if (request.recordTimeBetween.length === 2) {
+      query.recordTimeBetween = JSON.parse(JSON.stringify(request.recordTimeBetween))
+      query.recordTimeBetween[0] = formatInputTimeZone(
+        query.recordTimeBetween[0],
+        timeZone,
+        'start'
+      )
+      query.recordTimeBetween[1] = formatInputTimeZone(
+        query.recordTimeBetween[1],
+        timeZone,
+        'end'
+      )
+      query.recordTimeBetween = query.recordTimeBetween.join(',')
+    }
+  }
+
+  return query
+}
+
+async function loadRecords() {
+  page.loading = true;
+  const query = checkQuery()
   const { data: ret } = await getCouponRecords(query);
   page.pages = ret.pages;
   page.records = ret.records;
