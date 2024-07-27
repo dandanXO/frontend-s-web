87c74c0e54d294eadaec731dcb995fe4aee81682
WeiSeng <weiseng@eagrp.net>
2024-07-27 22:56:44 +0800

    feat: LH change promo fix

--- lh-static-h5/src/components/hotpromo/newplayerGuide/option2Area.vue
+++ lh-static-h5/src/components/hotpromo/newplayerGuide/option2Area.vue
@@ -258,7 +258,7 @@
         <div class="progress" :style="{ width: progressPercentage1 + '%' }"></div>
       </div>
       <div style="display: flex; justify-content: space-between; align-items: center">
-        <div>已领取：{{ getDoneRewardItem1?.earn }}元</div>
+        <div>已领取：{{ getDoneRewardItem1 }}元</div>
         <div v-if="matchRewardItem1 && matchRewardItem1.earn">
           距 {{ matchRewardItem1?.earn }} 元奖金，还需充值
           <span style="color: rgba(0, 136, 215, 1)">{{ matchRewardItem1?.ruleAmount - depositAmount }}</span>
@@ -299,7 +299,7 @@
         <div class="progress" :style="{ width: progressPercentage2 + '%' }"></div>
       </div>
       <div style="display: flex; justify-content: space-between; align-items: center">
-        <div>已领取：{{ getDoneRewardItem2?.earn }}元</div>
+        <div>已领取：{{ getDoneRewardItem2 }}元</div>
         <div v-if="matchRewardItem2 && matchRewardItem2.earn">
           距 {{ matchRewardItem2?.earn }} 元奖金，还需充值
           <span style="color: rgba(0, 136, 215, 1)">{{ matchRewardItem2?.ruleAmount - depositAmount }}</span>
@@ -366,10 +366,10 @@ const rewards2 = computed(() => {
   return updatedApiRes.value.filter((item) => targetRuleAmount2.includes(item.ruleAmount));
 });
 const getDoneRewardItem1 = computed(() => {
-  return rewards1.value.findLast((item) => item.state === "CLAIMED") ?? { earn: 0 };
+  return rewards1.value.filter((item) => item.state === "CLAIMED").reduce((sum, item) => sum + item.earn, 0);
 });
 const getDoneRewardItem2 = computed(() => {
-  return rewards2.value.findLast((item) => item.state === "CLAIMED") ?? { earn: 0 };
+  return rewards2.value.filter((item) => item.state === "CLAIMED").reduce((sum, item) => sum + item.earn, 0);
 });
 const matchRewardItem1 = computed(() => {
   return rewards1.value.find((item) => item.ruleAmount > depositAmount.value);
@@ -669,8 +669,9 @@ tbody tr:last-child {
 
 .rewards {
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   flex-wrap: wrap;
+  padding: 8px 0px;
   row-gap: 5px;
   column-gap: 5px;
 
