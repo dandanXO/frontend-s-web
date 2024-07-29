ebc991c2a833f986541dd7f830052d19eb595fe5
Mika <mika@eagrp.net>
2024-07-29 21:02:50 +0800

    fix: [kr] update

--- kr-static-web/src/components/PlatformViewFish.vue
+++ kr-static-web/src/components/PlatformViewFish.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="platform-section" :style="{ 'background-size':'cover', 'background-image':( platformType !== 'slot' && platformType !== 'fishing' && platformType !== 'casual') ? 'url(' + require('../assets/' + platformType + '/' + platformType + '-bg.png') + ')' : 'none' }">
     <div v-if="platformsListDisplay.length > 0" class="platform-container"
-         :class="[(platformType === 'slot' || platformType === 'casual') ? 'slot-container' : '', platformType === 'fishing'? 'casual-container': '']"
+         :class="[(platformType === 'casual') ? 'slot-container' : '', platformType === 'fishing' || platformType === 'slot'? 'casual-container': '']"
     >
       <div class="platform-container-slot" v-if="platformType === 'slot' || platformType === 'casual'">
         <img :src="require(`../assets/slot/slot-top-bg-${languageVal}.png`)">
