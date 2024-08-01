b4ffb993b9c2922314e8334066afe45b59a6a52f
Keng Hong <kenghongho@eagrp.net>
2024-08-01 14:38:27 +0800

    feat: [PAK, IND2, NGA] update version format

--- pak-static-h5/src/pages/HomePage.vue
+++ pak-static-h5/src/pages/HomePage.vue
@@ -3158,7 +3158,7 @@ const getVersionNo = async () => {
     const info = await App.getInfo();
     // console.log("APP Info");
     // console.log(info);
-    var current_version = parseInt(info.version.replaceAll(".", "") + info.build);
+    var current_version = parseInt(info.version.replaceAll(".", ""));
     // alert("Cur:" + current_version);
     // info.version && info.build
     const appType = "ALL";
