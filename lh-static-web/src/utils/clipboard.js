eed5fab4215b0daa69e473475054e7a0d8164a73
Sam <sam@flex-df.com>
2024-08-02 14:20:17 +0800

    feat: [lh/dy web/h5] Update promo rule, fix clipboard

--- lh-static-web/src/utils/clipboard.js
+++ lh-static-web/src/utils/clipboard.js
@@ -1,36 +1,40 @@
-import { uiStore } from "@/store/ui"
+import { uiStore } from "@/store/ui";
 
 const requestClipboardPermission = () => {
   return navigator.permissions.query({
-    name: 'clipboard-write',
-  })
-}
+    name: "clipboard-write"
+  });
+};
 
-export const writeClipboard = async (content) => {
-  const ui = uiStore()
+export const writeClipboard = async (content, useExecCommand = false) => {
+  const ui = uiStore();
   try {
-    if (window.isSecureContext && navigator.clipboard) {
-      const permission = await requestClipboardPermission()
-      if (permission.state !== 'granted') throw new Error()
-      await navigator.clipboard.writeText(content)
+    if (window.isSecureContext && navigator.clipboard && !useExecCommand) {
+      const permission = await requestClipboardPermission();
+      if (permission.state !== "granted") throw new Error();
+      await navigator.clipboard.writeText(content);
     } else {
-      const textArea = document.createElement('textarea')
-      textArea.value = content
-      textArea.style.position = 'absolute'
-      textArea.style.opacity = '0'
-      document.body.prepend(textArea)
-      textArea.select()
-      document.execCommand('copy')
-      textArea.remove()
+      const textArea = document.createElement("textarea");
+      textArea.value = content;
+      textArea.style.position = "absolute";
+      textArea.style.opacity = "0";
+      document.body.prepend(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      textArea.remove();
     }
     ui.notify({
-      type: 'success',
-      message: '复制成功'
-    })
+      type: "success",
+      message: "复制成功"
+    });
   } catch (error) {
-    ui.notify({
-      type: 'error',
-      message: '复制失败',
-    })
+    if (!useExecCommand) {
+      await writeClipboard(content, true);
+    } else {
+      ui.notify({
+        type: "error",
+        message: "复制失败"
+      });
+    }
   }
-}
+};
