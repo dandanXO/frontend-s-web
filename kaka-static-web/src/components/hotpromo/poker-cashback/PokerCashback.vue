48275ddb987a7a98cedd44a6eaf7af098f5e7f3f
WeiSeng <weiseng@eagrp.net>
2024-08-02 16:00:05 +0800

    feat: KAKA change promo icosn

--- kaka-static-web/src/components/hotpromo/poker-cashback/PokerCashback.vue
+++ kaka-static-web/src/components/hotpromo/poker-cashback/PokerCashback.vue
@@ -1,67 +1,68 @@
 <template>
-    <div class="row">
-        <img src="../../../assets/images/promotion/hotpromo/poker-cashback/poker-cashback-cards.png" />
-        <div class="col">
-            <div class="row">
-                <div>{{ t('promo.net_work_bonus') }}</div>
-                <el-input style="text-align:center;" v-model="claimAmt" :readonly="true" />
-            </div>
-            <div class="btn-color-blue standard-button action-btn" @click="onClickClaimNow">{{ t('promo.btn_claim_now') }}</div>
-        </div>
+  <div class="row">
+    <img src="../../../assets/images/promotion/hotpromo/poker-cashback/icon.png" />
+    <div class="col">
+      <div class="row">
+        <div>{{ t("promo.net_work_bonus") }}</div>
+        <el-input style="text-align: center" v-model="claimAmt" :readonly="true" />
+      </div>
+      <div class="btn-color-blue standard-button action-btn" @click="onClickClaimNow">
+        {{ t("promo.btn_claim_now") }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { ElMessage } from 'element-plus';
-import { bonusClaimPokerRefund, getBonusPokerCashback } from '../../../api/index/promo';
+import { ref, onMounted } from "vue";
+import { ElMessage } from "element-plus";
+import { bonusClaimPokerRefund, getBonusPokerCashback } from "../../../api/index/promo";
 import { useI18n } from "vue-i18n";
 
-
 const { t } = useI18n();
-const claimAmt = ref(0)
+const claimAmt = ref(0);
 const getAmount = () => {
-    getBonusPokerCashback().then((res) => {
-        if (res.code === 0) {
-            claimAmt.value = res.data;
-        } else {
-            ElMessage.error(res.message)
-        }
-    })
-}
+  getBonusPokerCashback().then((res) => {
+    if (res.code === 0) {
+      claimAmt.value = res.data;
+    } else {
+      ElMessage.error(res.message);
+    }
+  });
+};
 const onClickClaimNow = () => {
-    bonusClaimPokerRefund().then((res) => {
-        if (res.code === 0) {
-          ElMessage.success(t('common.claimedSuccess'))
-        } else {
-            ElMessage.error(res.message)
-        }
-    })
-}
+  bonusClaimPokerRefund().then((res) => {
+    if (res.code === 0) {
+      ElMessage.success(t("common.claimedSuccess"));
+    } else {
+      ElMessage.error(res.message);
+    }
+  });
+};
 onMounted(() => {
-    getAmount();
-})
+  getAmount();
+});
 </script>
 
 <style lang="scss" scoped>
 .el-input__inner {
-    text-align: center;
+  text-align: center;
 }
 .row {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .col {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
 }
 
 .action-btn {
-    width: 250px;
-    height: 45px;
+  width: 250px;
+  height: 45px;
 }
 </style>
--- kaka-static-web/src/components/hotpromo/slot-netloss/SlotNetloss.vue
+++ kaka-static-web/src/components/hotpromo/slot-netloss/SlotNetloss.vue
@@ -1,66 +1,68 @@
 <template>
-    <div class="row">
-        <img src="../../../assets/images/promotion/hotpromo/slot-netloss/slot-netloss-coins.png" />
-        <div class="col">
-            <div class="row">
-                <div>{{ t('promo.net_work_bonus') }}</div>
-                <el-input v-model="claimAmt" :readonly="true" />
-            </div>
-            <div class="btn-color-blue standard-button action-btn" @click="onClickClaimNow">{{ t('promo.btn_claim_now') }}</div>
-        </div>
+  <div class="row">
+    <img src="../../../assets/images/promotion/hotpromo/slot-netloss/icon.png" />
+    <div class="col">
+      <div class="row">
+        <div>{{ t("promo.net_work_bonus") }}</div>
+        <el-input v-model="claimAmt" :readonly="true" />
+      </div>
+      <div class="btn-color-blue standard-button action-btn" @click="onClickClaimNow">
+        {{ t("promo.btn_claim_now") }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { ElMessage } from 'element-plus';
-import { bonusClaimSlotsRefund, getBonusSlotsCashback } from '../../../api/index/promo';
+import { ref, onMounted } from "vue";
+import { ElMessage } from "element-plus";
+import { bonusClaimSlotsRefund, getBonusSlotsCashback } from "../../../api/index/promo";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
-const claimAmt = ref(0)
+const claimAmt = ref(0);
 const getAmount = () => {
-    getBonusSlotsCashback().then((res) => {
-        if (res.code === 0) {
-            claimAmt.value = res.data;
-        } else {
-            ElMessage.error(res.message)
-        }
-    })
-}
+  getBonusSlotsCashback().then((res) => {
+    if (res.code === 0) {
+      claimAmt.value = res.data;
+    } else {
+      ElMessage.error(res.message);
+    }
+  });
+};
 const onClickClaimNow = () => {
-    bonusClaimSlotsRefund().then((res) => {
-        if(res.code === 0) {
-            ElMessage.success(t('common.claimedSuccess'))
-        } else {
-            ElMessage.error(res.message)
-        }
-    })
-}
+  bonusClaimSlotsRefund().then((res) => {
+    if (res.code === 0) {
+      ElMessage.success(t("common.claimedSuccess"));
+    } else {
+      ElMessage.error(res.message);
+    }
+  });
+};
 onMounted(() => {
-    getAmount();
-})
+  getAmount();
+});
 </script>
 
 <style lang="scss" scoped>
 .el-input__inner {
-    text-align: center;
+  text-align: center;
 }
 .row {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .col {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
 }
 
 .action-btn {
-    width: 250px;
-    height: 45px;
+  width: 250px;
+  height: 45px;
 }
 </style>
