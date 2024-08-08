1276e84b671bc1b9fa154a15e376098c7e07110f
Neil Yang <des75421@gmail.com>
2024-08-08 15:27:15 +0800

    feat: lh-static-h5 livepoker-rebate promo api

--- lh-static-h5/src/components/hotpromo/livepoker-rebate/LivepokerRebate.vue
+++ lh-static-h5/src/components/hotpromo/livepoker-rebate/LivepokerRebate.vue
@@ -16,7 +16,7 @@
               <span
                 class="amount"
               >
-                888元
+                {{ totalValidBet }}元
               </span>
             </div>
           </div>
@@ -29,13 +29,15 @@
               <span
                 class="amount"
               >
-                8元
+                {{ bonus }}元
               </span>
             </div>
           </div>
         </div>
         <div class="livepoker-rebate-section-right">
-          <img src="../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/reward-btn.png" alt="" width="100%" />
+          <div class="bonus-image" @click="handleClaimBonus">
+            <img src="../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/reward-btn.png" alt="" width="100%" />
+          </div>
         </div>
       </div>
       <div class="livepoker-rebate-game-info">
@@ -132,6 +134,40 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+import { getLivePoker, claimLivePokerBonus } from "../../../api/index/promo";
+import { useNotify } from "src/hooks/notify";
+
+const notify = useNotify()
+
+const totalValidBet = ref(0)
+const bonus = ref(0)
+
+const handleClaimBonus = () => {
+  claimLivePokerBonus().then(res => {
+    if (res.code === 0) {
+      fetchData()
+    } else {
+      notify.error(res.message)
+    }
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+const fetchData = async () => {
+  try {
+    const res = await getLivePoker()
+    totalValidBet.value = res.data.totalValidBet
+    bonus.value = res.data.bonus
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <style scoped lang="scss">
@@ -172,6 +208,11 @@
   .livepoker-rebate-section-right {
     width: 180px;
     margin-top: 20px;
+
+    .bonus-image {
+      width: 100%;
+      cursor: pointer;
+    }
   }
 
   .livepoker-rebate-section-title {
