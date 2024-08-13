ab9b455bb07f834a4b70d4f78b066aed8ec18ef1
Kai Wen <kaiwen@caspen-tech.com>
2024-08-13 17:06:30 +0800

    fix: [lh web] Withdraw page adjust step item bg color

--- lh-static-web/src/views/account/MailboxView.vue
+++ lh-static-web/src/views/account/MailboxView.vue
@@ -94,7 +94,7 @@
             </template>
 
             <template v-else>
-              <div style="display: flex; justify-content: center; align-items: center; height: 300px">暂无记录</div>
+              <div class="no-record-text" style="display: flex; justify-content: center; align-items: center; height: 300px">暂无记录</div>
             </template>
           </el-tab-pane>
         </el-tabs>
@@ -719,6 +719,10 @@ onMounted(() => {
 }
 
 .dark {
+  .no-record-text {
+    color: #fff;
+  }
+  
   .menu-title {
     color: $color-white;
   }
--- lh-static-web/src/views/finance/WithdrawView.vue
+++ lh-static-web/src/views/finance/WithdrawView.vue
@@ -899,7 +899,8 @@ export default defineComponent({
   .account-container {
     .account-content-wrapper {
       .step-item {
-        background: $active-color-dark-linear;
+        background: linear-gradient(90deg, #344468 0%, #1A2338 100%);
+        border: 1px solid #78abdb;
       }
       .withdraw-type-item {
         border: unset;
