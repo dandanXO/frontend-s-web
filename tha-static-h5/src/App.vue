cb2619a2d45674f0e47f241c145342167902d31c
WeiSeng <weiseng@eagrp.net>
2024-08-14 11:32:17 +0800

    feat: XF DY THA siteCode change

--- tha-static-h5/src/App.vue
+++ tha-static-h5/src/App.vue
@@ -311,7 +311,7 @@ export default defineComponent({
           qs.stringify({
             way: way,
             sid: theSid,
-            siteCode: "th1"
+            siteCode: process.env.SITE
           })
         );
       }
