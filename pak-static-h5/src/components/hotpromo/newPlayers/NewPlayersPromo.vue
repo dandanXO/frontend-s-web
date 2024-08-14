c7e5adf6d816035bb8d0e48832eab80320362392
Keng Hong <kenghongho@eagrp.net>
2024-08-14 14:21:24 +0800

    feat: [pak h5] new promo: pak-new-player-cashback

--- pak-static-h5/src/components/hotpromo/newPlayers/NewPlayersPromo.vue
+++ pak-static-h5/src/components/hotpromo/newPlayers/NewPlayersPromo.vue
@@ -5,15 +5,30 @@
     </q-btn>
   </div>
 
-  <!-- <pre>{{ list }}</pre> -->
+  <q-dialog v-model="showPrizePopup">
+    <div class="prize-popup">
+      <q-btn icon="close" flat round dense v-close-popup class="q-ml-auto" />
+      <div class="prize-gold">
+        <img src="./../../../assets/images/promotion/hotpromo/bonus-spinwheel/prize-gold.png" width="80" />
+        <div>{{ $t("hotPromo.aviatorWheel.congratulations") }}</div>
+      </div>
+
+      <div class="prize-amount">Rs {{ prizePopupBonusAmt }}</div>
+
+      <q-btn no-caps unelevated class="btn-primary" @click="showPrizePopup = false">{{ $t("btn.confirm") }}</q-btn>
+    </div>
+  </q-dialog>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { eventapi } from "boot/axios";
 import { useQuasar } from "quasar";
 
 const $q = useQuasar();
 const props = defineProps(["list"]);
+const showPrizePopup = ref(false);
+const prizePopupBonusAmt = ref();
 
 const collectclaim = (promoCode) => {
   eventapi
@@ -26,6 +41,9 @@ const collectclaim = (promoCode) => {
           message: "Collect successfully",
           icon: "check_circle_outline"
         });
+
+        showPrizePopup.value = true;
+        prizePopupBonusAmt.value = res.data;
       }
     })
     .catch((err) => {
@@ -38,5 +56,27 @@ const collectclaim = (promoCode) => {
 .newplayers-collect {
   display: flex;
   justify-content: center;
+}
+
+.prize-popup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden !important;
+}
+
+.prize-gold {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: #c7c7c7;
+}
+
+.prize-amount {
+  font-size: 38px;
+  color: #ffffff;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 20px;
 }
 </style>
