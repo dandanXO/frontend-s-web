44be295303cfa7e179059d679fd2efa0836e7a78
WeiSeng <weiseng@eagrp.net>
2024-08-15 10:46:46 +0800

    feat: DY XF maintenance css

--- vnm-static-h5/src/pages/MaintenancePage.vue
+++ vnm-static-h5/src/pages/MaintenancePage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="maintenance-container">
+    <div class="maintenance-logo">
+      <img src="../assets/logo-web.svg" />
+    </div>
     <div class="maintenance-img">
       <img :src="require('../assets/images/maintenance/maintenance-img.png')" />
     </div>
     <div class="maintenance-details">
-      <div class="maintenance-logo">
-        <img src="../assets/logo-web.svg" />
-      </div>
       <div class="maintenance-title">{{ $t("lang.maintenance_title") }}</div>
       <div class="maintenance-desc">
         {{ $t("lang.maintenance_from") }} {{ ui.maintenanceStartTime }}
@@ -59,13 +59,14 @@ const ui = useUI();
   min-height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   background-image: url("../assets/images/maintenance/main-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: top center;
   flex-direction: column;
   padding: 20px;
+  padding-top: 45px;
 
   .maintenance-contact {
     margin-top: 24px;
@@ -104,15 +105,14 @@ const ui = useUI();
 
   .maintenance-title {
     margin-top: 10px;
-    font-family: PingFang SC;
-    color: #4c88f8;
+    color: #424f72;
     font-size: 30px;
     font-weight: 600;
+    text-align: center;
   }
 
   .maintenance-desc {
     margin-top: 10px;
-    font-family: PingFang SC;
     color: #858585;
     font-size: 18px;
     line-height: 1.6;
@@ -120,11 +120,12 @@ const ui = useUI();
   }
 
   .maintenance-img {
-    margin-left: 30px;
+    //margin-left: 30px;
     img {
       display: block;
-      width: 100%;
-      max-width: 600px;
+      width: 65%;
+      margin: 10px auto;
+      max-width: 450px;
     }
   }
 }
--- xf-static-h5/src/pages/MaintenancePage.vue
+++ xf-static-h5/src/pages/MaintenancePage.vue
@@ -1,62 +1,93 @@
 <template>
   <div class="maintenance-container">
-    <div class="maintenance-img">
-      <img class="logo-img" src="../assets/logo.png" />
-      <img src="../assets/images/maintenance/maintenance-img.png" />
+    <div class="maintenance-logo">
+      <img src="../assets/logo.png" />
     </div>
-    <div class="maintenance-title">网站维护中</div>
-    <div class="maintenance-content">
-      目前我们的网站正在维护中...
-      <br />
-      <template v-if="ui.maintenanceStartTime">
+    <div class="maintenance-img">
+      <img :src="require('../assets/images/maintenance/maintenance-img.png')" />
+    </div>
+
+    <div class="maintenance-title">目前我们的网站正在维护中...</div>
+    <div class="maintenance-details">
+      <div class="maintenance-desc">
         从：{{ ui.maintenanceStartTime }}
         <br />
-      </template>
-      <template v-if="ui.maintenanceEndTime">
         至：{{ ui.maintenanceEndTime }}
         <br />
-      </template>
-      期间所有游戏和服务均无法使用。
-      <br />
-      对于给您带来任何的不变，我们深表歉意，并感谢您的耐心等待！
+        <br />
+        期间所有游戏和服务均无法使用。
+        <br />
+        对于给您带来任何的不变，我们深表歉意，并感谢您的耐心等待！
+      </div>
     </div>
+    <!-- <div class="maintenance-btn">
+      <router-link to="/home" class="maintenance-cta">返回主页</router-link>
+    </div> -->
   </div>
 </template>
 
 <script setup>
-import { useUI } from "src/stores/ui";
+import { useUI } from "stores/ui";
+
 const ui = useUI();
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .maintenance-container {
-  min-height: 100dvh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  padding: 20px;
+  padding-top: 60px;
+  background-color: #F3F7fd;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top center;
   flex-direction: column;
-  position:relative;
-  z-index:9999;
 
-  .maintenance-img {
+  .maintenance-details {
+    display: flex;
+    flex-direction: column;
+
+    box-shadow: 0px -8px 8px 0px #c3d4e6 inset;
+    background: #fcfdfe;
+    border-radius: 20px;
+    padding: 30px 25px 40px;
+  }
+
+  .maintenance-logo {
+    margin: 0px auto 0;
     img {
       display: block;
-      width: 100%;
-      max-width: 250px;
+      width: 168px;
     }
   }
 
   .maintenance-title {
-    color: #fff;
-    display: flex;
+    margin: 30px auto;
+    color: #424f72;
+    font-size: 24px;
+    text-align: center;
+    font-weight: 600;
   }
 
-  .maintenance-content {
-    display: flex;
-    justify-content: center;
-    max-width: 400px;
-    padding: 16px;
-    color: #c1c1c1;
+  .maintenance-desc {
+    margin-top: 10px;
+    color: #858585;
+    font-size: 18px;
+    text-align: center;
+    line-height: 1.6;
+    font-weight: 500;
+  }
+
+  .maintenance-img {
+    img {
+      display: block;
+      width: 65%;
+      margin: 15px auto;
+      max-width: 400px;
+    }
   }
 }
 </style>
