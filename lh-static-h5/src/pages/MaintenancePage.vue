cafd6261a5db483c6f749c575c791962ea6cf8c0
WeiSeng <weiseng@eagrp.net>
2024-08-15 10:07:40 +0800

    feat: LH maintenance css

--- lh-static-h5/src/pages/MaintenancePage.vue
+++ lh-static-h5/src/pages/MaintenancePage.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="maintenance-container">
+    <div class="maintenance-logo">
+      <img src="../assets/logo-1.png" />
+    </div>
     <div class="maintenance-img">
       <img :src="require('../assets/images/maintenance/maintenance-img.png')" />
     </div>
+
+    <div class="maintenance-title">目前我们的网站正在维护中...</div>
     <div class="maintenance-details">
-      <div class="maintenance-logo">
-        <img src="../assets/logo-1.png" />
-      </div>
-      <div class="maintenance-title">目前我们的网站正在维护中...</div>
       <div class="maintenance-desc">
         从：{{ ui.maintenanceStartTime }}
         <br />
         至：{{ ui.maintenanceEndTime }}
+        <br />
         <br />
         期间所有游戏和服务均无法使用。
         <br />
@@ -35,21 +37,27 @@ const ui = useUI();
   min-height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  padding: 20px;
+  padding-top: 60px;
   background-image: url("../assets/images/maintenance/main-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: top center;
   flex-direction: column;
-  padding: 20px;
 
   .maintenance-details {
     display: flex;
     flex-direction: column;
+
+    box-shadow: 0px -8px 8px 0px #c3d4e6 inset;
+    background: #fcfdfe;
+    border-radius: 20px;
+    padding: 30px 25px 40px;
   }
 
   .maintenance-logo {
-    margin: 20px auto 0;
+    margin: 0px auto 0;
     img {
       display: block;
       width: 168px;
@@ -57,9 +65,10 @@ const ui = useUI();
   }
 
   .maintenance-title {
-    margin-top: 10px;
-    color: #4c88f8;
-    font-size: 30px;
+    margin: 30px auto;
+    color: #424f72;
+    font-size: 24px;
+    text-align: center;
     font-weight: 600;
   }
 
@@ -67,16 +76,17 @@ const ui = useUI();
     margin-top: 10px;
     color: #858585;
     font-size: 18px;
+    text-align: center;
     line-height: 1.6;
     font-weight: 500;
   }
 
   .maintenance-img {
-    margin-left: 30px;
     img {
       display: block;
-      width: 100%;
-      max-width: 600px;
+      width: 65%;
+      margin: 15px auto;
+      max-width: 400px;
     }
   }
 }
