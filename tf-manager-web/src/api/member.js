5d309cece0b8a4eab7fa345b7da1e772cd4903d3
SHIAN\dcshi <dcshian85@hotmail.com>
2024-08-16 10:59:53 +0800

    api update

--- tf-manager-web/src/api/member.js
+++ tf-manager-web/src/api/member.js
@@ -324,11 +324,15 @@ export const getAffiliateInfo = (id, siteId) => {
   return https().request(`/member/${id}/affiliate?siteId=${siteId}`, Method.GET)
 }
 
-export const getMemberBalanceByLoginNameSite = (loginName, siteId) => {
+export const getMemberBalanceByLoginNameSite = (
+  loginName,
+  siteId,
+  currency
+) => {
   return https().request(
     `/member/getMemberBalanceByLoginNameSite`,
     Method.GET,
-    { loginName: loginName, siteId: siteId },
+    { loginName: loginName, siteId: siteId, currency: currency },
     ContentType.form
   )
 }
