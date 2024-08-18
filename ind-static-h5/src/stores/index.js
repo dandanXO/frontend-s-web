dd071a3c3a55072d9bc47585dc5a890d00a2482e
WeiSeng <weiseng@eagrp.net>
2024-08-18 13:21:22 +0800

    feat: IND change

--- ind-static-h5/src/stores/index.js
+++ ind-static-h5/src/stores/index.js
@@ -46,7 +46,7 @@ export const userStore = defineStore("userStore", {
       googleadid: "",
       h5Url: "https://www.55ace.com/",
       hasUpdatedOneSignal: false,
-      walletCurrency: sessionStorage.getItem("WALLET_TYPE"),
+      walletCurrency: sessionStorage.getItem("WALLET_TYPE") ? sessionStorage.getItem("WALLET_TYPE") : "INR",
       multipleBalance: []
     };
   },
