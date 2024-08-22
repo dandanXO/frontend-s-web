cf9eaab0a1986b92e1d171da83b753085fae1e48
Neil Yang <des75421@gmail.com>
2024-08-22 15:35:18 +0800

    feat: xf-static-h5 before deposit withdrawal dailog style

--- xf-static-h5/src/pages/finance/WithdrawView.vue
+++ xf-static-h5/src/pages/finance/WithdrawView.vue
@@ -230,18 +230,25 @@
     <q-dialog width="100%" v-model="isNewUser" no-backdrop-dismiss no-esc-dismiss>
       <q-card style="width: 100%; padding: 20px" class="text-white">
         <q-card-section class="q-mb-md">
-          <strong style="display:inline-block;padding-bottom:16px;font-size:20px;">完成以下认证才可以提款</strong>
-          <div style="display: flex; gap: 12px; align-items: center; justify-content: space-between; margin: 16px 0" v-if="!store.realName">
-            <p style="margin: 0">提款需要绑定真实姓名</p>
-            <q-btn @click="router.push('/account/personal')" color="brightbtn" label="去绑定" />
+          <strong style="display: inline-block; padding-bottom: 16px; font-size: 20px">完成以下认证才可以提款</strong>
+          <div v-if="!store.realName" style="margin: 16px 0">
+            <div style="display: flex; gap: 12px; align-items: center; justify-content: space-between">
+              <p style="margin: 0">提款需要绑定真实姓名</p>
+              <q-btn @click="router.push('/account/personal')" color="brightbtn" label="去绑定?" />
+            </div>
+            <div style="font-size: 12px; color: red">为了您的资金安全，银行卡姓名需一致</div>
           </div>
-          <div style="display: flex; gap: 12px; align-items: center; justify-content: space-between" v-if="!store.phone">
-            <p style="margin: 0">提款需要绑定手机号</p>
-            <q-btn @click="router.push('/account/personal')" color="brightbtn" label="去绑定" />
+
+          <div v-if="!store.phone">
+            <div style="display: flex; gap: 12px; align-items: center; justify-content: space-between">
+              <p style="margin: 0">提款需要绑定手机号</p>
+              <q-btn @click="router.push('/account/personal')" color="brightbtn" label="去绑定!" />
+            </div>
+            <div style="font-size: 12px; color: red">为了您的资金安全，请绑定手机号</div>
           </div>
         </q-card-section>
         <q-card-actions align="right">
-          <q-btn style="width:100%;" label="暂不认证" color="brightbtn" @click="isNewUser = false" />
+          <q-btn style="width: 100%" label="暂不认证" color="brightbtn" @click="isNewUser = false" />
         </q-card-actions>
       </q-card>
     </q-dialog>
