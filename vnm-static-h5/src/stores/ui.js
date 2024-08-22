ddb083e411fec13afb80c5e6fb075a60f273e3ce
WeiSeng <weiseng@eagrp.net>
2024-08-22 15:55:22 +0800

    feat: VNM manitenance fix

--- vnm-static-h5/src/stores/ui.js
+++ vnm-static-h5/src/stores/ui.js
@@ -16,9 +16,8 @@ export const useUI = defineStore("ui-store", {
       adjust_open_app_event: "",
       adjust_register_fail_event: "",
       adjust_register_event: "",
-      edition: EDITION.NORMAL,
       maintenanceStartTime: "",
-      maintenanceEndTime: ""
+      maintenanceEndTime: "",
       edition: sessionStorage.getItem("HOME_EDITION") || EDITION.NORMAL
     };
   },
--- vnm-static-web/src/store/ui.js
+++ vnm-static-web/src/store/ui.js
@@ -8,9 +8,8 @@ export const uiStore = defineStore("ui-store", {
       footer: true,
       orientation: "landscape",
       downloadUrl: "",
-      edition: EDITION.NORMAL,
       maintenanceStartTime: "",
-      maintenanceEndTime: ""
+      maintenanceEndTime: "",
       edition: sessionStorage.getItem("HOME_EDITION") || EDITION.NORMAL
     };
   },
