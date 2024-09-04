24053b749ede6ae9ae562a622f9c8f902fb2d28a
yijing <yijing@eagrp.net>
2024-09-04 17:00:47 +0800

    fix: 渠道报表

--- tf-manager-web/src/i18n/en.js
+++ tf-manager-web/src/i18n/en.js
@@ -1613,6 +1613,7 @@ export default {
     totalDepositAmount: 'Total Deposit Amount',
     totalDepositCount: 'Total Deposit Count',
     totalDepositMemberCount: 'Total Member Deposit Count',
+    totalWithdrawMemberCount: 'Total Member Withdraw Count',
     totalDepositedAmount: 'Total deposited amount',
     totalDepositNumber: 'Total Deposit Member',
     totalDownlineAffiliates: 'Total Downline Affiliates',
--- tf-manager-web/src/i18n/zh.js
+++ tf-manager-web/src/i18n/zh.js
@@ -1613,6 +1613,7 @@ export default {
     totalDepositAmount: '存款总额',
     totalDepositCount: '总存款次数',
     totalDepositMemberCount: '总充值人数',
+    totalWithdrawMemberCount: '总提款人数',
     totalDepositedAmount: '存款总额',
     totalDepositNumber: '总充值会员数',
     totalDownlineAffiliates: '总直属代理',
