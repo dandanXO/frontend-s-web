853de40eb806f46ca681b1cd7d08a2b8bdc2cc45
karchuan <karchuan@caspen-tech.com>
2024-09-10 15:50:16 +0800

    i18n

--- tf-manager-web/src/i18n/en.js
+++ tf-manager-web/src/i18n/en.js
@@ -2801,6 +2801,7 @@ export default {
     'Sites Config': 'Site Config',
     'Mooncake Fest Spin Record': 'Mooncake Fest Spin Record 2024',
     'Platform WC': 'Platform WC',
+    'Site Platform Report': 'Site Platform Report',
   },
   google: {
     google_auth_menu: 'Google Authenticator',
