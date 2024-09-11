adfa6b97a00d0dd696cf1d9c90e01633e158ff76
WeiSeng <weiseng@eagrp.net>
2024-09-11 16:35:20 +0800

    feat: LH auto withdrawl

--- lh-static-h5/src/stores/index.js
+++ lh-static-h5/src/stores/index.js
@@ -1,8 +1,8 @@
-import {defineStore} from "pinia";
-import {api, cashier, eventapi} from "boot/axios";
-import {SessionStorage, Notify, Platform} from "quasar";
+import { defineStore } from "pinia";
+import { api, cashier, eventapi } from "boot/axios";
+import { SessionStorage, Notify, Platform } from "quasar";
 import LocalStorage from "boot/local-storage";
-import {isAndroid} from "boot/utils"
+import { isAndroid } from "boot/utils";
 import { useUI } from "./ui";
 
 var qs = require("qs");
@@ -32,8 +32,8 @@ export const userStore = defineStore("userStore", {
       token: getStoreToken(),
       vip: "",
       evip: "",
-      currency: {value: "￥", label: "RMB"},
-      personalAddress: '',
+      currency: { value: "￥", label: "RMB" },
+      personalAddress: "",
       unreadInboxMail: 0,
       phoneVerified: false,
       emailVerified: false,
@@ -41,7 +41,8 @@ export const userStore = defineStore("userStore", {
       levelUpDeposit: "",
       visitorId: "",
       profilePhoto: "",
-      isDisplayLogin: false
+      isDisplayLogin: false,
+      withdrawType: ""
     };
   },
   actions: {
@@ -77,9 +78,7 @@ export const userStore = defineStore("userStore", {
     },
     isApp() {
       if (
-        (Platform.is.ios &&
-          "standalone" in window.navigator &&
-          window.navigator.standalone) ||
+        (Platform.is.ios && "standalone" in window.navigator && window.navigator.standalone) ||
         (Platform.is.android && Platform.is.capacitor)
       ) {
         return true;
@@ -93,10 +92,11 @@ export const userStore = defineStore("userStore", {
     isNotAppPromo() {
       // console.log(window.location.pathname);
       //当 LH H5 在 /promotion 或者某些页面时，很多Api都不需要Call + 省时间。
-      if(
+      if (
         window.location.pathname === "/deposit" ||
         window.location.pathname === "/vip" ||
-        window.location.pathname === "/promotion"){
+        window.location.pathname === "/promotion"
+      ) {
         // console.log("IS In App")
         return false;
       }
@@ -124,7 +124,7 @@ export const userStore = defineStore("userStore", {
         } else {
           useUI().notify({
             type: "error",
-            message: ret.message,
+            message: ret.message
           });
         }
       });
@@ -151,7 +151,7 @@ export const userStore = defineStore("userStore", {
         } else {
           useUI().notify({
             type: "error",
-            message: ret.message,
+            message: ret.message
           });
         }
       });
@@ -212,6 +212,8 @@ export const userStore = defineStore("userStore", {
 
           this.profilePhoto = response.data.profilePhoto;
           this.unreadInboxMail = 0;
+
+          this.withdrawType = response.data.withdrawType;
           // this.unreadInboxMail = 16;
           this.getBalance();
         } else {
@@ -238,12 +240,12 @@ export const userStore = defineStore("userStore", {
     },
     getUnreadTotal() {
       if (this.token) {
-        return api.get('/session/inbox/getUnreadTotal').then((total) => {
+        return api.get("/session/inbox/getUnreadTotal").then((total) => {
           console.log(total);
           if (total.code === 0) {
             this.unreadInboxMail = total.data;
           }
-        })
+        });
       }
     },
     autoLogin(token) {
@@ -254,14 +256,12 @@ export const userStore = defineStore("userStore", {
       }
     },
     memberLogout() {
-      return api
-        .post("/session/logout")
-        .then(() => {
-          LocalStorage.remove("TOKEN");
-          SessionStorage.remove("TOKEN");
+      return api.post("/session/logout").then(() => {
+        LocalStorage.remove("TOKEN");
+        SessionStorage.remove("TOKEN");
 
-          location.reload();
-        });
+        location.reload();
+      });
     }
   }
 });
