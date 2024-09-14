bc9451d58a42615317bb1aa1d188cfd59290054a
Kai Wen <kaiwen@caspen-tech.com>
2024-09-14 16:37:51 +0800

    fix: [vnm web, vnm h5] Amend promo text

--- vnm-static-web/src/components/hotpromo/newPlayer/NewPlayerPromo.vue
+++ vnm-static-web/src/components/hotpromo/newPlayer/NewPlayerPromo.vue
@@ -101,11 +101,10 @@
   </div>
 </template>
 <script setup>
-import { onMounted, computed } from "vue";
+import { onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { ElMessage } from "element-plus";
 import { getNewPlayerMission, submitNewPlayerMission } from "@/api/promotion/newPlayer";
-import { userStore } from "@/store";
 
 import AccountSvg from "@/assets/images/promotion/hotpromo/newPlayer/account.svg";
 import transferSvg from "@/assets/images/promotion/hotpromo/newPlayer/transfer.svg";
@@ -113,81 +112,41 @@ import depositSvg from "@/assets/images/promotion/hotpromo/newPlayer/deposit.svg
 import walletSvg from "@/assets/images/promotion/hotpromo/newPlayer/wallet.svg";
 
 const { t } = useI18n();
-const store = userStore();
 
-const missions = computed(() => {
-  if(store.token) {
-    return [
-      {
-        code: "newbie-mission-t1",
-        description: "Cập nhật thông tin tài khoản ngân hàng, xác nhận số điện thoại.",
-        reward: 18,
-        icon: AccountSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t2",
-        description: "Thực hiện rút tiền thành công 1 lần.",
-        reward: 28,
-        icon: transferSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t3",
-        description: "Tích lũy tổng nạp tiền 700 VNDP, có tổng cộng 20 vé cược hợp lệ tại Thể Thao hoặc Live casino.",
-        reward: 70,
-        icon: depositSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t4",
-        description: "Tích lũy tổng nạp tiền 3,500 VNDP, tích lũy tổng tiền cược hợp lệ 7,000 VNDP.",
-        reward: 175,
-        icon: walletSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      }
-    ]
+const missions = ref([
+  {
+    code: "newbie-mission-t1",
+    description: "Cập nhật thông tin tài khoản ngân hàng, xác nhận số điện thoại.",
+    reward: 8,
+    icon: AccountSvg,
+    missionProgress: "0/0",
+    status: "Incomplete"
+  },
+  {
+    code: "newbie-mission-t2",
+    description: "Thực hiện rút tiền thành công 1 lần.",
+    reward: 28,
+    icon: transferSvg,
+    missionProgress: "0/0",
+    status: "Incomplete"
+  },
+  {
+    code: "newbie-mission-t3",
+    description: "Tích lũy tổng nạp tiền 700 VNDP, có tổng cộng 20 vé cược hợp lệ tại Thể Thao hoặc Live casino.",
+    reward: 38,
+    icon: depositSvg,
+    missionProgress: "0/0",
+    status: "Incomplete"
+  },
+  {
+    code: "newbie-mission-t4",
+    description: "Tích lũy tổng nạp tiền 3,500 VNDP, tích lũy tổng tiền cược hợp lệ 7,000 VNDP.",
+    reward: 188,
+    icon: walletSvg,
+    missionProgress: "0/0",
+    status: "Incomplete"
   }
-
-  return [
-      {
-        code: "newbie-mission-t1",
-        description: "Cập nhật thông tin tài khoản ngân hàng, xác nhận số điện thoại.",
-        reward: 8,
-        icon: AccountSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t2",
-        description: "Thực hiện rút tiền thành công 1 lần.",
-        reward: 28,
-        icon: transferSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t3",
-        description: "Tích lũy tổng nạp tiền 700 VNDP, có tổng cộng 20 vé cược hợp lệ tại Thể Thao hoặc Live casino.",
-        reward: 38,
-        icon: depositSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      },
-      {
-        code: "newbie-mission-t4",
-        description: "Tích lũy tổng nạp tiền 3,500 VNDP, tích lũy tổng tiền cược hợp lệ 7,000 VNDP.",
-        reward: 188,
-        icon: walletSvg,
-        missionProgress: "0/0",
-        status: "Incomplete"
-      }
-    ]
-})
+]);
 
 const handleClaimClick = (mission, index) => {
   if (mission.status !== "Claim") return;
