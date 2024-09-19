d65cb7382a1955dc4ba7b503882a10b25c2e4d09
neil yang <neil.yang@relaistech.net>
2024-09-19 16:18:20 +0800

    lh-static-h5 lh1-lol-s14 promo

--- lh-static-h5/src/components/HotPromotion.vue
+++ lh-static-h5/src/components/HotPromotion.vue
@@ -96,6 +96,7 @@
     <BbdachaCsgo2 v-if="list.redirectUrl === 'lh-bbdacha-csgo2'" :promo-code="list.promoCode"/>
     <EslProCsgo2 v-if="list.redirectUrl === 'lh-eslpro-csgo2'" :promo-code="list.promoCode"/>
     <NationalDay2024 v-if="list.redirectUrl === 'lh1-national-day-2024'" :promo-code="list.promoCode" />
+    <LoLS14 v-if="list.redirectUrl === 'lh1-lol-s14'" :promo-code="list.promoCode" />
     <Dota2Ti13 v-if="list.redirectUrl === 'lh1-dota2-ti13'" :promo-code="list.promoCode"/>
     <BlackMythWuKongPromo v-if="list.redirectUrl === 'lh-blackmyth-wukong'"/>
     <DailiPromo v-if="list.redirectUrl === 'lh1-all-daili'" :params="list.param"/>
@@ -210,6 +211,7 @@ const NewFootball = defineAsyncComponent(() => import("../components/hotpromo/Ne
 const BbdachaCsgo2 = defineAsyncComponent(() => import("../components/hotpromo/bbdacha-csgo2/BbdachaCsgo2.vue"));
 const EslProCsgo2 = defineAsyncComponent(() => import("../components/hotpromo/eslpro-csgo2/EslProCsgo2.vue"));
 const NationalDay2024 = defineAsyncComponent(() => import("../components/hotpromo/national-day-2024/NationalDay2024.vue"));
+const LoLS14 = defineAsyncComponent(() => import("../components/hotpromo/lol-s14/LoLS14.vue"));
 const Dota2Ti13 = defineAsyncComponent(() => import("../components/hotpromo/dota2-ti13/Dota2Ti13.vue"));
 const BlackMythWuKongPromo = defineAsyncComponent(() => import("./hotpromo/blackMythWuKong/BlackMythWuKongPromo.vue"));
 const SubmitClaimPromo = defineAsyncComponent(() => import("../components/hotpromo/submitclaim/SubmitClaimPromo.vue"));
@@ -278,6 +280,7 @@ export default defineComponent({
     BbdachaCsgo2,
     EslProCsgo2,
     NationalDay2024,
+    LoLS14,
     Dota2Ti13,
     BlackMythWuKongPromo,
     SubmitClaimPromo,
