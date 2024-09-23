39c23d5451338522a34729c8352164bddde0b9fc
Keng Hong <kenghongho@preec.net>
2024-09-23 17:31:37 +0800

    feat: [ind1, ind2] update hot games api

--- ind-static-v2/src/pages/HomePage.vue
+++ ind-static-v2/src/pages/HomePage.vue
@@ -2016,8 +2016,9 @@ const loadHotGameList = () => {
   cached
     .get(key2, () =>
       api
-        .get("/member/hot", {
+      .get("/sitePlatformAndGamesByLabel", {
           params: {
+            gameLabel: "HOT",
             device: regDevice
           }
         })
@@ -2036,7 +2037,7 @@ const loadHotGameList = () => {
       cached
         .get(key, () =>
           api
-            .get("/platformGamesByLabel", {
+            .get("/platformGamesByLabelV1", {
               params: {
                 gameLabel: "HOT",
                 device: regDevice
