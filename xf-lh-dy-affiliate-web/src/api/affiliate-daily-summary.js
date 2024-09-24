76cc4c0292c6b618a9dd536108c66eb2bf31d2ec
SHIAN\dcshi <dcshian85@hotmail.com>
2024-09-24 17:45:12 +0800

    remove the get total to faster the load speed

--- xf-lh-dy-affiliate-web/src/api/affiliate-daily-summary.js
+++ xf-lh-dy-affiliate-web/src/api/affiliate-daily-summary.js
@@ -30,16 +30,7 @@ export const queryDailySummaryTotal = form => {
 
 export const queryPh1DailySummary = form => {
   return https().request(
-    '/ph-affiliate-daily-summary',
-    Method.GET,
-    form,
-    ContentType.form
-  )
-}
-
-export const queryPh1DailySummaryTotal = form => {
-  return https().request(
-    '/ph-affiliate-daily-summary/getTotal',
+    '/ph-affiliate-daily-summary/list',
     Method.GET,
     form,
     ContentType.form
