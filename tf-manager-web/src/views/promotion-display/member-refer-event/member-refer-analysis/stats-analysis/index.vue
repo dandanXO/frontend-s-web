00e9deabed30624042681837e3eecd363b58b8f3
WeiSeng <weiseng@eagrp.net>
2024-09-25 14:45:40 +0800

    feat: MANAGE :deep to :deep()_

--- tf-manager-web/src/views/promotion-display/member-refer-event/member-refer-analysis/stats-analysis/index.vue
+++ tf-manager-web/src/views/promotion-display/member-refer-event/member-refer-analysis/stats-analysis/index.vue
@@ -243,7 +243,7 @@ onMounted(async () => {
   justify-content: flex-end;
 }
 
-.el-form-item--level-color:deep .el-form-item__content {
+.el-form-item--level-color:deep(.el-form-item__content) {
   display: flex !important;
 }
 .el-overlay-dialog .el-image {
--- tf-manager-web/src/views/report/affiliate-report/index.vue
+++ tf-manager-web/src/views/report/affiliate-report/index.vue
@@ -542,7 +542,7 @@ onMounted(async () => {
   padding: 4px 0;
 }
 
-.el-input-number:deep .el-input__inner {
+.el-input-number:deep(.el-input__inner) {
   text-align: left;
 }
 </style>
