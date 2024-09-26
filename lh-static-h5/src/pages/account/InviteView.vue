854894ec654c91dc98d43f342becba7f80ed06d9
Kai Wen <kaiwen@caspen-tech.com>
2024-09-26 11:47:53 +0800

    fix: [lh h5] Invite page 活动详情 button cater for app redirection to promo page

--- lh-static-h5/src/pages/account/InviteView.vue
+++ lh-static-h5/src/pages/account/InviteView.vue
@@ -7,7 +7,7 @@
           <div class="top-line1-content">
             您通过推广链接邀请的用户注册并存款，您将获得最高累计
             <span class="prize-span">2,000</span>
-            元的奖励。<router-link to="/promo?name=lh1-invite">活动详情</router-link>
+            元的奖励。<span class="promo-details-btn" @click="onClickPromoDetailsBtn">活动详情</span>
           </div>
         </div>
       </div>
@@ -123,7 +123,7 @@ import VueQRCodeComponent from 'vue-qrcode-component'
 import {userStore} from "src/stores";
 import {useQuasar, Platform, scroll} from "quasar";
 import {api, eventapi} from "boot/axios"
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useNotify } from "src/hooks/notify";
 
 export default defineComponent({
@@ -135,6 +135,7 @@ export default defineComponent({
     const notify = useNotify();
     const { getScrollTarget, setVerticalScrollPosition } = scroll;
     const route = useRoute();
+    const router = useRouter();
     const $q = useQuasar();
     const store = userStore();
     const selfTgurl = ref("");
@@ -178,6 +179,16 @@ export default defineComponent({
         });
       }, 100)
 
+    }
+
+    const onClickPromoDetailsBtn = () => {
+      if (window.location.pathname === "/invitefriend") {
+        window.location.href = `/promotion?name=lh1-summon-event&token=${store.token}`
+        return;
+      }
+
+      router.push('/promo?name=lh1-invite');
+      return;
     }
 
     async function copyToClipboard(textToCopy) {
@@ -257,7 +268,8 @@ export default defineComponent({
       SummonUrl,
       SummonQrCode,
       refTotalSummon,
-      store
+      store,
+      onClickPromoDetailsBtn
     }
   }
 });
@@ -273,9 +285,15 @@ export default defineComponent({
     &.grad {
       background: linear-gradient(180deg, #4ad8fc, #3f9dff);
       color: #ffffff;
+
       .qr-title, .prize-span, .top-line1-content a {
         color: #ffffff;
       }
+    }
+
+    .promo-details-btn {
+      cursor: pointer;
+      text-decoration: underline;
     }
   }
 
