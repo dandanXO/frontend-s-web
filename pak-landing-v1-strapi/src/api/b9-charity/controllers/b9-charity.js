1eb81aacf7012f33964fba892900c8b41fb9434d
zhixuan <zhixuan@preec.net>
2024-09-30 13:35:40 +0800

    Initial commit

---
+++ pak-landing-v1-strapi/src/api/b9-charity/controllers/b9-charity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * b9-charity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::b9-charity.b9-charity');
