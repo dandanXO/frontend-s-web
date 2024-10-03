692039d9be268c7438cf96d28c25f5f27101ac2f
Mika <mika@eagrp.net>
2024-10-03 15:40:04 +0800

    fix: change skype contact.

--- xf-lh-dy-affiliate-web/src/components/customer-service/index.vue
+++ xf-lh-dy-affiliate-web/src/components/customer-service/index.vue
@@ -116,7 +116,7 @@ const initContactList = () => {
     {
       icon: 'cskype',
       type: t('common.skype'),
-      link: 'live:.cid.1b8d9a018a52a8f5',
+      link: 'live:.cid.1a1ab9b6b5b0721f',
       btns: [{
         text: t('common.copy'),
         action: ''
