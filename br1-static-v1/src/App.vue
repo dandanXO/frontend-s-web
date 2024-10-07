e0072d84613f8a6433a614951a8935bc16c045a2
Keng Hong <kenghongho@preec.net>
2024-10-07 11:15:55 +0800

    feat: [br1] update api for memberStatistics submit

--- br1-static-v1/src/App.vue
+++ br1-static-v1/src/App.vue
@@ -259,10 +259,8 @@ export default defineComponent({
       console.log(theSid);
 
       if (theSid) {
-        const res = await axios.post(
-          "https://memberstatistics.j9zwvu1ogrg.com/memberStatistics/submit",
-        // const res = await api.post(
-        //   "/memberStatistics/submit",
+        const res = await api.post(
+          "/memberStatistics/submit",
           qs.stringify({
             way: way,
             sid: theSid,
