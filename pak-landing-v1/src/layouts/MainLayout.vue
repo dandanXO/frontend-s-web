5bc08d519a67acf72cfee835c6e379893afe3ce8
zhixuan <zhixuan@preec.net>
2024-10-08 18:09:32 +0800

    feat: add anim on register btn

--- pak-landing-v1/src/layouts/MainLayout.vue
+++ pak-landing-v1/src/layouts/MainLayout.vue
@@ -85,6 +85,7 @@ const openUrl = () => {
   height: 34px;
   width: 80px;
   letter-spacing: 0.5px;
+  animation: smallbeat 2s infinite;
 }
 .q-toolbar {
   padding: 0;
@@ -136,5 +137,29 @@ const openUrl = () => {
 :deep(.q-tabs__content) {
   overflow: auto;
   white-space: nowrap;
+}
+
+@keyframes smallbeat {
+  0% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+  14% {
+    -webkit-transform: scale(1.15);
+    transform: scale(1.15);
+  }
+
+  28% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+  42% {
+    -webkit-transform: scale(1.15);
+    transform: scale(1.15);
+  }
+  70% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
 }
 </style>
