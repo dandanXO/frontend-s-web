5e2db40507451e1f70ec0b237ee99421c7c9d2c4
Mika <mika@eagrp.net>
2024-10-08 22:12:14 +0800

    fix: [lh h5] add message

--- lh-static-h5/src/pages/mail/InboxView.vue
+++ lh-static-h5/src/pages/mail/InboxView.vue
@@ -4,9 +4,10 @@
   </div>
 </template>
 <script lang="js">
-import {defineComponent, onMounted, ref} from "vue";
+import {defineComponent, onActivated, onMounted, ref} from "vue";
 import {api} from "boot/axios";
 import MailComponent from "../../components/MailComponent.vue";
+import { userStore } from "stores/index";
 
 
 export default defineComponent({
@@ -14,6 +15,7 @@ export default defineComponent({
     MailComponent
   },
   setup() {
+    const store = userStore();
     const visible = ref(true);
     const mailData = ref([]);
     const mailboxData = ref({
@@ -49,6 +51,9 @@ export default defineComponent({
       loadInbox()
     })
 
+    onActivated(() => {
+      store.getUnreadTotal();
+    })
     return {
       visible,
       mailData,
