3e8c73a4f17519bae0461f7179f67ebb7dae7ec9
Keng Hong <kenghongho@preec.net>
2024-10-09 14:01:38 +0800

    feat: [pk3] translation and env

--- pk3-static-v3/src/i18n/index.js
+++ pk3-static-v3/src/i18n/index.js
@@ -1,13 +1,11 @@
 import { createI18n } from "vue-i18n";
 import en from "./en";
-import id from "./id";
 
 const i18n = createI18n({
   legacy: false,
-  locale: localStorage.getItem("languageLocale") || "id",
+  locale: localStorage.getItem("languageLocale") || "en",
   allowComposition: true,
   messages: {
-    id,
     en
   },
   globalInjection: true
