87f80d181da1a593321b020c86554af3475bf7da
Keng Hong <kenghongho@preec.net>
2024-10-15 11:34:13 +0800

    feat: [ind1] PromoDetail open InAppBrowser

--- ind-static-h5/src/router/index.js
+++ ind-static-h5/src/router/index.js
@@ -6,6 +6,8 @@ import { createRouter, createMemoryHistory, createWebHistory, createWebHashHisto
 import routes from "./routes";
 import { StatusBar } from "@capacitor/status-bar";
 import { Platform } from "quasar";
+import { isAndroid } from "boot/utils";
+import { SessionStorage } from "quasar";
 
 /*
  * If not building with SSR mode, you can
@@ -44,11 +46,22 @@ export default route(function (/* { store, ssrContext } */) {
       to.path === "/login" ||
       to.path === "/register" ||
       to.path === "/forgot-password" ||
-      to.path === "/maintenance"
+      to.path === "/maintenance" ||
+      to.path === "/promotion"
     ) {
       ui.hiddenFooter();
     } else {
       ui.showFooter();
+    }
+
+    if (to.path === "/promotion") {
+      // debugger;
+      if (isAndroid()) {
+        localStorage.setItem("TOKEN", to.query.token);
+      } else {
+        SessionStorage.set("TOKEN", to.query.token);
+      }
+      user.token = to.query.token;
     }
 
     if (Platform.is.capacitor && Platform.is.android) {
