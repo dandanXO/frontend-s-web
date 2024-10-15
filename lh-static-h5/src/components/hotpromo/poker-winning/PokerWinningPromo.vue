85366644616a553ce2e35f57d15502b172f31b5c
Sam <sam@flex-df.com>
2024-10-15 16:33:26 +0800

    fix: [lh landing-v6] Change contact info

---
+++ lh-static-h5/src/components/hotpromo/poker-winning/PokerWinningPromo.vue
@@ -0,0 +1,357 @@
+<template>
+  <div class="livepoker-rebate-wrapper">
+    <div class="livepoker-rebate-container">
+      <div class="livepoker-rebate-game-info">
+        <div class="title"></div>
+        <div class="little-title">
+          <div class="left">活动时间</div>
+          <div class="right">长期活动</div>
+        </div>
+        <div class="little-title" style="flex-direction: column; justify-content: flex-start; align-items: flex-start">
+          <div class="left">活动内容</div>
+          <div class="right">每日棋牌场馆单局投注≥50元，所投注牌型中包含下列特殊牌型即可领取牌型礼金。</div>
+        </div>
+
+        <table class="livepoker-rebate-game-info-table">
+          <tbody>
+            <tr>
+              <th>棋牌游戏特殊牌型</th>
+              <th>奖金</th>
+              <th>流水倍数</th>
+            </tr>
+            <tr>
+              <td>牛牛：五花牛</td>
+              <td>18</td>
+              <td rowspan="8">3倍</td>
+            </tr>
+            <tr>
+              <td>牛牛：五小牛</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>炸金花：豹子</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>炸金花：同花顺</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>牌九：至尊宝</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>二八杠：二八</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>德州扑克：皇家同花顺</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>三公：爆九</td>
+              <td>18</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="livepoker-rebate-game-bottom-rule">
+        <div class="title"></div>
+        <div class="content">
+          <div class="item">
+            <div class="item-num">1</div>
+            申请方式：棋牌场馆单局投注50元以上，可截图保留牌型记录或注单号，请在符合活动要求的当天23:59点前登录在线客服进行申请，有效注单次日失效；
+          </div>
+          <div class="item">
+            <div class="item-num">2</div>
+            每位玩家当日只可申请一次，彩金3倍流水即可提款。特殊牌型仅当天有效，历史特殊牌型不计算在内；
+          </div>
+          <div class="item">
+            <div class="item-num">3</div>
+            同一手机号、姓名、邮箱地址、银行卡号、IP
+            地址等身份认证信息视为同一账号，仅限一个账号参与、任何团体或个人以非法方式套取优惠（如投注对冲等），平台保留在不提前通知情况下做出处理；
+          </div>
+          <div class="item">
+            <div class="item-num">4</div>
+            为避免文字理解差异，本站保留本活动最终解释权；
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.livepoker-rebate-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.livepoker-rebate-container {
+  width: 100%;
+  height: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.livepoker-rebate-section {
+  box-shadow: 0px 0px 4px 0px #01497b0f;
+  padding: 20px 12px 40px;
+  border-radius: 12px;
+  border: 1px solid #acd4f6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: url("../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/section-bg.png");
+  background-size: 100% 100%;
+  align-items: center;
+  width: 100%;
+
+  .livepoker-rebate-section-left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .livepoker-rebate-section-right {
+    width: 180px;
+    margin-top: 20px;
+
+    .bonus-image {
+      width: 100%;
+      cursor: pointer;
+
+      &:active {
+        filter: brightness(0.85);
+        transform: translate(0px, 1px);
+      }
+    }
+  }
+
+  .livepoker-rebate-section-title {
+    color: #000000;
+    font-size: 16px;
+    line-height: 1;
+    font-weight: 600;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+}
+
+.livepoker-rebate-game {
+  width: 100%;
+  height: 302px;
+  border-radius: 12px;
+  // border: 1px solid #51acff;
+  background-color: #fff;
+  position: relative;
+  margin-bottom: 12px;
+}
+
+.livepoker-rebate-game-bottom {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .livepoker-rebate-game-bottom-left-title {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 22.4px;
+    color: #ff0000;
+  }
+  .livepoker-rebate-game-bottom-left-btn {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 22.4px;
+    color: #ff0000;
+    cursor: pointer;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+  }
+}
+
+.livepoker-rebate-game-info {
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+  background: #f2f8fe;
+  border-radius: 12px;
+  padding: 20px 12px 12px;
+  border: 1px solid #acd4f6;
+  box-shadow: 0px 0px 4px 0px #01497b0f;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  .title {
+    background-image: url("../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/info-title.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    width: 240px;
+    height: 26px;
+    margin: 0 auto 20px;
+  }
+  .little-title {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+    .left {
+      background-image: url("../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/info-little-title-bg.png");
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      width: 64px;
+      height: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 12px;
+      font-weight: 600;
+      line-height: 23.33px;
+      color: #ffffff;
+      margin-right: 16px;
+    }
+    .right {
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 28px;
+      color: #000000;
+    }
+  }
+}
+
+.livepoker-rebate-game-info-table {
+  width: 100%;
+  height: 100%;
+  border-collapse: separate;
+  border-spacing: 0;
+  text-align: center;
+  vertical-align: middle;
+  th {
+    height: 36px;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 28px;
+    color: #fff;
+    background: linear-gradient(180deg, #70cbfb 0%, #4aa5ff 49%, #4aa5ff 91.5%, #6ec7fd 100%);
+    &:first-child {
+      border-top-left-radius: 12px;
+    }
+    &:last-child {
+      border-top-right-radius: 12px;
+    }
+  }
+  tr {
+    &:last-child {
+      td {
+        &:first-child {
+          // border-bottom-left-radius: 12px;
+        }
+      }
+    }
+    &:nth-child(2) {
+      td {
+        &:last-child {
+          // border-bottom-right-radius: 12px;
+        }
+      }
+    }
+  }
+  td {
+    background: transparent;
+    border: 1px solid #acd4f6;
+    height: 36px;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 28px;
+    color: #000000;
+  }
+}
+
+.livepoker-rebate-game-bottom-rule {
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+  background: #f2f8fe;
+  border-radius: 12px;
+  padding: 20px 12px 12px;
+  border: 1px solid #acd4f6;
+  box-shadow: 0px 0px 4px 0px #01497b0f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title {
+    background-image: url("../../../assets/images/promotion/hotpromo/lh-livepoker-rebate/rule-title.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    width: 240px;
+    height: 20px;
+    margin-bottom: 20px;
+  }
+  .content {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 20px;
+    color: #000000;
+    padding: 8px;
+    .item {
+      display: flex;
+      gap: 10px;
+      align-items: baseline;
+
+      .item-num {
+        color: #ffffff;
+        font-size: 12px;
+        line-height: 1;
+        border-radius: 50%;
+        height: 16px !important;
+        width: 16px !important;
+        min-width: 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 2px;
+        background: linear-gradient(180deg, #70cbfb 0%, #4aa5ff 49%, #4aa5ff 91.5%, #6ec7fd 100%);
+      }
+    }
+  }
+}
+
+.reward-info {
+  border: 1px solid rgba(215, 235, 255, 1);
+  padding: 8px;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+}
+
+.reward-info-icon {
+  width: 16px;
+  height: 16px;
+  margin-right: 10px;
+}
+
+.reward-info-content {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  color: black;
+
+  .amount {
+    color: #00a1ff;
+    font-weight: 600;
+  }
+}
+</style>
