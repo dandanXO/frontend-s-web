189728568aefb5ac898b1032b18eafff0d00dce9
Mika <mika@eagrp.net>
2024-10-16 19:50:00 +0800

    fix: [lh] update data loading

--- lh-static-web/src/views/VIPView.vue
+++ lh-static-web/src/views/VIPView.vue
@@ -164,8 +164,8 @@
           >
             {{ isLoading["all"] ? "领取中" : "一键领取" }}
           </div> -->
-          <div class="amount">
-            <div class="text" v-show="isDataLoaded">
+          <div class="amount" v-show="isDataLoaded">
+            <div class="text">
                 保级剩余天数：<span class="required-amount">{{ balanceRetainDay }}</span> 天
               </div>
               <div class="progressBarContainer">
