ab308a921de7466b4a7eba47defc877bb510660b
darren <darren@eagrp.net>
2024-10-18 14:14:44 +0800

    feat: 宝箱活动

---
+++ tf-manager-web/src/views/promotion-display/treasure/claim-record/index.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="roles-main">
+    <div class="header-container">
+      <div class="search">
+        <el-select
+          v-model="request.siteId"
+          size="small"
+          :placeholder="t('fields.site')"
+          class="filter-item"
+          style="width: 200px;"
+          @focus="loadSites"
+        >
+          <el-option
+            v-for="item in sites.list"
+            :key="item.id"
+            :label="item.siteName"
+            :value="item.id"
+          />
+        </el-select>
+        <el-date-picker
+          v-model="request.receiveTime"
+          format="DD/MM/YYYY"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          size="small"
+          type="daterange"
+          :start-placeholder="t('fields.startDate')"
+          :end-placeholder="t('fields.endDate')"
+          style="width: 280px; margin-left: 10px;"
+          :shortcuts="shortcuts"
+          :editable="false"
+          :clearable="false"
+          :default-time="defaultTime"
+        />
+        <el-input
+          v-model="request.loginName"
+          size="small"
+          style="width: 200px; margin-left: 10px;"
+          :placeholder="t('fields.loginName')"
+        />
+        <el-button style="margin-left: 20px" icon="el-icon-search" size="mini" type="success" @click="loadClaimRecord">
+          {{ t('fields.search') }}
+        </el-button>
+        <el-button icon="el-icon-refresh" size="mini" type="warning" @click="resetQuery()">
+          {{ t('fields.reset') }}
+        </el-button>
+      </div>
+    </div>
+    <el-table
+      :data="page.records"
+      ref="table"
+      row-key="id"
+      size="small"
+      v-loading="page.loading"
+      highlight-current-row
+      :empty-text="t('fields.noData')"
+    >
+      <el-table-column prop="loginName" :label="t('fields.loginName')" min-width="300" />
+      <el-table-column prop="treasure" :label="t('fields.treasure')" min-width="300">
+        <template #default="scope">
+          <span>{{ t('treasure.' + scope.row.treasure) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="keyUsed" :label="t('fields.keyUsed')" min-width="200" />
+      <el-table-column prop="bonus" :label="t('fields.bonus')" min-width="200">
+        <template #default="scope">
+          $ <span v-formatter="{data: scope.row.bonus, type: 'money'}" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="receiveTime" :label="t('fields.receiveTime')" min-width="400">
+        <template #default="scope">
+          <span v-if="scope.row.receiveTime === null">-</span>
+          <!-- eslint-disable -->
+          <span
+            v-if="scope.row.receiveTime !== null"
+            v-formatter="{
+              data: scope.row.receiveTime,
+              timeZone: timeZone,
+              type: 'date',
+            }"
+          />
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      :total="page.total"
+      :page-sizes="[20, 50, 100, 150]"
+      layout="total,sizes,prev, pager, next"
+      class="pagination"
+      v-model:page-size="request.size"
+      v-model:page-count="page.pages"
+      v-model:current-page="request.current"
+      @current-change="loadClaimRecord"
+      @size-change="loadClaimRecord"
+    />
+  </div>
+</template>
+
+<script setup>
+
+import { computed, reactive, ref } from "vue";
+import { getSiteListSimple } from "@/api/site";
+import { getTreasureClaimRecord } from "@/api/treasure";
+import { onMounted } from "@vue/runtime-core";
+import { useStore } from '@/store';
+import { TENANT } from "@/store/modules/user/action-types";
+import { useI18n } from "vue-i18n";
+import { getShortcuts } from "@/utils/datetime";
+import moment from "moment";
+
+const { t } = useI18n();
+const store = useStore();
+const LOGIN_USER_TYPE = computed(() => store.state.user.userType);
+const site = ref(null);
+let timeZone = null
+const sites = reactive({
+  list: []
+});
+const promoCode = ref('-blast-premier-treasure')
+
+const defaultTime = [
+  new Date(2000, 1, 1, 0, 0, 0),
+  new Date(2000, 1, 1, 23, 59, 59),
+];
+const shortcuts = getShortcuts(t);
+
+function convertDate(date) {
+  return moment(date).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+}
+
+function convertStartDate(date) {
+  return moment(date).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+}
+
+const request = reactive({
+  size: 20,
+  current: 1,
+  siteId: null,
+  loginName: null,
+  receiveTime: [convertStartDate(new Date()), convertDate(new Date())]
+});
+
+const page = reactive({
+  pages: 0,
+  records: [],
+  total: 0,
+  loading: false
+});
+
+async function loadClaimRecord() {
+  page.loading = true;
+  const requestCopy = { ...request };
+  const query = {};
+  Object.entries(requestCopy).forEach(([key, value]) => {
+    if (value) {
+      query[key] = value;
+    }
+  });
+  if (request.receiveTime !== null) {
+    if (request.receiveTime.length === 2) {
+      query.receiveTime = request.receiveTime.join(",");
+    }
+  }
+  const siteCode = sites.list.find(s => s.id === query.siteId).siteCode;
+  query.promoCode = siteCode.toLocaleLowerCase() + promoCode.value;
+  const { data: ret } = await getTreasureClaimRecord(query);
+  page.pages = ret.pages;
+  page.records = ret.records;
+  timeZone = sites.list.find(e => e.id === request.siteId).timeZone
+  page.total = ret.total;
+  page.loading = false;
+}
+
+async function loadSites() {
+  const { data: site } = await getSiteListSimple();
+  sites.list = site;
+}
+
+function resetQuery() {
+  request.siteId = site.value.id;
+  request.loginName = null;
+  request.receiveTime = [convertStartDate(new Date()), convertDate(new Date())];
+}
+
+onMounted(async () => {
+  await loadSites();
+  if (LOGIN_USER_TYPE.value === TENANT.value) {
+    site.value = sites.list.find(s => s.siteName === store.state.user.siteName);
+  } else {
+    site.value = sites.list[0];
+  }
+  request.siteId = site.value.id;
+  await loadClaimRecord();
+});
+
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.header-container {
+  margin-bottom: 10px;
+}
+
+.search {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.btn-group {
+  margin-top: 15px;
+}
+
+.dialog-footer {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.el-form-item--level-color:deep(.el-form-item__content) {
+  display: flex !important;
+}
+</style>
