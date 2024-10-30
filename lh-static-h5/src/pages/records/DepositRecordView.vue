071240dd9d2fa2061581cdcdb75fb719494637ad
zhixuan <zhixuan@preec.net>
2024-10-30 14:56:04 +0800

    feat: add date filter for records (deposit), add record search component

--- lh-static-h5/src/pages/records/DepositRecordView.vue
+++ lh-static-h5/src/pages/records/DepositRecordView.vue
@@ -1,12 +1,14 @@
 <template>
+  <RecordSearch class="q-my-sm" :startDate="startDate" :endDate="endDate" @handleDateChange="handleDateChange" />
   <div class="table-record">
     <RecordComponent
-        recordType="deposit"
-        :loading="visible"
-        :list="tableData"
-        :headers="tableHeaders"
-        @loadnewdata="loadNewData"
-        :isEnded="isEnded"
+      ref="recordRef"
+      recordType="deposit"
+      :loading="visible"
+      :list="tableData"
+      :headers="tableHeaders"
+      @loadnewdata="loadNewData"
+      :isEnded="isEnded"
     />
   </div>
 </template>
@@ -15,11 +17,13 @@ import { defineComponent, onMounted, ref } from "vue";
 import RecordComponent from "../../components/RecordComponent.vue";
 import { api } from "boot/axios";
 import moment from "moment";
-import { cached, TIME_EXPIRED } from "boot/cache";
+import { cached } from "boot/cache";
+import RecordSearch from "src/components/RecordSearch.vue";
 
 export default defineComponent({
   components: {
-    RecordComponent
+    RecordComponent,
+    RecordSearch
   },
   setup() {
 
@@ -29,27 +33,18 @@ export default defineComponent({
 
     var apiUrl = "/session/member/deposit";
 
-    var endDate = moment().format("YYYY-MM-DD");
-    var startDate = moment().add(-7, "days").format("YYYY-MM-DD");
+    var endDate = ref(moment().format("YYYY-MM-DD"));
+    const startDate = ref(moment().add(-7, "days").format("YYYY-MM-DD"));
     var current = ref(1);
     var maxPage = ref(0);
+    const recordRef = ref();
 
     const loadNewData = () => {
-      if(maxPage.value > current.value){
+      if (maxPage.value > current.value) {
         current.value++;
-      }else {
-        current.value = 1;
-        endDate = moment(startDate).add(-1, "days").format("YYYY-MM-DD");
-        // console.log(endDate);
-
-        startDate = moment(endDate).add(-7, "days").format("YYYY-MM-DD");
-        // console.log(startDate);
-
-        if (endDate <= moment().add(-29, "days").format("YYYY-MM-DD")) {
-          // console.log("mor than 3 months");
-          isEnded.value = true;
-          return;
-        }
+      } else {
+        isEnded.value = true;
+        return;
       }
       loadDepositTable(false);
     };
@@ -58,17 +53,14 @@ export default defineComponent({
       if (isNew) {
         visible.value = true;
       }
-      // console.log(startDate);
-      // console.log(endDate);
 
       let paramData = {
-        "startDate": startDate,
-        "endDate": endDate,
+        "startDate": startDate.value,
+        "endDate": endDate.value,
         "size": 10,
         "current": current.value
       };
-      var apiKey = apiUrl + "_" + startDate + "_" + endDate + "_" + current.value;
-      // console.log(apiKey);
+      var apiKey = apiUrl + "_" + startDate.value + "_" + endDate.value + "_" + current.value;
 
       cached.get(apiKey, () => api.get(apiUrl, {
           params: paramData
@@ -82,13 +74,20 @@ export default defineComponent({
         }
 
         tableData.value.push(...res.records);
-        // console.log("TableData");
-        // console.log(tableData.value);
       }).catch((err) => {
         if (isNew) {
           visible.value = false;
         }
       });
+    };
+
+    const handleDateChange = (data) => {
+      const {val, isStartDate} = data
+      isStartDate ? startDate.value = val : endDate.value = val
+      tableData.value = [];
+      isEnded.value = false;
+      recordRef.value.clearTable();
+      loadDepositTable(true);
     };
 
     const tableHeaders = [
@@ -124,7 +123,11 @@ export default defineComponent({
       isEnded,
       tableHeaders,
       loadNewData,
-      tableData
+      tableData,
+      startDate,
+      endDate,
+      recordRef,
+      handleDateChange
     };
   }
 });
