96ca9f3f001a6e4a57c613e957148b7ea175c4db
Kai Wen <kaiwen@caspen-tech.com>
2024-11-07 16:04:49 +0800

    fix: [lh web, lh h5] Move promo content to backend for lh1-valorant-champion-tour-2024 promo

--- lh-static-h5/src/components/hotpromo/valorant-champion-tour-2024/ValorantChampionTour2024.vue
+++ lh-static-h5/src/components/hotpromo/valorant-champion-tour-2024/ValorantChampionTour2024.vue
@@ -45,116 +45,6 @@
           </div>
         </div>
       </div>
-      <div class="livepoker-rebate-game-info">
-        <div class="title"></div>
-
-        <div class="little-title">
-          <div class="left">活动时间</div>
-          <div class="right">2024年11月8日-11月18日00:00</div>
-        </div>
-
-        <div class="little-title" style="flex-direction: column; justify-content: flex-start; align-items: flex-start">
-          <div class="left">活动内容</div>
-          <div class="right">
-            活动期间，投注无畏契约改变者全球冠军赛赛事有效投注≥2,000元即可获得领取彩金条件，最高可领1,888元
-          </div>
-        </div>
-        <table class="livepoker-rebate-game-info-table">
-          <tr>
-            <th>有效投注</th>
-            <th>投注嘉奖</th>
-          </tr>
-          <tr>
-            <td>≥2,000</td>
-            <td>8元</td>
-          </tr>
-          <tr>
-            <td>≥5,000</td>
-            <td>18元</td>
-          </tr>
-          <tr>
-            <td>≥10,000</td>
-            <td>38元</td>
-          </tr>
-          <tr>
-            <td>≥30,000</td>
-            <td>88元</td>
-          </tr>
-          <tr>
-            <td>≥50,000</td>
-            <td>188元</td>
-          </tr>
-          <tr>
-            <td>≥100,000</td>
-            <td>328元</td>
-          </tr>
-          <tr>
-            <td>≥200,000</td>
-            <td>588元</td>
-          </tr>
-          <tr>
-            <td>≥500,000</td>
-            <td>1,088元</td>
-          </tr>
-          <tr>
-            <td>≥1,000,000</td>
-            <td>1,888元</td>
-          </tr>
-        </table>
-        <div class="livepoker-rebate-game-bottom">
-          <div class="livepoker-rebate-game-bottom-left-title">
-            <div class="livepoker-rebate-game-bottom-left-btn">
-              <img
-                src="../../../assets/images/promotion/hotpromo/lh1-blast-premier/game-bottom-left-btn.png"
-                alt=""
-                style="width: 10px"
-              />
-              <span>示例</span>
-            </div>
-            会员A投注无畏契约改变者全球冠军赛有效投注6,000元，会员可在次日领取18元彩金
-          </div>
-        </div>
-      </div>
-
-      <div class="livepoker-rebate-game-bottom-rule">
-        <div class="title"></div>
-        <div class="content">
-          <div class="item">
-            <div class="item-num">1</div>
-            <div style="display: flex; flex-direction: column">
-              <div>
-                活动期间，会员在电竞场馆投注无畏契约改变者全球冠军赛有效投注≥2,000的会员在活动页面可领取8元嘉奖奖金；
-              </div>
-              <div class="hint">注：奖金不叠加派发，奖金按最高档位每日派发一次。</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="item-num">2</div>
-            活动期间，仅计算投注电竞场馆，符合领取条件的会员，请在次日24小时内在活动页面点击【点击领取】按钮领取彩金。彩金5倍流水即可提款；
-          </div>
-          <div class="item">
-            <div class="item-num">3</div>
-            本活动有效投注额仅对已结算并产生输赢结果的投注额进行计算，任何滚球、走水、串关、提前结算的投注、取消的赛事将不计算在有效投注；
-          </div>
-          <div class="item">
-            <div class="item-num">4</div>
-            任何低于欧洲盘 1.7 或亚洲盘 0.7
-            水位的投注及在同一局游戏中同时投注对等盘口、当日注单取消或本金退还,将不计算为有效投注额内；
-          </div>
-          <div class="item">
-            <div class="item-num">5</div>
-            根据博彩公平有序规则，任何用户或团体以不正常的方式进行投注，如有风险投注、对赌行为或欺骗方式，本站保留权力在不通知的情况下冻结或关闭相关账户；
-          </div>
-          <div class="item">
-            <div class="item-num">6</div>
-            此活动只适用于拥有一个账户的会员，每一个住址、每一个电子邮箱地址、每一个电话号码、相同支付方式及IP地址视为同一账户，若有违规者，将不享受此红利；
-          </div>
-          <div class="item">
-            <div class="item-num">7</div>
-            为避免文字理解差异，雷火电竞保留此活动最终解释权；
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -257,7 +147,7 @@ onMounted(() => {
 
 .livepoker-rebate-section {
   box-shadow: 0px 0px 4px 0px #01497b0f;
-  padding: 20px 12px 40px;
+  padding: 1rem 1.2rem;
   border-radius: 12px;
   border: 1px solid #acd4f6;
   display: flex;
@@ -267,6 +157,7 @@ onMounted(() => {
   background-size: 100% 100%;
   align-items: center;
   width: 100%;
+  font-family: 'PingFang SC', 'PingFang', sans-serif;
 
   .livepoker-rebate-section-left {
     display: flex;
@@ -277,7 +168,6 @@ onMounted(() => {
 
   .livepoker-rebate-section-right {
     width: 180px;
-    margin-top: 20px;
 
     .bonus-image {
       width: 100%;
@@ -298,7 +188,7 @@ onMounted(() => {
 
   .livepoker-rebate-section-title {
     color: #000000;
-    font-size: 16px;
+    font-size: 1.3rem;
     line-height: 1;
     font-weight: 600;
     display: flex;
@@ -341,164 +231,6 @@ onMounted(() => {
   }
 }
 
-.livepoker-rebate-game-info {
-  width: 100%;
-  height: 100%;
-  margin-top: 40px;
-  background: #f2f8fe;
-  border-radius: 12px;
-  padding: 20px 12px 12px;
-  border: 1px solid #acd4f6;
-  box-shadow: 0px 0px 4px 0px #01497b0f;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-
-  .title {
-    background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-title.png");
-    background-repeat: no-repeat;
-    background-size: 100%;
-    width: 240px;
-    height: 26px;
-    margin: 0 auto;
-  }
-  .little-title {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 10px;
-    .left {
-      background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-little-title-bg.png");
-      background-repeat: no-repeat;
-      background-size: 100% 100%;
-      width: 64px;
-      height: 20px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 12px;
-      font-weight: 600;
-      line-height: 23.33px;
-      color: #ffffff;
-      margin-right: 16px;
-    }
-    .right {
-      font-size: 12px;
-      font-weight: 400;
-      line-height: 28px;
-      color: #000000;
-    }
-  }
-}
-
-.livepoker-rebate-game-info-table {
-  width: 100%;
-  height: 100%;
-  border-collapse: separate;
-  border-spacing: 0;
-  text-align: center;
-  vertical-align: middle;
-  th {
-    height: 36px;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 28px;
-    color: #fff;
-    background: linear-gradient(180deg, #70cbfb 0%, #4aa5ff 49%, #4aa5ff 91.5%, #6ec7fd 100%);
-    &:first-child {
-      border-top-left-radius: 12px;
-    }
-    &:last-child {
-      border-top-right-radius: 12px;
-    }
-  }
-  tr {
-    &:last-child {
-      td {
-        &:first-child {
-          // border-bottom-left-radius: 12px;
-        }
-      }
-    }
-    &:nth-child(2) {
-      td {
-        &:last-child {
-          // border-bottom-right-radius: 12px;
-        }
-      }
-    }
-  }
-  td {
-    background: transparent;
-    border: 1px solid #acd4f6;
-    height: 36px;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 28px;
-    color: #000000;
-  }
-}
-
-.livepoker-rebate-game-bottom-rule {
-  width: 100%;
-  height: 100%;
-  margin-top: 40px;
-  background: #f2f8fe;
-  border-radius: 12px;
-  padding: 20px 12px 12px;
-  border: 1px solid #acd4f6;
-  box-shadow: 0px 0px 4px 0px #01497b0f;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .title {
-    background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/rule-title.png");
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    width: 240px;
-    height: 20px;
-    margin-bottom: 20px;
-  }
-  .content {
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 20px;
-    color: #000000;
-    padding: 8px;
-    .item {
-      display: flex;
-      gap: 10px;
-      align-items: baseline;
-
-      .item-num {
-        color: #ffffff;
-        font-size: 12px;
-        line-height: 1;
-        border-radius: 50%;
-        height: 16px !important;
-        width: 16px !important;
-        min-width: 16px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 2px;
-        background: linear-gradient(180deg, #70cbfb 0%, #4aa5ff 49%, #4aa5ff 91.5%, #6ec7fd 100%);
-      }
-
-      .hint {
-        font-size: 12px;
-        font-weight: 400;
-        line-height: 22.4px;
-        color: #ff0000;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        gap: 4px;
-      }
-    }
-  }
-}
-
 .reward-info {
   border: 1px solid rgba(215, 235, 255, 1);
   padding: 8px;
@@ -509,8 +241,8 @@ onMounted(() => {
 }
 
 .reward-info-icon {
-  width: 16px;
-  height: 16px;
+  width: 18px;
+  height: 18px;
   margin-right: 10px;
 }
 
@@ -519,8 +251,8 @@ onMounted(() => {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  font-size: 12px;
-  color: black;
+  font-size: 1rem;
+  color: #333;
 
   .amount {
     color: #00a1ff;
