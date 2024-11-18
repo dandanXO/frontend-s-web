edd31443c4c036d39f40aa4677dd046baf05e2d8
Sam <sam@flex-df.com>
2024-11-18 16:33:39 +0800

    test: [pak1] Restrict alert trigger conditions

--- pak-static-h5/src/pages/account/DepositView.vue
+++ pak-static-h5/src/pages/account/DepositView.vue
@@ -796,7 +796,7 @@ async function pDepo(deposit) {
           const submitResult = res.data.result.data;
           submitMessage.value = submitResult.split(",");
         } else {
-          if (store.memberType === "TEST") {
+          if (store.memberType === "TEST" && store.nickName === "03080808080") {
             alert(navigator.userAgent);
           }
           if (isTikTokInAppBrowser()) {
