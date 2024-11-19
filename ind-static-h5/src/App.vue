3560ee961a0cd1f8698808bf96700a087dd76164
WeiSeng <weiseng@eagrp.net>
2024-11-19 11:59:52 +0800

    feat: MX1 BZ1 IND add timeout to statusbar

--- ind-static-h5/src/App.vue
+++ ind-static-h5/src/App.vue
@@ -302,7 +302,9 @@ export default defineComponent({
           "deviceready",
           () => {
             onDeviceReady();
-            setStatusBarColor();
+            setTimeout(() => {
+              setStatusBarColor();
+            }, 1000);
           },
           false
         );
