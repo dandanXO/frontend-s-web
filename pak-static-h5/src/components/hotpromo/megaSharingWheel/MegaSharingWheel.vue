ba78bcef72e0fb948765effccc1d51a58bf03a15
zhixuan <zhixuan@preec.net>
2024-11-28 16:13:55 +0800

    fix: completed not showing when mission is finished, enable persistent for claim success dialog - prevent spinning of wheels multiple times

--- pak-static-h5/src/components/hotpromo/megaSharingWheel/MegaSharingWheel.vue
+++ pak-static-h5/src/components/hotpromo/megaSharingWheel/MegaSharingWheel.vue
@@ -25,7 +25,7 @@
     </div>
   </div>
 
-  <q-dialog v-model="showPrizePopup" backdrop-filter="none">
+  <q-dialog v-model="showPrizePopup" backdrop-filter="none" persistent>
     <div class="congrats-container">
       <div class="congrats-header">
         <span class="congrats-amt">{{ convertToCommaAmount(prizePopupBonusAmt) }}PKR</span>
