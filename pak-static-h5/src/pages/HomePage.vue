f0e790222516f7c280e64fdce326d02b51ab5c13
zhixuan <zhixuan@preec.net>
2024-11-29 17:27:55 +0800

    fix: home page float promo - mega sharing wheel dialog not opening after closed once

--- pak-static-h5/src/pages/HomePage.vue
+++ pak-static-h5/src/pages/HomePage.vue
@@ -1376,7 +1376,7 @@
         <PopupController v-model="popupPromo" :hasWheel="hasInviteWheelPromo" />
       </template>
     </MoneyRainModal>
-    <q-btn class="money-rain-close" icon="close" round dense @click="moneyRainDialogClose" />
+    <q-btn class="money-rain-close" icon="close" round dense @click="closeDialog" />
   </q-dialog>
 
   -
@@ -1385,8 +1385,9 @@
     :model-value="megaSharingWheelDialogModel"
     full-width
     class="mega-sharing-wheel-dialog"
+    persistent
   >
-    <q-btn class="mega-sharing-wheel-dialog-close" icon="close" round dense v-close-popup />
+    <q-btn class="mega-sharing-wheel-dialog-close" icon="close" round dense @click="closeDialog" />
     <MegaSharingWheelModal>
       <template #controller>
         <PopupController v-model="popupPromo" :hasWheel="hasInviteWheelPromo" />
@@ -1497,7 +1498,7 @@ provide("closeMegaSharingWheelDialog", () => {
   megaSharingWheelDialogModel.value = false;
 });
 
-const moneyRainDialogClose = () => {
+const closeDialog = () => {
   popupPromo.value = "";
 };
 const activateSlide = (item) => {
