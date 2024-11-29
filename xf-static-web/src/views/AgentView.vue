7bb56760524c8b102e330057be5cf641a09206c2
Keng Hong <kenghongho@preec.net>
2024-11-29 10:01:35 +0800

    feat: [xf web] update domain

--- xf-static-web/src/views/AgentView.vue
+++ xf-static-web/src/views/AgentView.vue
@@ -191,7 +191,7 @@
       </div>
       <div class="agent-demo-btm" style="margin-top: 20px; padding-bottom: 40px">
         <a
-          :href="'https://affiliate-web.gwd4jptunz.com/xf/register?agent=' + (affCode ? affCode : '')"
+          :href="'https://xf1-affiliate.gwd4jptunz.com/xf/register?agent=' + (affCode ? affCode : '')"
           class="agent-reg-btn"
         >
           立即申请代理
