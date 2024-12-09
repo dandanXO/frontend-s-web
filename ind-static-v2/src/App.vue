9c45f2f2ae927c85371666271c7376d783ff8e59
WeiSeng <weiseng@eagrp.net>
2024-12-09 15:22:15 +0800

    test: IW2 deploy run

--- ind-static-v2/src/App.vue
+++ ind-static-v2/src/App.vue
@@ -340,11 +340,8 @@ export default defineComponent({
     };
 
     onMounted(async () => {
-      // const info = await App.getInfo();
-      // console.log("APP Info");
-      // console.log(info);
-      // checkSID();
-      // getCSA();
+      console.log("START");
+
       checkServerStatus();
       getAppInfo();
       initOrientation();
