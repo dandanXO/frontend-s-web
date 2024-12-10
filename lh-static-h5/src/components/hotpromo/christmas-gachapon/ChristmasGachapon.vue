9dda9280ddf27011d9f6f4dcd5cea5aae3824f07
WeiSeng <weiseng@eagrp.net>
2024-12-10 10:15:37 +0800

    style: LH css christmas promo amend

--- lh-static-h5/src/components/hotpromo/christmas-gachapon/ChristmasGachapon.vue
+++ lh-static-h5/src/components/hotpromo/christmas-gachapon/ChristmasGachapon.vue
@@ -31,9 +31,9 @@
   </div>
   <q-dialog class="christmas-modal" v-model="isModal" align-center>
     <div class="title">
-      <img src="../../../assets/promo/christmas-gachapon/side.png">
+      <img src="../../../assets/promo/christmas-gachapon/side.png" />
       {{ modalContent.title }}
-      <img src="../../../assets/promo/christmas-gachapon/side.png">
+      <img src="../../../assets/promo/christmas-gachapon/side.png" />
     </div>
     <div class="inner-contents">
       <div v-if="modalContent.type === 'rule'" class="rules" v-html="rules"></div>
@@ -58,90 +58,93 @@
                 size="sm"
             />
             </div> -->
-            <div class="details" v-if="modalContent.type === 'detail'">
-            
-            <div class="detailed-info">
-                <div class="little-title">
-                <div class="left">活动时间</div>
-                <div class="right">2024.12.24-2025.1.1</div>
-                </div>
-                <div class="little-title">
-                <div class="left">活动对象</div>
-                <div class="right">全网会员</div>
-                </div>
-                <div class="little-title" style="justify-content: flex-start; align-items: flex-start">
-                <div class="left">优惠详情</div>
-                <div class="right" style="max-width: 73%;">
-                    在2024.12.24-2025.1.1活动期间，每日投注额≥1000元及以上次日即可获得一次抽奖机会，每日可抽50次，活动周期内抽奖次数可累计。
-                </div>
-                </div>
-                <table class="detailed-info-table">
-                <tbody>
-                    <tr>
-                    <th>当日有效投注</th>
-                    <th>抽奖机会</th>
-                    <th>流水倍数</th>
-                    </tr>
-                    <tr>
-                    <td>≥1,000元</td>
-                    <td>1次</td>
-                    <td rowspan="11">5倍流水</td>
-                    </tr>
-                    <tr>
-                    <td>≥5,000元</td>
-                    <td>2次</td>
-                    </tr>
-                    <tr>
-                    <td>≥8,000元</td>
-                    <td>3次</td>
-                    </tr>
-                    <tr>
-                    <td>≥15,000元</td>
-                    <td>5次</td>
-                    </tr>
-                    <tr>
-                    <td>≥30,000元</td>
-                    <td>10次</td>
-                    </tr>
-                    <tr>
-                    <td>≥80,000元</td>
-                    <td>15次</td>
-                    </tr>
-                    <tr>
-                    <td>≥100,000元</td>
-                    <td>20次</td>
-                    </tr>
-                    <tr>
-                    <td>≥300,000元</td>
-                    <td>25次</td>
-                    </tr>
-                    <tr>
-                    <td>≥800,000元</td>
-                    <td>30次</td>
-                    </tr>
-                    <tr>
-                    <td>≥1,000,000元</td>
-                    <td>40次</td>
-                    </tr>
-                    <tr>
-                    <td>≥3,000,000元</td>
-                    <td>50次</td>
-                    </tr>
-                </tbody>
-                </table>
-
-                <div class="detailed-info-bottom">
-                <div class="detailed-info-bottom-left-title">
-                    <div class="detailed-info-bottom-left-btn">
-                    <img src="../../../assets/images/promotion/hotpromo/lh1-blast-premier/game-bottom-left-btn.png" alt="" width="22px" />
-                    <span>示例</span>
-                    </div>
-                    会员A在2024年12月25日累计有效投注为20000元, 2024年12月26日即可在优惠页面点击【立即抽奖】获得财富金,
-                    根据规则会员可获得5次抽奖机会, 奖金仅需5倍水即可出款
-                </div>
-                </div>
+      <div class="details" v-if="modalContent.type === 'detail'">
+        <div class="detailed-info">
+          <div class="little-title">
+            <div class="left">活动时间</div>
+            <div class="right">2024.12.24-2025.1.1</div>
+          </div>
+          <div class="little-title">
+            <div class="left">活动对象</div>
+            <div class="right">全网会员</div>
+          </div>
+          <div class="little-title" style="justify-content: flex-start; align-items: flex-start">
+            <div class="left">优惠详情</div>
+            <div class="right" style="max-width: 73%">
+              在2024.12.24-2025.1.1活动期间，每日投注额≥1000元及以上次日即可获得一次抽奖机会，每日可抽50次，活动周期内抽奖次数可累计。
             </div>
+          </div>
+          <table class="detailed-info-table">
+            <tbody>
+              <tr>
+                <th>当日有效投注</th>
+                <th>抽奖机会</th>
+                <th>流水倍数</th>
+              </tr>
+              <tr>
+                <td>≥1,000元</td>
+                <td>1次</td>
+                <td rowspan="11">5倍流水</td>
+              </tr>
+              <tr>
+                <td>≥5,000元</td>
+                <td>2次</td>
+              </tr>
+              <tr>
+                <td>≥8,000元</td>
+                <td>3次</td>
+              </tr>
+              <tr>
+                <td>≥15,000元</td>
+                <td>5次</td>
+              </tr>
+              <tr>
+                <td>≥30,000元</td>
+                <td>10次</td>
+              </tr>
+              <tr>
+                <td>≥80,000元</td>
+                <td>15次</td>
+              </tr>
+              <tr>
+                <td>≥100,000元</td>
+                <td>20次</td>
+              </tr>
+              <tr>
+                <td>≥300,000元</td>
+                <td>25次</td>
+              </tr>
+              <tr>
+                <td>≥800,000元</td>
+                <td>30次</td>
+              </tr>
+              <tr>
+                <td>≥1,000,000元</td>
+                <td>40次</td>
+              </tr>
+              <tr>
+                <td>≥3,000,000元</td>
+                <td>50次</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div class="detailed-info-bottom">
+            <div class="detailed-info-bottom-left-title">
+              <div class="detailed-info-bottom-left-btn">
+                <img
+                  src="../../../assets/images/promotion/hotpromo/lh1-blast-premier/game-bottom-left-btn.png"
+                  alt=""
+                  width="22px"
+                />
+                <span>示例</span>
+              </div>
+              会员A在2024年12月25日累计有效投注为20000元, 2024年12月26日即可在优惠页面点击【立即抽奖】获得财富金,
+              根据规则会员可获得5次抽奖机会, 奖金仅需5倍水即可出款
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   </q-dialog>
 
@@ -179,7 +182,7 @@ const isModal = ref(false);
 const isPrizeModal = ref(false);
 const modalContent = {
   title: "",
-  type: "",
+  type: ""
 };
 const prizes = ref([]);
 const availableDraw = ref(0);
@@ -200,11 +203,10 @@ const pagesNumber = computed(() => Math.ceil(totalItems.value / pagination.value
 
 const totalItems = ref(0);
 const openModal = (type) => {
-  modalContent.type = type
+  modalContent.type = type;
   if (type === "detail") {
     modalContent.title = "活动内容";
-  }
-  else if (type === "rule") {
+  } else if (type === "rule") {
     modalContent.title = "活动规则";
   } else if (type === "record") {
     getDrawRecord(promoCode.value, params).then((res) => {
@@ -378,39 +380,6 @@ onMounted(() => {
     opacity: 0; // Fade out at the end
   }
 }
-.spinner {
-  display: inline-block;
-  width: 150px; /* Set the width of your PNG */
-  height: 150px; /* Set the height of your PNG */
-  overflow: hidden; /* Optional: Ensures content stays within the div */
-}
-.q-loading-mask {
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  color: #ffffff;
-  z-index: 9999;
-  overflow: hidden;
-  height: 100vh;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent overlay */
-}
-.loading-text {
-  font-size: 24px; /* Adjust size as needed */
-  color: #ffffff; /* Text color */
-  background: linear-gradient(90deg, #ff7e5f, #feb47b); /* Gradient text */
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent; /* Make gradient visible in text */
-  text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.3); /* Add a soft shadow */
-  animation: bounce 2s linear infinite; /* Apply spinning animation */
-}
-.spinner img {
-  width: 88%; /* Ensures the image scales properly */
-  height: 88%;
-  animation: spin 1.5s ease-in-out infinite;
-}
 
 @keyframes bounce {
   0% {
@@ -446,6 +415,41 @@ onMounted(() => {
   width: 100%;
   min-height: 100vh;
   padding-bottom: 20vw;
+
+  .q-loading-mask {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #ffffff;
+    z-index: 9999;
+    overflow: hidden;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent overlay */
+  }
+  .loading-text {
+    font-size: 24px; /* Adjust size as needed */
+    color: #ffffff; /* Text color */
+    background: linear-gradient(90deg, #ff7e5f, #feb47b); /* Gradient text */
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; /* Make gradient visible in text */
+    text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.3); /* Add a soft shadow */
+    animation: bounce 2s linear infinite; /* Apply spinning animation */
+  }
+  .spinner {
+    display: inline-block;
+    width: 150px; /* Set the width of your PNG */
+    height: 150px; /* Set the height of your PNG */
+    overflow: hidden; /* Optional: Ensures content stays within the div */
+  }
+  .spinner img {
+    width: 88%; /* Ensures the image scales properly */
+    height: 88%;
+    animation: spin 1.5s ease-in-out infinite;
+  }
+
   .promodetail {
     top: 136vw;
     cursor: pointer;
@@ -545,127 +549,125 @@ onMounted(() => {
     right: 20px;
     bottom: 0px;
   }
-  
 
-.detailed-info-bottom {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  .detailed-info-bottom-left-title {
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 22.4px;
-    color: #ff0000;
-  }
-  .detailed-info-bottom-left-btn {
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 22.4px;
-    color: #ff0000;
-    cursor: pointer;
+  .detailed-info-bottom {
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
-    gap: 4px;
-  }
-}
-.details {
-  max-height: 60vh;
-}
-
-.detailed-info {
-  width: 100%;
-  height: 100%;
-  border-radius: 12px;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-
-  .title {
-    background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-title.png");
-    background-repeat: no-repeat;
-    background-size: 100%;
-    width: 240px;
-    height: 26px;
-    margin: 0 auto;
-  }
-  .little-title {
-    .left {
-      filter: hue-rotate(150deg) saturate(3.5);
-      background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-little-title-bg.png");
-      background-repeat: no-repeat;
-      background-size: 100% 100%;
-      width: 64px;
-      height: 20px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    .detailed-info-bottom-left-title {
       font-size: 12px;
       font-weight: 600;
-      line-height: 23.33px;
-      color: #ffffff;
-      margin-right: 16px;
-      display: inline-flex;
+      line-height: 22.4px;
+      color: #ff0000;
     }
-    .right {
+    .detailed-info-bottom-left-btn {
+      font-size: 12px;
+      font-weight: 600;
+      line-height: 22.4px;
+      color: #ff0000;
+      cursor: pointer;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 4px;
+    }
+  }
+  .details {
+    max-height: 60vh;
+  }
+
+  .detailed-info {
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    .title {
+      background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-title.png");
+      background-repeat: no-repeat;
+      background-size: 100%;
+      width: 240px;
+      height: 26px;
+      margin: 0 auto;
+    }
+    .little-title {
+      .left {
+        filter: hue-rotate(150deg) saturate(3.5);
+        background-image: url("../../../assets/images/promotion/hotpromo/lh1-blast-premier/info-little-title-bg.png");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        width: 64px;
+        height: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+        font-weight: 600;
+        line-height: 23.33px;
+        color: #ffffff;
+        margin-right: 16px;
+        display: inline-flex;
+      }
+      .right {
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 28px;
+        color: #000000;
+        display: inline;
+      }
+    }
+  }
+
+  .detailed-info-table {
+    width: 100%;
+    height: 100%;
+    border-collapse: separate;
+    border-spacing: 0;
+    text-align: center;
+    vertical-align: middle;
+    th {
       font-size: 12px;
       font-weight: 400;
       line-height: 28px;
-      color: #000000;
-      display: inline;
+      color: #fff;
+      background: #f34e38;
     }
-  }
-}
-
-.detailed-info-table {
-  width: 100%;
-  height: 100%;
-  border-collapse: separate;
-  border-spacing: 0;
-  text-align: center;
-  vertical-align: middle;
-  th {
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 28px;
-    color: #fff;
-    background: #F34E38;
-  }
-  tr {
-    &:first-child {
+    tr {
+      &:first-child {
         td {
-            &:last-child {
-                background: #ffffff;
-            }
-        }
-    }
-    &:last-child {
-      td {
-        &:first-child {
-          // border-bottom-left-radius: 12px;
+          &:last-child {
+            background: #ffffff;
+          }
         }
       }
-    }
-    &:nth-child(2) {
-      td {
-        &:last-child {
-          // border-bottom-right-radius: 12px;
+      &:last-child {
+        td {
+          &:first-child {
+            // border-bottom-left-radius: 12px;
+          }
         }
       }
+      &:nth-child(2) {
+        td {
+          &:last-child {
+            // border-bottom-right-radius: 12px;
+          }
+        }
+      }
+      &:nth-child(odd) {
+        background: #f34e3820;
+      }
     }
-    &:nth-child(odd) {
-        background: #F34E3820;
+    td {
+      border: 1px solid #f34e3832;
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 16px;
+      color: #000000;
     }
   }
-  td {
-    border: 1px solid #F34E3832;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 16px;
-    color: #000000;
-  }
-}
-
 }
 .prize-modal {
   .prizes {
@@ -713,7 +715,8 @@ onMounted(() => {
     }
   }
 }
-.christmas-modal .q-dialog__inner, .prize-modal .q-dialog__inner {
+.christmas-modal .q-dialog__inner,
+.prize-modal .q-dialog__inner {
   background: url(../../../assets/promo/christmas-gachapon/modal-bg.png) no-repeat center center;
   background-size: 100% 100%;
   padding: 120px 0 80px;
@@ -733,93 +736,93 @@ onMounted(() => {
     overflow: auto;
   }
   .rules {
-  height: 45vh;
-  width: 95%;
-  margin: auto;
-  font-size: 14px;
-  color: #333; // Default text color
-  line-height: 1.6;
-  padding: 10px 0;
-  border-radius: 8px; // Optional: Rounded corners for the rules container
+    height: 45vh;
+    width: 95%;
+    margin: auto;
+    font-size: 14px;
+    color: #333; // Default text color
+    line-height: 1.6;
+    padding: 10px 0;
+    border-radius: 8px; // Optional: Rounded corners for the rules container
 
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    counter-reset: item; // Initialize counter
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      counter-reset: item; // Initialize counter
 
-    li {
-      position: relative;
-      padding-left: 30px; // Space for numbered circle
-      margin-bottom: 15px;
+      li {
+        position: relative;
+        padding-left: 30px; // Space for numbered circle
+        margin-bottom: 15px;
 
-      &::before {
-        content: counter(item); // Display the index
-        counter-increment: item; // Increment counter
-        position: absolute;
-        top: 10px;
-        left: 0px;
-        transform: translateY(-50%);
-        background: linear-gradient(135deg, #e6374a, #ac1828);
-        color: #ffffff;
-        border-radius: 50%;
-        width: 20px;
-        height: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 14px;
-        font-weight: bold;
+        &::before {
+          content: counter(item); // Display the index
+          counter-increment: item; // Increment counter
+          position: absolute;
+          top: 10px;
+          left: 0px;
+          transform: translateY(-50%);
+          background: linear-gradient(135deg, #e6374a, #ac1828);
+          color: #ffffff;
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 14px;
+          font-weight: bold;
+        }
       }
     }
+
+    font {
+      color: #c24f4a;
+      font-weight: bold; // Optional: Emphasize important notes
+    }
+  }
+  .q-table thead {
+    position: sticky;
+    top: 0;
+    background-color: #fff; /* Ensure the header background is white */
+    z-index: 1; /* Ensure it stays above the table body */
+  }
+  .q-table__grid-item-row {
+    display: flex;
+    gap: 15px;
+    .q-table__grid-item-title {
+      color: #e6374a;
+      opacity: 1;
+    }
+  }
+  /* Table header color */
+  .custom-table .q-table__head {
+    background: #f34e38;
+    color: white;
   }
 
-  font {
-    color: #c24f4a;
-    font-weight: bold; // Optional: Emphasize important notes
+  /* Even rows color */
+  .custom-table .q-table__row:nth-child(even) {
+    background: #f34e3855; /* Light transparent red for even rows */
   }
-}
-.q-table thead {
-  position: sticky;
-  top: 0;
-  background-color: #fff; /* Ensure the header background is white */
-  z-index: 1; /* Ensure it stays above the table body */
-}
-.q-table__grid-item-row {
-  display: flex;
-  gap: 15px;
-  .q-table__grid-item-title {
-    color: #e6374a;
-    opacity: 1;
+
+  /* Optional: Style odd rows differently if needed */
+  .custom-table .q-table__row:nth-child(odd) {
+    background: white; /* or any other background color */
   }
-}
-/* Table header color */
-.custom-table .q-table__head {
-  background: #f34e38;
-  color: white;
-}
-
-/* Even rows color */
-.custom-table .q-table__row:nth-child(even) {
-  background: #f34e3855; /* Light transparent red for even rows */
-}
-
-/* Optional: Style odd rows differently if needed */
-.custom-table .q-table__row:nth-child(odd) {
-  background: white; /* or any other background color */
-}
-.table-scroll {
-  height: 40vh;
-  overflow: auto;
-  position: relative;
-}
-.q-table__bottom--nodata {
-  color: #ff0000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 30vh;
-}
+  .table-scroll {
+    height: 40vh;
+    overflow: auto;
+    position: relative;
+  }
+  .q-table__bottom--nodata {
+    color: #ff0000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30vh;
+  }
 }
 .prize-modal.once .q-dialog__inner {
   padding: 140px 0 40px;
