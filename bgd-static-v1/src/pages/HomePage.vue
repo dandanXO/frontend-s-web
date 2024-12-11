3b0214464d583559aa622522b7ae0cf246d0e7e2
zhixuan <zhixuan@preec.net>
2024-12-11 17:25:07 +0800

    fix: carousel mounted even when hb promo list is empty

--- bgd-static-v1/src/pages/HomePage.vue
+++ bgd-static-v1/src/pages/HomePage.vue
@@ -102,6 +102,7 @@
         <!--        </div>-->
         <div>
           <q-carousel
+            v-if="hbPromo.length > 0"
             class="hb-float"
             :navigation="hbPromo.length > 1 ? true : false"
             v-model="hbSlide"
