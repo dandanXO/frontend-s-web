e0c948f18e4bc47c458800aa7cba4b0e81b6c2ca
WeiSeng <weiseng@eagrp.net>
2024-12-13 20:47:06 +0800

    feat: BRA random imgs slider effect

--- br1-static-v1/public/static/pwa/js/index.js
+++ br1-static-v1/public/static/pwa/js/index.js
@@ -282,3 +282,64 @@ function openLinkInPreferredBrowser(url) {
     window.open(url, "_blank");
   }
 }
+
+insertRandomImages();
+
+function insertRandomImages() {
+  const defaultScrollList = document.querySelector("#scroll-lists");
+
+  var imageUrls = [];
+  var extraStyle = "";
+
+  var fileCountLists = [12, 13, 16, 16, 6];
+  var fileNum = Math.floor(Math.random() * 5) + 1;
+  // var fileNum= 1;
+  var fileDirec = "img" + fileNum;
+
+  var fileLength = fileCountLists[fileNum - 1];
+  console.log(fileDirec);
+  var fileArray = [];
+  for (var i = 0; i < fileLength; i++) {
+    fileArray.push(i + 1);
+  }
+  shuffleArray(fileArray);
+// console.log(fileArray);
+
+  var fileLists = fileArray.slice(0, 5);
+  console.log(fileLists);
+
+  fileLists.forEach((file) => {
+    var fileName = `images/${fileDirec}/${file}.jpg`;
+    imageUrls.push(fileName);
+  });
+
+  if (fileNum === 2 || fileNum === 4) {
+    extraStyle = `style="height:300px;"`;
+    defaultScrollList.style.height= "300px"
+  } else {
+    extraStyle = `style="width:65vw;"`;
+    defaultScrollList.style.height= "auto"
+  }
+
+  imageUrls.forEach((imageUrl) => {
+    var innerHtml = `
+<div class="ULeU3b Utde2e" role="listitem">
+                                        <div class="Atcj9b"><img src="${imageUrl}" class="T75of B5GQxf"
+                                        ${extraStyle}
+                                                                 alt="55Ace" loading="lazy"></div>
+</div>
+`;
+    // Append the image to the container
+    defaultScrollList.innerHTML += innerHtml;
+  });
+
+  defaultScrollList.style.display = "flex";
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
