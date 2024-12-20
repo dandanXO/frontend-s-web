29ed32fa8757029b9940ce49ac9422bf058d5b98
zhixuan <zhixuan@preec.net>
2024-12-20 10:18:34 +0800

    feat: add bind phone number

--- bgd-static-v1/src/i18n/en.js
+++ bgd-static-v1/src/i18n/en.js
@@ -65,6 +65,7 @@ export default {
     phone_rules_01: "Please insert Phone number",
     phone_rules_02: "The phone number must be 10 - 11 digits",
     phone_rules_03: "The phone number must start with '01'",
+    bindPhoneNumber: "Bind Phone Number",
     password: "Password",
     password_placeholder: "Please enter password",
     password_rules_01: "Please insert password",
