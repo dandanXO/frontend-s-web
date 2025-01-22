17a6a82b68efb08a697d430728b9f9ad9f41fd4c
martin <martin@eagrp.net>
2025-01-22 11:50:35 +0800

    游戏标签页面增加功能 - PG（假/原厂切换），热门标签重新排序

--- tf-manager-web/src/i18n/vn.js
+++ tf-manager-web/src/i18n/vn.js
@@ -1718,6 +1718,7 @@ export default {
     downloadManagerRecord: 'Tải xuống bản ghi quản lý',
     downloadExportedExcel: 'Tải xuống Excel đã xuất',
     redPacketAmountAfterReachingLimit: 'Số tiền bao lì xì sau khi đạt giới hạn',
+    reOrderHotGame: 'Sắp xếp lại Hot Game Seq',
     requestBy: 'Yêu cầu bởi',
     requestTime: 'Thời gian yêu cầu',
     progress: 'Tiến trình',
@@ -1958,6 +1959,7 @@ export default {
     unfreezeMemberSuccess: 'Tháo băng thành viên thành công',
     found: 'tìm thấy',
     gameMatchEnded: 'trận đấu kết thúc',
+    gameNotFound: 'Trò chơi không tìm thấy',
     giftDelivered: 'quà tặng đã giao',
     giftNoCashRedeemAmount:
       'không có số tiền đổi quà thành tiền mặt. Vui lòng thiết lập số tiền đổi quà thành tiền mặt trước.',
