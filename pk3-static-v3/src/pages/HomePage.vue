da1d62e4aac01f99edfd12b527163144e94300f0
Sam <sam@flex-df.com>
2025-01-23 14:21:13 +0800

    feat: [br1/co1/id1/ind1/ind2/mx1/nga/ph1/pk3] Migrate hot games column count change from th2

--- pk3-static-v3/src/pages/HomePage.vue
+++ pk3-static-v3/src/pages/HomePage.vue
@@ -228,7 +228,7 @@
           </div>
 
           <div class="platform-game-wrapper" v-else>
-            <div class="platform-game-container grid-view">
+            <div class="platform-game-container grid-view-col-4">
               <template v-for="(item, index) in hotGameList" :key="index">
                 <template v-if="item.type && item.type === 'game'">
                   <div
@@ -3839,6 +3839,13 @@ onBeforeUnmount(() => {
     row-gap: 16px;
   }
 
+  &.grid-view-col-4 {
+    display: grid;
+    grid-template-columns: repeat(4, minmax(75px, 1fr));
+    column-gap: 8px;
+    row-gap: 16px;
+  }
+
   &.sport-platform {
     grid-template-columns: 1fr;
     row-gap: 12px;
@@ -3894,7 +3901,7 @@ onBeforeUnmount(() => {
       white-space: nowrap;
       overflow: hidden;
       text-overflow: ellipsis;
-      max-width: 100px;
+      // max-width: 100px;
     }
 
     img {
