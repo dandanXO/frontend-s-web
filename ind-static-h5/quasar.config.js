d17fb04f165399643426a7eed9b10c77402fcadb
WeiSeng <weiseng@eagrp.net>
2025-01-23 18:30:59 +0800

    feat: INd add VUE_ROUTER_MODE for 2 mode

--- ind-static-h5/quasar.config.js
+++ ind-static-h5/quasar.config.js
@@ -50,10 +50,10 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: process.env.VUE_ROUTER_MODE === "hash" || "history" , // available values: 'hash', 'history'
 
       // transpile: false,
-      publicPath: `/${process.env.ROUTER_BASE}/`,
+      publicPath: process.env.ROUTER_BASE ? `/${process.env.ROUTER_BASE}/` : "",
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
--- ind-static-h5/src/router/index.js
+++ ind-static-h5/src/router/index.js
@@ -22,8 +22,8 @@ export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : process.env.VUE_ROUTER_MODE === "history"
-    ? createWebHistory('/507620957/')
-    : createWebHashHistory;
+      ? createWebHistory
+      : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -32,7 +32,9 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createWebHashHistory()
+    history: createHistory(
+      process.env.MODE === "ssr" ? void 0 : process.env.VUE_ROUTER_BASE
+    )
   });
   Router.beforeEach((to, from, next) => {
     const user = userStore();
