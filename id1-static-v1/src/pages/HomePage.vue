803d1c00c3b193a9fcf6fca9c57dbdd4366d7905
WeiSeng <weiseng@eagrp.net>
2025-01-27 13:55:11 +0800

    style: ID1 IND adjust home col 3

--- id1-static-v1/src/pages/HomePage.vue
+++ id1-static-v1/src/pages/HomePage.vue
@@ -3872,7 +3872,7 @@ onBeforeUnmount(() => {
 
   &.grid-view {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 32%);
     column-gap: 8px;
     row-gap: 16px;
   }
