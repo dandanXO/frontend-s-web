732c77fb62ac90d8ce5d0b8120640561fff3a460
Kai Wen <kaiwen@caspen-tech.com>
2025-02-19 19:47:10 +0800

    feat: [ind] Add countdown for otp

--- ind-static-h5/src/boot/axios.js
+++ ind-static-h5/src/boot/axios.js
@@ -164,6 +164,9 @@ export default boot(({ app, router }) => {
       if (res.code === ResponseCode.EMPTY_PROMO_POPOUT || res.code === ResponseCode.EMPTY_PROMO_ID_NOTFOUND) {
         return res;
       }
+      if(res.code === ResponseCode.OTP_COOLDOWN_ERROR) {
+        return res;
+      }
       if (res.code === ResponseCode.ERROR_GUEST_LOGGED) {
         return res;
       }
