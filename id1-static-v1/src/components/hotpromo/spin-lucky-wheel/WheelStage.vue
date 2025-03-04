3030357697e7c443bbbffdaf70a3e1a015fc8d15
Kai Wen <kaiwen@caspen-tech.com>
2025-03-04 17:50:30 +0800

    fix: [id1] Add translation

--- id1-static-v1/src/components/hotpromo/spin-lucky-wheel/WheelStage.vue
+++ id1-static-v1/src/components/hotpromo/spin-lucky-wheel/WheelStage.vue
@@ -279,7 +279,7 @@ const handleReceiveClick = () => {
   eventapi.post("/refer-spin/withdraw").then((res) => {
     if (res.code === 0) {
       $q.notify({
-        message: "Receive successfully",
+        message: "Berhasil diterima",
         color: "positive",
         position: "top"
       });
