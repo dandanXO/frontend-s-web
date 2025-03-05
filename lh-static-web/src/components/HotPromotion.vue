2482dccea578ba08d97b290f522b653758ab0f08
Neil Yang <des75421@gmail.com>
2025-03-05 16:58:56 +0800

    web new promo pgl dota s3

--- lh-static-web/src/components/HotPromotion.vue
+++ lh-static-web/src/components/HotPromotion.vue
@@ -55,6 +55,7 @@
     />
     <LiveDailyRebates v-if="list.redirectUrl === 'lh1-live-daily-rebates'" :promo-code="list.promoCode" />
     <Dota2Pgl v-if="list.redirectUrl === 'lh1-dota2-pgl'" :promo-code="list.promoCode" />
+    <PglS32025 v-if="list.redirectUrl === 'lh1-2025-pgl-s3'" :promo-code="list.promoCode" />
     <NewVipRebate v-if="list.redirectUrl === 'lh1-newvip-rebate'" :promo-code="list.promoCode" />
     <LongNewVip v-if="list.redirectUrl === 'lh1-vip'" :promo-code="list.promoCode" />
     <NationalDay2024 v-if="list.redirectUrl === 'lh1-national-day-2024'" :promo-code="list.promoCode" />
@@ -179,6 +180,7 @@ import LplPKlck2025loss from "../components/hotpromo/lpl-lck-2025-loss/lpl-lck-2
 import LanternFestival2025 from "../components/hotpromo/lantern-festival-2025/LanternFestival2025.vue";
 import LiveDailyRebates from "../components/hotpromo/live-daily-rebates/LiveDailyRebates.vue";
 import Dota2Pgl from "../components/hotpromo/dota2-pgl/Dota2Pgl.vue";
+import PglS32025 from "../components/hotpromo/pgl-s3-2025/PglS32025.vue"
 import NewVipRebate from "../components/hotpromo/newVipRebate/NewVipRebate.vue";
 import LongNewVip from "../components/hotpromo/longNewVip/LongNewVip.vue";
 import NationalDay2024 from "../components/hotpromo/national-day-2024/NationalDay2024.vue";
@@ -323,6 +325,7 @@ export default defineComponent({
     LplPKlck2025loss,
     LiveDailyRebates,
     Dota2Pgl,
+    PglS32025,
     NewVipRebate,
     LongNewVip,
     NationalDay2024,
