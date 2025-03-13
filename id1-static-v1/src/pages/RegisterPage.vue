3ce7f5e63f16d5449b83fdb427f1c90f51f11e13
Keng Hong <kenghongho@preec.net>
2025-03-13 18:07:31 +0800

    feat: [55ace] IW2, ID1, PH1, NGA - update register logic with referrer

--- id1-static-v1/src/pages/RegisterPage.vue
+++ id1-static-v1/src/pages/RegisterPage.vue
@@ -73,6 +73,7 @@
           outlined
           placeholder="Masukkan nomor OTP Anda"
           label-color="brand"
+          :disable="isOtpEnable"
         >
           <template v-slot:prepend>
             <img class="white-svg" src="../assets/images/auth/otp.svg" />
@@ -320,6 +321,14 @@ export default defineComponent({
         isAgreeReg.value === false
       ) {
         $q.loading.hide();
+      } else if (regForm.referrer && isOtpEnable.value){
+        $q.notify({
+          color: "negative",
+          position: "top",
+          message: "Silakan masukkan nomor OTP",
+          icon: "report_problem"
+        });
+        $q.loading.hide();
       } else {
         var qs = require("qs");
         const fpPromise = FingerprintJS.load();
@@ -448,7 +457,9 @@ export default defineComponent({
       }
     );
 
+    const isOtpEnable = ref(true);
     const openPhoneVeriDialog = () => {
+      isOtpEnable.value = false;
       loginNameRef.value.validate();
       if (!loginNameRef.value.hasError) {
         showCaptchaDialog.value = true;
@@ -580,7 +591,8 @@ export default defineComponent({
       imgOnLoad,
       imgOnError,
       otpCountdown,
-      otpCountdownInterval
+      otpCountdownInterval,
+      isOtpEnable
     };
   }
 });
