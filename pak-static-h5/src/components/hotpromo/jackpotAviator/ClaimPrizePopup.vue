aa72e4c2526a3dc2eb236d8fe95b9dd9a1fe210c
Mika <mika@eagrp.net>
2025-03-25 15:14:13 +0800

    fix: [pak] promo update.

--- pak-static-h5/src/components/hotpromo/jackpotAviator/ClaimPrizePopup.vue
+++ pak-static-h5/src/components/hotpromo/jackpotAviator/ClaimPrizePopup.vue
@@ -5,14 +5,14 @@
         <slot name="controller" />
       </div>
       <q-btn icon="close" round dense v-close-popup class="congrats-close" />
-      <div class="congrats-header"><img src="../../../assets/images/index/modal/congrats-header.png" /></div>
-      <div class="congrats-coupons"><img src="../../../assets/images/promotion/hotpromo/jackpot-aviator/prize.png" /></div>
+      <!-- <div class="congrats-header"><img src="../../../assets/images/index/modal/congrats-header.png" /></div> -->
+      <!-- <div class="congrats-coupons"><img src="../../../assets/images/promotion/hotpromo/jackpot-aviator/prize.png" /></div> -->
       <!-- <div class="congrats-title">{{ props?.claimPrizeAmt }}</div> -->
       <div class="congrats-highlight">{{ store.currency.value }} {{ props?.claimPrizeAmt }}</div>
   
       <div class="congrats-button" @click="props?.onClickClose">
-        <q-btn no-caps unelevated class="btn-primary" :loading="false">
-          {{ $t("btn.close") }}
+        <q-btn no-caps unelevated class="button-bg" :loading="false">
+          {{ $t("btn.recharge") }}
         </q-btn>
       </div>
     </div>
@@ -43,26 +43,29 @@
     align-items: center;
   }
   .congrats-container {
-    background-color: #113413;
+    // background-color: #113413;
+      background-image: url(../../../assets/images/index/modal/aviator-grats-bg.png);
+      background-size: contain;
     max-width: 400px;
+    min-height: 600px;
     width: 100%;
     padding: 16px;
     position: relative;
     overflow: visible;
     border-radius: 12px;
   
-    &:before {
-      content: "";
-      background-image: url(../../../assets/images/index/modal/congrats-container-light.png);
-      background-size: 100% 100%;
-      background-position: center center;
-      background-repeat: no-repeat;
-      width: 100%;
-      height: 150px;
-      position: absolute;
-      left: 0;
-      top: -150px;
-    }
+    // &:before {
+    //   content: "";
+    //   background-image: url(../../../assets/images/index/modal/congrats-container-light.png);
+    //   background-size: 100% 100%;
+    //   background-position: center center;
+    //   background-repeat: no-repeat;
+    //   width: 100%;
+    //   height: 150px;
+    //   position: absolute;
+    //   left: 0;
+    //   top: -150px;
+    // }
   
     .congrats-header {
       display: flex;
@@ -94,18 +97,52 @@
       font-weight: bold;
     }
   
-    .congrats-highlight {
-      color: #fff96f;
-      font-size: 16px;
+    
+.congrats-button {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 3%;
+  display: flex;  
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+
+  .button-bg {
+    background: url(../../../assets/images/index/modal/aviator-btn.png) center center no-repeat;
+    background-size: 100% 100%;
+    aspect-ratio: 389/139;
+    color: #ffffff;
+    font-weight: bold;
+    width: 70%;
+    font-size: 23px;
+    padding-bottom: 25px;
+  }
+}
+  .congrats-highlight {
+      // color: #fff96f;
+      // font-size: 26px;
+      // font-weight: bold;
+      // text-align: center;
+      // background-image: url(../../../assets/images/index/modal/congrats-highlight-bg.png);
+      padding: 15px 12px;
+      // background-repeat: no-repeat;
+      // background-size: 70% 100%;
+      // background-position: center;
+      // margin-top: 16px;
+      background: unset;
+      position: absolute;
+      top: 67%;
+      font-size: 45px;
+      font-weight: 900;
       text-align: center;
-      background-image: url(../../../assets/images/index/modal/congrats-highlight-bg.png);
-      padding: 8px 12px;
-      background-repeat: no-repeat;
-      background-size: 70% 100%;
-      background-position: center;
-      margin-top: 16px;
+      font-family: "Poppins";
+      left: 0;
+      right: 0;
+      margin: auto;
+      color: #FF3F1C;
+      text-shadow: 0px 0.9px #FFFFFF;
     }
-  
     .controller-wrapper {
       position: absolute;
       top: -80px;
