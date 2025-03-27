338d906e551b70c827071c4f971fa2d56652ed32
dan <dan@homway.tw>
2025-03-27 15:25:46 +0800

    update

--- lh-static-web/src/components/HotPromotion.vue
+++ lh-static-web/src/components/HotPromotion.vue
@@ -77,6 +77,7 @@
     />
     <FissureUniverseS4 v-if="list.redirectUrl === 'lh1-fissure-universe-s4'" :promo-code="list.promoCode" />
     <FpSlotPromo v-if="list.redirectUrl === 'lh1-fp-slot'" :promo-code="list.promoCode" />
+    <elsOne2025 v-if="list.redirectUrl === 'lh1-esl-one-raleigh-2025'" :promo-code="list.promoCode" />
 
     <el-dialog class="award-modal" :modal="false" v-model="privilegeClaimedModalVisible" align-center>
       <div class="modal-div">
@@ -139,12 +140,14 @@ import EslProLeagueS21 from "./hotpromo/esl-pro-league-s21/EslProLeagueS21.vue";
 import PGLBucharest2025 from "./hotpromo/pgl-bucharest-2025/PGLBucharest2025.vue";
 import FissureUniverseS4 from "./hotpromo/fissure-universe-s4/FissureUniverseS4.vue";
 import FpSlotPromo from "./hotpromo/fp-slot-promo/FpSlotPromo.vue";
+import elsOne2025 from "../components/hotpromo/elsOne2025/elsOne2025.vue";
 
 export default defineComponent({
   name: "HotPromo",
   order: 1,
   // setup: (props, { emit }) => {},
   components: {
+    elsOne2025,
     SubmitClaimPromo,
     newFootballFight,
     newplayerGuide,
