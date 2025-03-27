492fa32f09dab160bca2790b5bc0f921c1d04cc8
dan <dan@homway.tw>
2025-03-27 18:02:50 +0800

    update i18n

--- pak-static-h5/src/components/hotpromo/spin-lucky-wheel/EnvelopeStage.vue
+++ pak-static-h5/src/components/hotpromo/spin-lucky-wheel/EnvelopeStage.vue
@@ -28,8 +28,8 @@
       <div v-else class="selected-envelope">
         <span class="prize">{{$t("hotPromo.rs")}}{{ prizeList[selectedIndex]?.prize }}</span>
         <span class="desc">{{$t("hotPromo.withdraw_money_over_rs") }} {{ targetWithdrawAmount }}</span>
-        <CommonButton class="withdraw-btn" @click="$emit('envelopeClick')">{{ $t("hotPomo.go_withdraw_now") }}</CommonButton>
-        <span class="remaining-time">{{ $t("hotPromo.time left") }}: {{ remainingTime }}</span>
+        <CommonButton class="withdraw-btn" @click="$emit('envelopeClick')">{{ $t("hotPromo.go_withdraw_now") }}</CommonButton>
+        <span class="remaining-time">{{ $t("hotPromo.time_left ") }}: {{ remainingTime }}</span>
       </div>
       <img class="footer dragon" src="../../../assets/images/promotion/hotpromo/spin-lucky-wheel/envelope-stage/footer.png" />
       <!-- <img class="footer tiger" src="../../../assets/images/promotion/hotpromo/spin-lucky-wheel/decoration-tiger.png" />
