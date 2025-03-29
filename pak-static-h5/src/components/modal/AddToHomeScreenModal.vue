64fd12dff1469d21c483d31bd8d3be8def947bf3
Keng Hong <kenghongho@preec.net>
2025-03-29 15:46:52 +0800

    feat: [pak] revamp ui download app popout

--- pak-static-h5/src/components/modal/AddToHomeScreenModal.vue
+++ pak-static-h5/src/components/modal/AddToHomeScreenModal.vue
@@ -2,6 +2,9 @@
   <q-dialog v-model="isAddToHomeScreenModal" @hide="closeDialog" position="bottom">
     <div class="add-to-homescreen-container">
       <q-btn class="close-btn" flat round dense icon="close" @click="closeDialog" />
+      <div class="add-to-homescreen-download">
+        <img src="../../assets/images/index/download/download-phone.png" />
+      </div>
       <div class="add-to-homescreen-top">
         <img class="app-logo" src="../../assets/images/index/download/download-app-logo_2.png" />
         <div class="add-to-homescreen-content">
@@ -11,20 +14,16 @@
       <div class="continue-to-h5" @click="closeDialog">{{ $t("modal.addToHomeScreen.continueToUseH5") }}</div>
 
       <div class="add-to-homescreen-btn">
+        <div class="more-benefits-emoji">
+          <img src="../../assets/images/index/download/benefits-emoji.png" alt="" />
+        </div>
         <div class="more-benefits-txt">{{ $t("modal.addToHomeScreen.enjoyMoreBenefits") }}</div>
         <q-btn v-if="Platform.is.ios" no-caps unelevated class="btn-secondary" @click="handleIosBtnClick">
           <img class="os-logo" src="../../assets/images/index/download/ios-logo.png" />
           {{ $t("modal.addToHomeScreen.appAndWeb") }}
         </q-btn>
-        <q-btn
-          v-else
-          no-caps
-          unelevated
-          class="btn-primary btn-primary__full"
-          :href="ui.downloadAppUrl"
-          @click="closeDialog"
-        >
-          <img class="os-logo" src="../../assets/images/index/download/android-logo.png" />
+        <q-btn v-else no-caps unelevated class="btn-primary" :href="ui.downloadAppUrl" @click="closeDialog">
+          <img class="os-logo" src="../../assets/images/index/download/android-logo-black.png" />
           {{ $t("modal.addToHomeScreen.app") }}
         </q-btn>
       </div>
@@ -79,74 +78,130 @@ watch(
 
 <style scoped lang="scss">
 .add-to-homescreen-container {
-  background-color: rgba(255, 255, 255, 0.75);
+  // background-color: rgba(255, 255, 255, 0.75);
+  background-image: url("../../assets/images/index/download/download-bg.png");
+  background-position: top center;
+  background-size: cover;
+  background-color: #fff;
+  border-top-left-radius: 36px;
+  border-top-right-radius: 36px;
   color: #000;
-  padding: 16px;
+  padding: 32px 16px 16px;
+
+  .add-to-homescreen-download {
+    position: absolute;
+    top: -80px;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-left: -12px;
+    img {
+      display: block;
+      width: 100%;
+      max-width: 120px;
+    }
+  }
   .add-to-homescreen-top {
     display: flex;
     align-items: center;
     justify-content: center;
     .app-logo {
-      width: 60px;
-      height: 60px;
+      width: 76px;
+      height: 76px;
       margin-right: 15px;
     }
     .add-to-homescreen-content {
       margin-right: 15px;
-      font-weight: 600;
+      // font-weight: 600;
       font-size: 14px;
     }
   }
 
   .continue-to-h5 {
-    color: #339dff;
-    text-decoration: underline;
+    color: #24ee89;
+    text-decoration: none;
     font-weight: 600;
     width: 100%;
     text-align: center;
-    margin: 18px 0;
+    margin: 12px 0 60px;
     font-size: 13px;
     cursor: pointer;
   }
 
   .add-to-homescreen-btn {
     position: relative;
-    .more-benefits-txt {
-      background-color: #e84d44;
-      color: #fff;
-      padding: 2px 10px;
-      border-radius: 20px;
+    .more-benefits-emoji {
       position: absolute;
-      top: -14px;
+      top: -48px;
       right: 0;
+      img {
+        display: block;
+        width: 44px;
+      }
+    }
+    .more-benefits-txt {
+      // background-color: #e84d44;
+      background: linear-gradient(90deg, #ffa534 0%, #f4622d 100%);
+      color: #fff;
+      padding: 6px 10px;
+      border-radius: 4px;
+      position: absolute;
+      top: -48px;
+      right: 50px;
       z-index: 2;
+      font-size: 13px;
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: -6px; /* Adjusts position below the bubble */
+        right: 10px; /* Moves the triangle towards the right */
+        width: 0;
+        height: 0;
+        border-left: 6px solid transparent;
+        border-right: 6px solid transparent;
+        border-top: 6px solid #f4622d; /* Matches the background color */
+      }
     }
   }
 
   .btn-primary {
-    background: none;
-    background-color: #4488ff;
-    color: #fff;
+    background: linear-gradient(90deg, #2ced88 0%, #9ee871 100%);
+    box-shadow: 0px 2.16px 0px 0px #1cca6a;
+    color: #333;
     font-size: 16px;
     height: 46px;
+    border-radius: 24px;
+    text-transform: uppercase;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
   }
   .btn-secondary {
     background-color: transparent;
-    color: #4488ff;
+    color: #333;
     font-size: 16px;
     height: 46px;
-    border: 1px solid #4488ff;
+    // border: 1px solid #4488ff;
     border-radius: 10px;
-    width: 100%;
+    width: 80%;
+    background: linear-gradient(90deg, #2ced88 0%, #9ee871 100%);
+    box-shadow: 0px 2.16px 0px 0px #1cca6a;
+    border-radius: 24px;
+    text-transform: uppercase;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
   }
   .btn-secondary::before {
     background: none;
   }
 
   .os-logo {
-    width: 20px;
-    height: 20px;
+    width: 30px;
+    height: 30px;
     margin-right: 8px;
+    filter: grayscale(1);
   }
 }
 
@@ -173,8 +228,9 @@ watch(
   background-color: #9e9e9e;
   color: #fff;
   position: absolute;
-  right: 10px;
-  top: 8px;
+  right: 16px;
+  top: 16px;
   font-size: 0.6em;
+  display: none;
 }
 </style>
