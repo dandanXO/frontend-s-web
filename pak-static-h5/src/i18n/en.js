237dceb9f7ba28ed7d7503764bcdbd343e241499
Kai Wen <kaiwen@caspen-tech.com>
2025-04-03 10:56:16 +0800

    fix: [pak] Add translation

--- pak-static-h5/src/i18n/en.js
+++ pak-static-h5/src/i18n/en.js
@@ -294,6 +294,7 @@ export default {
     loggingOut: "Logging out...",
     phoneVerifySuccessful: "Phone verified successfully",
     emailAlreadyUsed: "Email already used. Please try another email.",
+    bankAddedSuccessfully: "Bank card added successfully",
     virtualWalletAddedSuccessfully: "Virtual wallet added successfully",
     newpasswordupdated: "New password updated successfully",
     withdrewSuccessfully: "Withdrew successfully",
