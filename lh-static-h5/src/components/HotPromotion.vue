b222dddfa33fce1e92e43ebe4ee48c654dd4e483
Sam <sam@flex-df.com>
2025-04-16 13:38:10 +0800

    feat: [lh h5] Add yu'e bao promo

--- lh-static-h5/src/components/HotPromotion.vue
+++ lh-static-h5/src/components/HotPromotion.vue
@@ -77,6 +77,7 @@
     <Cct2025 v-else-if="list.redirectUrl === 'lh1-cct-global-finals-2025'" :promo-code="list.promoCode" />
     <FpSlotPromo v-else-if="list.redirectUrl === 'lh1-fp-slot'" :promo-code="list.promoCode" />
     <elsOne2025 v-else-if="list.redirectUrl === 'lh1-esl-one-raleigh-2025'" :promo-code="list.promoCode" />
+    <YuEBaoPromo v-else-if="list.redirectUrl === 'lh1-yu-e-bao'" :promo-code="list.promoCode" />
   </div>
 
   <q-dialog v-model="isClaimModal" persistent>
@@ -186,6 +187,7 @@ const PGLBucharest2025 = defineAsyncComponent(() => import("./hotpromo/pgl-bucha
 const Blast2025S3 = defineAsyncComponent(() => import("./hotpromo/Blast2025-s3/Blast2025S3.vue"));
 const FpSlotPromo = defineAsyncComponent(() => import("./hotpromo/fp-slot-promo/FpSlotPromo.vue"));
 const elsOne2025 = defineAsyncComponent(() => import("./hotpromo/elsOne2025/elsOne2025.vue"));
+const YuEBaoPromo = defineAsyncComponent(() => import("./hotpromo/yu-e-bao/YuEBaoPromo.vue"));
 
 export default defineComponent({
   name: "HotPromo",
@@ -248,7 +250,8 @@ export default defineComponent({
     FissureUniverseS4,
     IemMelbourne2025,
     PglWallachiaS4,
-    FpSlotPromo
+    FpSlotPromo,
+    YuEBaoPromo
   },
   props: {
     list: {
