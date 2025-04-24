d1e6a4ff46797807a94f2153f61a8137760c6000
Sam <sam@flex-df.com>
2025-04-24 15:07:07 +0800

    feat: [vnm web/h5] Remove unused domains

--- vnm-static-h5/src/boot/utils.js
+++ vnm-static-h5/src/boot/utils.js
@@ -129,7 +129,6 @@ export const toGMT7 = (dateTime, format = "DD/MM/YYYY hh:mm A") => {
 export const isOperaPixelUrl = () => {
   if (
     window.location.href.indexOf("5svn88.com") > -1 ||
-    window.location.href.indexOf("tfpromo88.com") > -1 ||
     window.location.href.indexOf("tf88bof.com") > -1 ||
     window.location.href.indexOf("tslotf88.com") > -1 ||
     window.location.href.indexOf("tfnohu18.com") > -1 ||
