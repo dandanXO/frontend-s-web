6c318d93ebd9cf87e3bb701fa92834e8a1151d58
WeiSeng <weiseng@eagrp.net>
2025-05-14 17:51:01 +0800

    feat: LH h5 vue websocket

--- lh-static-h5/src/boot/vue-native-websocket.js
+++ lh-static-h5/src/boot/vue-native-websocket.js
@@ -2,25 +2,17 @@ import { boot } from "quasar/wrappers";
 import VueNativeSock from "vue-native-websocket-vue3";
 import { useSocketStore } from "src/cs-client-web/stores/socket";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ app }) => {
+  // ✅ 现在 Pinia 已经注册，可以安全调用 store
   const store = useSocketStore();
   store.appConfig = app.config;
 
-  // 使用VueNativeSock插件，并进行相关配置
   app.use(VueNativeSock, "//127.0.0.1", {
-    // 启用pinia集成 | enable pinia integration
-    store: store,
-    // 数据发送/接收使用使用json
+    store,
     format: "json",
-    // 开启手动调用 connect() 连接服务器
     connectManually: true,
-    // 开启自动重连
     reconnection: false,
-    // 尝试重连的次数
     reconnectionAttempts: 5,
-    // 重连间隔时间
     reconnectionDelay: 3000
   });
 });
