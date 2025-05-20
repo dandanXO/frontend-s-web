e43e1671129d07c1a50b4b4899cfa59cdecc26a7
zhixuan <zhixuan@preec.net>
2025-05-20 13:41:54 +0800

    update: [LH web] add open new cs window

--- lh-static-web/src/store/index.js
+++ lh-static-web/src/store/index.js
@@ -156,31 +156,39 @@ export const userStore = defineStore("userStore", {
       });
     },
     openLiveChat() {
+      // const left = (screen.width - 350) * 2;
+      // const top = (screen.height - 650) / 4;
+
+      // return getCSAFromServer()
+      //   .then((res) => {
+      //     // console.log(res.data);
+      //     var lineUrl = "";
+      //     const randNum = Math.floor(Math.random() * 2) + 1;
+      //     if (randNum === 1) {
+      //       lineUrl = res.data.liveUrl1;
+      //     } else {
+      //       lineUrl = res.data.liveUrl2;
+      //     }
+
+      //     const token = this.token ?? "";
+      //     window.open(
+      //       // `https://csweb01.c8nhwrqx4.com/?partnerCode=DYCS&way=WEB&lang=zh-CN&token=${this.token}`,
+      //       `${lineUrl}&token=${token}`,
+      //       "Chat Server",
+      //       "resizable=yes, width=" + 800 + ", height=" + 880 + ", top=" + top + ", left=" + left
+      //     );
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+
+      const width = 500;
+      const height = 880;
       const left = (screen.width - 350) * 2;
       const top = (screen.height - 650) / 4;
 
-      return getCSAFromServer()
-        .then((res) => {
-          // console.log(res.data);
-          var lineUrl = "";
-          const randNum = Math.floor(Math.random() * 2) + 1;
-          if (randNum === 1) {
-            lineUrl = res.data.liveUrl1;
-          } else {
-            lineUrl = res.data.liveUrl2;
-          }
-
-          const token = this.token ?? "";
-          window.open(
-            // `https://csweb01.c8nhwrqx4.com/?partnerCode=DYCS&way=WEB&lang=zh-CN&token=${this.token}`,
-            `${lineUrl}&token=${token}`,
-            "Chat Server",
-            "resizable=yes, width=" + 800 + ", height=" + 880 + ", top=" + top + ", left=" + left
-          );
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      const url = `https://${window.location.host}/live-chat`;
+      window.open(url, "Chat Server", `resizable=yes, width=${width}, height=${height}, top=${top}, left=${left}`);
     }
   }
 });
