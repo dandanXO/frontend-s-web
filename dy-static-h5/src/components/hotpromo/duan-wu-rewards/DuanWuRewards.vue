548707b2ea7a6fc032378919c38a789561db16e6
dan <dan@homway.tw>
2025-05-21 12:41:50 +0800

    update table

--- dy-static-h5/src/components/hotpromo/duan-wu-rewards/DuanWuRewards.vue
+++ dy-static-h5/src/components/hotpromo/duan-wu-rewards/DuanWuRewards.vue
@@ -829,6 +829,7 @@ onMounted(() => {
 }
 
 .dialog {
+  max-height: 400px !important;
   background-color: #fbfbe3;
   width: 80%;
   min-height: 504px;
@@ -838,8 +839,9 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   .inner-table{
+    overflow-y: scroll;
+    height: 300px;
     table {
-
       border-collapse: separate; /* 關鍵！讓邊角能獨立呈現 */
       border-spacing: 0;
     }
