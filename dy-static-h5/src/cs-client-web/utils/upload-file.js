7d688bd1fab4007abc05256ef7c1b30dfac02835
zhixuan <zhixuan@preec.net>
2025-06-04 14:54:13 +0800

    fix: [DY] cs page - upload file

--- dy-static-h5/src/cs-client-web/utils/upload-file.js
+++ dy-static-h5/src/cs-client-web/utils/upload-file.js
@@ -6,12 +6,10 @@ export default function uploadFile(fileName, file, progressCallback) {
   return new Promise((resolve, reject) => {
     fileApi.upload(file, progressCallback).then((res) => {
       // 文件上传失败
-      if (!res.data.success) {
+      if (res.code !== 0) {
         reject({ msg: "System exception", code: -1 });
-      }
-
-      if (res.data.success) {
-        let path = res.data.data[0];
+      } else {
+        let path = res.data[0];
         let fileLast = path.split("/").slice(-1)[0];
         let fileUrl = `${CDN_URL}/${path}`;
 
