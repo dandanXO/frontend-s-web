8b3005741a86cf02010bd0603a321500624137fa
zhixuan <zhixuan@preec.net>
2025-06-11 18:05:15 +0800

    update: [pk3] remove redundant code

--- pk3-static-v3/src/pages/account/OrderView.vue
+++ pk3-static-v3/src/pages/account/OrderView.vue
@@ -180,7 +180,6 @@ const isWithdrawLastPage = ref(false);
 const scrollWithdrawRef = ref(null);
 
 const onLoadWithdraw = (index, done) => {
-  isWithdrawLastPage.value = true;
   const { startDate, endDate } = searchForm;
   const gmtStartDate = convertToGMT8(startDate);
   const gmtEndDate = convertToGMT8(endDate);
