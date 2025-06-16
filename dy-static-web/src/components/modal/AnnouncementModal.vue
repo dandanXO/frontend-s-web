210e973225c11fc219ecdcd98af6f659f0955642
eudora-hsj <eudora.hsj@gmail.com>
2025-06-16 16:45:43 +0800

    dy-web-inbox style update (copy lh) -1

--- dy-static-web/src/components/modal/AnnouncementModal.vue
+++ dy-static-web/src/components/modal/AnnouncementModal.vue
@@ -1,57 +1,102 @@
 <template>
   <el-dialog
-    v-model="isAnnouncementModalVisible"
+    v-model="visible"
     destroyOnClose
     :append-to-body="true"
     :show-close="false"
-    width="520px"
+    width="540px"
     class="announcement-modal"
   >
-    <div class="header">
-      <img class="header-icon" src="../../assets/home/announcement/mail-icon.svg" />
-      <span class="header-title">站内信</span>
-      <img class="close-icon" src="../../assets/home/announcement/close-icon.svg" @click="isAnnouncementModalVisible = false" />
-    </div>
-    <div class="content">
-      <div class="item" :class="{'unread-status': !inboxItem.readTime}" v-for="inboxItem, index in inboxData">
-        <input type="checkbox" :id="`toggle-${index}`">
-        <label :for="`toggle-${index}`" class="content-title">
-          <div class="title-label" v-html="inboxItem.title" />
-          <div class="read-status" v-if="inboxItem.readTime">已读</div>
-          <img class="collapse-arrow" src="../../assets/home/announcement/right-arrow.svg" @click.stop="goToMailDetail(inboxItem)" />
-        </label>
-        <div class="content-desc" v-html="inboxItem.content" />
+    <div>
+      <div class="dialog-wrapper">
+        <div class="dialog-header only-inbox">
+          <div
+            class="dialog-tab-item"
+            :class="currentTab === 'announcement' ? 'active' : ''"
+            @click="currentTab = 'announcement'"
+            v-if="announceData.length > 0"
+          >
+            <img
+              v-if="currentTab === 'announcement'"
+              src="../../assets/home/announcement/tab-announcement-active.png"
+              alt=""
+            />
+            <p v-else>重要公告</p>
+          </div>
+          <div class="dialog-tab-item" :class="currentTab === 'inbox' ? 'active' : ''" @click="currentTab = 'inbox'">
+            <img v-if="currentTab === 'inbox'" src="../../assets/home/announcement/tab-inbox-active.png" alt="" />
+            <p v-else>站內信</p>
+          </div>
+        </div>
+        <div class="dialog-content">
+          <InboxComponent v-if="currentTab === 'inbox' && mailData.length > 0" :mailData="mailData" />
+          <AnnouncementComponent
+            v-if="currentTab === 'announcement' && announceData.length > 0"
+            :announceData="announceData"
+          />
+        </div>
+      </div>
+      <div class="dialog-action">
+        <div class="dialog-action-row today-not-remind">
+          <div class="dialog-action-item">
+            <el-checkbox v-model="checked" style="color: white" text-color="white">今天不再提醒</el-checkbox>
+          </div>
+        </div>
+
+        <div class="dialog-action-row">
+          <div class="dialog-action-item close-icon" @click="visible = false">
+            <el-icon size="32px">
+              <img src="../../assets/home/close-circle-fill.svg" />
+            </el-icon>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div class="actions">
-      <div class="cs-btn"  @click="store.openLiveChat()">
-        <img src="../../assets/home/announcement/cs-icon.svg" />联系客服</div>
-    </div>
-
-    <img class="close-icon-outlined" src="../../assets/home/announcement/close-icon-outlined.svg" @click="isAnnouncementModalVisible = false" />
-    <el-checkbox class="do-not-remind" v-model="checked" style="color: white" text-color="white">今天不再提醒</el-checkbox>
   </el-dialog>
 </template>
 
 <script setup>
+import { ref, watch, onMounted } from "vue";
+import InboxComponent from "./InboxComponent.vue";
+import AnnouncementComponent from "./AnnouncementComponent.vue";
 import { popupMailBox } from "@/api/personal/mailbox";
 import { userStore } from "@/store";
-import { ref, watch } from "vue";
 import { useLocalStorage } from "@vueuse/core";
 import moment from "moment";
-import { useRouter } from "vue-router";
 
-const router = useRouter();
-const isAnnouncementModalVisible = ref(false);
-const inboxData = ref([]);
 const store = userStore();
-const lastAnnouncementDateStr = useLocalStorage("DY_LAST_ANNOUNCEMENT_DATE", null);
-const checked = ref(false);
+const lastAnnouncementDateStr = useLocalStorage("LH_LAST_ANNOUNCEMENT_DATE", null);
 
-const goToMailDetail = (mail) => {
-  router.push(`/center/mailbox?mailid=${mail.id}&type=${mail.type}`);
-};
+const visible = ref(false);
+const currentTab = ref("inbox");
+const checked = ref(false);
+const mailData = ref([]);
+const announceData = ref([]);
+
+onMounted(() => {
+  // if (!store.token) return;
+  //
+  // if (lastAnnouncementDateStr.value) {
+  //   const today = moment();
+  //   const lastAnnouncementDate = moment(lastAnnouncementDateStr.value);
+  //   const diff = today.diff(lastAnnouncementDate, "days");
+  //   if (!diff) return;
+  // }
+  // popupMailBox()
+  //   .then((res) => {
+  //     if (res.code === 0) {
+  //       mailData.value = res.data;
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  //   .finally(() => {
+  //     if (mailData.value.length > 0) {
+  //       visible.value = true;
+  //     }
+  //   });
+});
 
 watch(
   () => store.token,
@@ -67,15 +112,15 @@ watch(
       popupMailBox()
         .then((res) => {
           if (res.code === 0) {
-            inboxData.value = res.data;
+            mailData.value = res.data;
           }
         })
         .catch((err) => {
           console.log(err);
         })
         .finally(() => {
-          if (inboxData.value.length > 0) {
-            isAnnouncementModalVisible.value = true;
+          if (mailData.value.length > 0) {
+            visible.value = true;
           }
         });
     }
@@ -91,211 +136,98 @@ watch(checked, (val) => {
 </script>
 
 <style lang="scss" scoped>
-.header {
-  background: linear-gradient(90deg, #32B8FD 0%, #DB70FF 100%);
-  color: #f8f8f8;
+.dialog-header {
+  background: linear-gradient(0deg, #3480f9 0%, #6cadff 100%);
+  height: 42px;
   display: flex;
   align-items: center;
-  gap: 10px;
-  width: 520px;
-  height: 48px;
-
-  .header-icon {
-    margin-left: 20px;
-  }
-
-  .close-icon {
-    margin-left: auto;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .header-title {
-    font-family: Inter;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 100%;
-    letter-spacing: 0%;
-  }
-}
-
-.content {
-  background-color: #f2f2f2;
-  height: 400px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  padding: 20px;
-  overflow-y: scroll;
-
-  .item {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    border-bottom: 1px solid #d7d7d7;
-    justify-content: center;
-
-    .content-title {
-      display: flex;
-      gap: 10px;
-      font-family: 'PingFang SC';
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 20px;
-
-      .title-label {
-        display: flex;
-        align-items: center;
-        color: #000;
-        
-        :deep(p) {
-          margin: 0 !important;
-        }
-      }
-    }
-
-    .content-desc {
-      font-family: 'PingFang SC';
-      font-weight: 400;
-      font-size: 14px;
-      line-height: 20px;
-      max-height: 0;
-      overflow: hidden;
-      opacity: 0;
-      padding: 0 10px;
-      color: #000;
-    }
-
-    input {
-      display: none;
-    }
-
-    input:checked ~ .content-desc {
-      max-height: 500px; /* Set a larger max-height to ensure smooth expansion */
-      opacity: 1;
-      padding: 10px;
-    }
-
-    .collapse-arrow {
-      transition: transform 0.3s ease-in-out;
-      color: #d9d9d9;
-      font-size: 14px;
-      margin-left: auto;
-    }
-
-    input:checked ~ .content-title .collapse-arrow {
-      transform: rotate(90deg);
-    }
-
-    .read-status {
-      background: #D9D9D9;
-      color: #fff;
-      width: 40px;
-      height: 20px;
-      border-radius: 4px;
-      font-family: 'PingFang SC';
-      font-weight: 400;
-      font-size: 14px;
-      line-height: 20px;
-      text-align: center;
-    }
-
-    &.unread-status {
-      .content-title {
-        .title-label {
-          color: #000000;
-          font-weight: 700;
-
-          &:after {
-            content: "";
-            width: 8px;
-            height: 8px;
-            background-color: red;
-            border-radius: 50%;
-            display: inline-block;
-            margin-left: 10px;
-          }
-        }
-      }
-    }
-  }
-}
-.actions {
-  display: flex;
   justify-content: center;
-  background-color: #f2f2f2;
-  padding: 20px;
-
-  .cs-btn, .redirect-btn {
-    cursor: pointer;
-
-    &:hover {
-      filter: brightness(0.9);
-    }
-
-    &:active {
-      transform: translateY(2px);
-    }
-  }
-
-  .cs-btn {
-    border: 1px solid #D1D5DB;
-    box-shadow: 0px 1px 2px 0px #0000000D;
-    width: 230px;
-    height: 40px;
-    border-radius: 100px;
-    border-width: 1px;
-    padding-top: 9px;
-    padding-right: 17px;
-    padding-bottom: 9px;
-    padding-left: 17px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-  }
-
-  .redirect-btn {
-    width: 480px;
-    height: 41px;
-    border-radius: 100px;
-    background: linear-gradient(180deg, #52ACFF 0%, #3559DA 100%);
-    width: 230px;
-    height: 40px;
-    border-radius: 100px;
-    gap: 6px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    color: #fff;
-  }
-}
-
-
-.do-not-remind {
-  position: absolute;
-  right: 0;
-}
-
-.close-icon-outlined {
-  position: absolute;
-  right: 50%;
-  bottom: -7%;
-  transform: translate(50%, 0%);
+  border-radius: 12px 12px 0 0;
+  position: relative;
   cursor: pointer;
-}
 
-</style>
-<style lang="scss">
-.announcement-modal {
-  background: transparent !important;
+  &.only-inbox {
+    background: #fff;
+    height: 52px;
+    border-bottom: 1px solid #999;
+  }
 
-  .el-dialog__body {
-    padding: 0;
-    border-radius: 10px;
+  .dialog-tab-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    border-radius: 12px 12px 0 0;
     overflow: hidden;
 
+    &.active {
+      img {
+        height: 50px;
+      }
+    }
   }
+
+  p {
+    margin: 0;
+    color: white;
+    font-size: 20px;
+    white-space: nowrap;
+  }
+}
+
+.dialog-footer {
+  background: #e8f0fd;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0 0 12px 12px;
+  gap: 4px;
+
+  .dot {
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: #bbcbdb;
+
+    &.active {
+      background: #2792fd;
+      width: 20px;
+      border-radius: 56px;
+    }
+  }
+}
+
+.dialog-action {
+  position: absolute;
+  right: 0;
+  width: 100%;
+
+  .dialog-action-row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &.today-not-remind {
+      justify-content: flex-end;
+    }
+  }
+
+  .dialog-action-item {
+    &.close-icon {
+      justify-content: center;
+      cursor: pointer;
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+      background: #ffffff99;
+      border: 1px solid #ffffffb2;
+      fill: black;
+    }
+  }
+}
+
+::v-deep(.el-checkbox__input.is-checked + .el-checkbox__label) {
+  color: white !important;
 }
 </style>
---
+++ dy-static-web/src/components/modal/InboxComponent.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="announcement-component">
+    <el-carousel class="banner-slider" :autoplay="false" :interval="5000">
+      <el-carousel-item class="banner-container" v-for="item in mailData" :key="item.id">
+        <div class="announcement-title" v-html="item.title"></div>
+        <template v-if="item.content">
+          <div class="announcement-content" v-html="item.content"></div>
+        </template>
+
+        <div class="announcement-footer">
+          <div class="footer-button" @click="store.openLiveChat()">
+            联系客服
+            <img class="cs-icon" src="@/assets/images/home/sticky-sidebar/cs-icon.svg" />
+          </div>
+          <!--          v-show="item.redirectType !== 'NONE'"-->
+          <div class="footer-button detail" @click="goToMailDetail(item)">
+            查看详情
+            <el-icon :size="20">
+              <img src="../../assets/home/arrow-drop-right-line.svg" />
+            </el-icon>
+          </div>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script setup>
+import { userStore } from "@/store";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const props = defineProps({
+  mailData: {
+    type: Array,
+    default: () => []
+  }
+});
+const store = userStore();
+
+const goToMailDetail = (mail) => {
+  console.log(mail);
+  router.push(`/center/mailbox?mailid=${mail.id}&type=${mail.type}`);
+};
+</script>
+
+<style lang="scss" scoped>
+.banner-container {
+  min-height: 400px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 46px;
+}
+.announcement-component {
+  padding: 10px 12px 16px;
+}
+
+.announcement-title {
+  font-size: 16px;
+  font-weight: 600;
+  color: black;
+  margin-bottom: 10px;
+}
+
+.announcement-content {
+  font-size: 16px;
+  color: #666666;
+  margin-bottom: 12px;
+  flex: 1;
+  overflow: auto;
+  margin-bottom: 70px;
+}
+
+.announcement-footer {
+  display: flex;
+  justify-content: space-between;
+  gap: 12px;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  left: 0;
+  bottom: 50px;
+
+  .footer-button {
+    cursor: pointer;
+    color: #2f3244;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 6px;
+    height: 48px;
+    font-size: 16px;
+    border: 1px solid #2f3244;
+    flex: 1;
+    gap: 2px;
+
+    &.detail {
+      background: #2792fd;
+      color: white;
+      border: none;
+    }
+    img.cs-icon {
+      width: unset;
+    }
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+    &:active {
+      transform: translate(0px, 1px);
+      opacity: 0.9;
+    }
+  }
+}
+
+:deep(.el-carousel__container) {
+  height: 600px;
+  width: 100%;
+}
+</style>
