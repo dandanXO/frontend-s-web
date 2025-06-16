c302a2e7bbbe315973858ac40294636f24f4a437
Kai Wen <kaiwen@caspen-tech.com>
2025-06-16 15:23:03 +0800

    feat: [manager, vnm] Add continuity for lang preference between web, h5 and vnm aff web

--- xf-lh-dy-affiliate-web/src/components/lang/LangToggle.vue
+++ xf-lh-dy-affiliate-web/src/components/lang/LangToggle.vue
@@ -1,17 +1,14 @@
 <template>
   <el-select v-model="languageVal" placeholder="Select" size="small" style="position:absolute;top:10px;right:25px;width:120px;" @change="changeLang">
-    <el-option v-for="item in langOptions" :key="item.value" :label="item.label" :value="item.value" />
+    <el-option :label="$t('fields.vietnamese')" :value="'vi'" />
+    <el-option :label="$t('fields.english')" :value="'en'" />
   </el-select>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { i18nStore } from '@/store/language';
 import { storeToRefs } from 'pinia';
-import { useI18n } from "vue-i18n";
 
-const { t } = useI18n();
-const langOptions = ref([{ label: t('fields.vietnamese'), value: 'vi' }, { label: t('fields.english'), value: 'en' }]);
 const i18nStoreLanguage = i18nStore();
 const { languageVal } = storeToRefs(i18nStoreLanguage);
 
