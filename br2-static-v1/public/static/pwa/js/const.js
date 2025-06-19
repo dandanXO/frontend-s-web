1c039d4b6da378b8f380c76dcac1029de4827849
WeiSeng <weiseng@eagrp.net>
2025-06-19 14:19:03 +0800

    feat: BR1 pwa put

---
+++ br2-static-v1/public/static/pwa/js/const.js
@@ -0,0 +1,29 @@
+export const PWA_DATA_KEY = "BRA_PWA_DATA";
+export const INSTALLATION_STATUS_KEY = "BRA_INSTALLATION_STATUS";
+export const FBQ_INITIALIZED = 'BRA_FBQ_INITIALIZED';
+
+export const domainLists = {
+
+};
+
+export const affiliateLists = {
+
+};
+
+export const fbqLists = {
+  "gwu7c.cc": {
+    id: "617199024196854"
+  },
+  "58b2e.cc": {
+    id: "924020233240132"
+  },
+  "854le.cc": {
+    id: "915917220527164"
+  },
+  "z8m7m.cc": {
+    id: "1578015609491051"
+  },
+  "ri6yz.cc": {
+    id: "585628870757333"
+  }
+};
