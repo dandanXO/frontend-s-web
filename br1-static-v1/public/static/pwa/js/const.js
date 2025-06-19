6f7fbfd4216df1d62ead8a6334ca01741562d5fa
WeiSeng <weiseng@eagrp.net>
2025-06-19 14:53:51 +0800

    refactor: BR2 to BR1

--- br1-static-v1/public/static/pwa/js/const.js
+++ br1-static-v1/public/static/pwa/js/const.js
@@ -3,139 +3,27 @@ export const INSTALLATION_STATUS_KEY = "BRA_INSTALLATION_STATUS";
 export const FBQ_INITIALIZED = 'BRA_FBQ_INITIALIZED';
 
 export const domainLists = {
-  "6sx65.cc": {
-    affiliateCode: "4059DF"
-  },
-  "dtaw7.cc": {
-    affiliateCode: "773A06"
-  },
-  "5zves.cc": {
-    affiliateCode: "E92EF9"
-  },
-  "g8wwo.cc": {
-    affiliateCode: "0D5F18"
-  },
-  "0zupt.cc": {
-    affiliateCode: "B14C1A"
-  },
-  "cakup.cc": {
-    affiliateCode: "C8B4A4"
-  },
-  "3275x.cc": {
-    affiliateCode: "53246F"
-  },
-  "vjjxe.cc": {
-    affiliateCode: "DC49A4"
-  },
-  "ktmqb.cc": {
-    affiliateCode: "AA97F0"
-  },
-  "lq246.cc": {
-    affiliateCode: "697F04"
-  },
-  "i8seg.cc": {
-    affiliateCode: "9FBFEA"
-  },
-  "z9nsx.cc": {
-    affiliateCode: "D3827F"
-  },
-  "rp864.cc": {
-    affiliateCode: "2A7CDB"
-  },
-  "wypkv.cc": {
-    affiliateCode: "FC33FB"
-  },
-  "phy0n.cc": {
-    affiliateCode: "FA1365"
-  },
-  "8bpi8.cc": {
-    affiliateCode: "64C9D0"
-  },
-  "nk1ta.cc": {
-    affiliateCode: "9E8F63"
-  },
-  "sloju.cc": {
-    affiliateCode: "3A6987"
-  },
-  "9cjxw.cc": {
-    affiliateCode: "177183"
-  },
-  "1mw9q.cc": {
-    affiliateCode: "5DE816"
-  }
+
 };
 
 export const affiliateLists = {
-  "6805B0": {
-    adCode: "1jbcv746"
-  },
-  "4059DF": {
-    adCode: "1jo14aqf"
-  },
-  "773A06": {
-    adCode: "1jubhy46"
-  },
-  "E92EF9": {
-    adCode: "1j42dvtz"
-  },
-  "0D5F18": {
-    adCode: "1jmuatvy"
-  },
-  "B14C1A": {
-    adCode: "1jaiy8ka"
-  },
-  "C8B4A4": {
-    adCode: "1j5oiyte"
-  },
-  "53246F": {
-    adCode: "1j4ka3qm"
-  },
-  "DC49A4": {
-    adCode: "1jhwqrie"
-  },
-  "AA97F0": {
-    adCode: "1j281k2x"
-  },
-  "697F04": {
-    adCode: "1jmlxn4s"
-  },
-  "9FBFEA": {
-    adCode: "1jfbq4li"
-  },
-  "D3827F": {
-    adCode: "1jeszen4"
-  },
-  "2A7CDB": {
-    adCode: "1japf1ft"
-  },
-  "FC33FB": {
-    adCode: "1j13zm26"
-  },
-  "FA1365": {
-    adCode: "1j714ba4"
-  },
-  "64C9D0": {
-    adCode: "1j3gm5sl"
-  },
-  "9E8F63": {
-    adCode: "1j4o14xq"
-  },
-  "3A6987": {
-    adCode: "1j6px6oz"
-  },
-  "177183": {
-    adCode: "1j1u1pjq"
-  },
-  "5DE816": {
-    adCode: "1jm2f4nf"
-  }
+
 };
 
 export const fbqLists = {
-  "6sx65.cc": {
-    id: "906416248141520"
+  "gwu7c.cc": {
+    id: "617199024196854"
   },
-  "dtaw7.cc": {
-    id: "1129685552192624"
+  "58b2e.cc": {
+    id: "924020233240132"
+  },
+  "854le.cc": {
+    id: "915917220527164"
+  },
+  "z8m7m.cc": {
+    id: "1578015609491051"
+  },
+  "ri6yz.cc": {
+    id: "585628870757333"
   }
 };
--- br2-static-v1/public/static/pwa/js/pwa.js
+++
@@ -1,8 +0,0 @@
-import { redirectToGame } from "./redirect.js";
-
-if (window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true) {
-  const fbclid = localStorage.getItem("fbclid");
-  redirectToGame(fbclid);
-} else {
-  window.location.replace(`${window.location.origin}/register`)
-}
