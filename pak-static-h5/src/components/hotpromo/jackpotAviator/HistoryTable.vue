7af09828b0388a3a6b2227cf3ef4e9f2496b5820
Micha <mika.caspen@gmail.com>
2025-06-21 14:40:25 +0800

    fix: [pak] update aviator

--- pak-static-h5/src/components/hotpromo/jackpotAviator/HistoryTable.vue
+++ pak-static-h5/src/components/hotpromo/jackpotAviator/HistoryTable.vue
@@ -34,8 +34,10 @@ const props = defineProps(['historyList']);
     
     border-radius: 5px;
     overflow: hidden;
-        margin: 0 0 70px;
+        // margin: 0 0 70px;
+    margin: 0;
         
+    max-height: calc(100vh - 600px);
     // padding-bottom: 0px;
         // padding-bottom: 15px;
     .item {
@@ -46,7 +48,7 @@ const props = defineProps(['historyList']);
         font-family: Poppins;
         font-size: 14px;
         font-weight: 600;
-        line-height: 40px;
+        line-height: 20px;
         text-align: center;
         color: #FFFFFF80;
         height: 30px;
