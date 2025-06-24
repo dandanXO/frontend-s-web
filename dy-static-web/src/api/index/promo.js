097ab179760b88e0992c10d75368cc48fdbd2277
zhixuan <zhixuan@preec.net>
2025-06-24 15:20:59 +0800

    feat: [DY] add fissure universe s5 promo

--- dy-static-web/src/api/index/promo.js
+++ dy-static-web/src/api/index/promo.js
@@ -776,3 +776,10 @@ export function getNewUserSportBonus2025Bonus(promoCode) {
 export function claimNewUserSportBonus2025Bonus(promoCode) {
   return server.EVENT.post(`/session/game-type-bonus/claim?promoCode=${promoCode}`);
 }
+
+export function getFissureUniverseS5Bonus(promoCode) {
+  return server.EVENT.get(`/session/competition/yesterday?promoCode=${promoCode}`);
+}
+export function claimFissureUniverseS5Bonus(promoCode) {
+  return server.EVENT.post(`/session/competition/claimBonus?promoCode=${promoCode}`);
+}
