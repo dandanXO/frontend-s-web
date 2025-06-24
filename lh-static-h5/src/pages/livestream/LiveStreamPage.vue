5481802a70826fb1c595331f246ddc8b2f29ca08
Sam <sam@flex-df.com>
2025-06-24 19:40:53 +0800

    Revert "feat: [lh web/h5] Remove liststream sort"
    This reverts commit 2f0258229a2a15e17f5f855fbb8f7f29dabce271.

--- lh-static-h5/src/pages/livestream/LiveStreamPage.vue
+++ lh-static-h5/src/pages/livestream/LiveStreamPage.vue
@@ -11,7 +11,7 @@
     <q-tab-panels v-model="moduleTab" class="content-panels" animated keep-alive>
       <q-tab-panel class="content-panel" name="livestream">
         <livestreamList
-          :livestream-list="liveStreamList"
+          :livestream-list="sortedLiveStreamList"
           :is-loading="isLivestreamListLoading"
           @livestream-click="handleLivestreamClick"
         />
@@ -62,6 +62,9 @@ const openGame = (gameName, code, gameCode) => {
 };
 
 const liveStreamList = ref([]);
+const sortedLiveStreamList = computed(() => {
+  return [...liveStreamList.value].sort((a, b) => a.sort - b.sort);
+});
 
 const getLiveUrlList = () => {
   isLivestreamListLoading.value = true;
