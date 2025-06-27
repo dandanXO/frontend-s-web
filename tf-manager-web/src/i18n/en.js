7ed7ada44f7f796fae9b4ce762ca9462bd417e21
Mingpoo <soonmingpoo@yahoo.com>
2025-06-27 14:31:44 +0800

    feat: update en

--- tf-manager-web/src/i18n/en.js
+++ tf-manager-web/src/i18n/en.js
@@ -3557,6 +3557,8 @@ export default {
     'Sport Live Block Member': 'Sport Live Block Member Setting',
     'Sport Live Chat Vip Status': 'Sport Live Vip Message Setting',
     'Affiliate Settlement Setting': 'Affiliate Settlement Setting',
+    'Sport Live History Block': 'Sport Live History Message',
+    'Sport Live Monitor': 'Sport Live Monitor',
     'Sport Live Admin': 'Sport Live Admin Setting',
     'Member Refer Stat Analysis': 'Refer Friend Analysis',
     'Member Refer Relation': 'Refer Relation Query',
