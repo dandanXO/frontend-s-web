d01fdc45b30ca5993836b0997ab4be641b05c090
Kai Wen <kaiwen@caspen-tech.com>
2025-07-05 19:48:34 +0800

    fix: [lk6 web] Remove unused imgs

--- lk6-static-web/src/views/home/index.vue
+++ lk6-static-web/src/views/home/index.vue
@@ -1,11 +1,6 @@
 <template>
-    <DarkHomeView v-if="isDark" />
-    <HomeView v-else />
+    <HomeView />
 </template>
 <script setup>
-import { useDark } from "@vueuse/core";
 import HomeView from "@/views/home/HomeView.vue";
-import DarkHomeView from "@/views/home/DarkHomeView.vue";
-
-const isDark = useDark();
 </script>
