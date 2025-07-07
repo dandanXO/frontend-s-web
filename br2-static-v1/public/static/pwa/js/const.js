d5b5a7d09b99b2626660f20d9f998dc51b710816
WeiSeng <weiseng@eagrp.net>
2025-07-07 18:40:42 +0800

    feat: BR2 prepare pwa

---
+++ br2-static-v1/public/static/pwa/js/const.js
@@ -0,0 +1,9 @@
+export const PWA_DATA_KEY = "BRA_PWA_DATA";
+export const INSTALLATION_STATUS_KEY = "BRA_INSTALLATION_STATUS";
+export const FBQ_INITIALIZED = "BRA_FBQ_INITIALIZED";
+
+export const domainLists = {};
+
+export const affiliateLists = {};
+
+export const fbqLists = {};
