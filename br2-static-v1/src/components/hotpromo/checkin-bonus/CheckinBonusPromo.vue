59f119d294797a3010b0868ae31aa07a5f949529
WeiSeng <weiseng@eagrp.net>
2025-07-07 14:17:49 +0800

    style: BR2 move css

--- br2-static-v1/src/components/hotpromo/checkin-bonus/CheckinBonusPromo.vue
+++ br2-static-v1/src/components/hotpromo/checkin-bonus/CheckinBonusPromo.vue
@@ -334,44 +334,6 @@ onMounted(() => {
   }
 }
 
-.promo-subtitle {
-  // padding: 0 24px;
-  margin-top: 24px;
-  text-transform: uppercase;
-  font-weight: 900;
-  color: #ffffff;
-  font-size: 18px;
-  line-height: 1;
-  font-style: italic;
-  text-align: center;
-  position: relative;
-  display: flex;
-  justify-content: center;
-
-  &:before {
-    content: "";
-    background: url(../../../assets/images/promotion/receive-earn/promo-subtitle-icon.png);
-    width: 25px;
-    height: 9px;
-    display: block;
-    background-size: 100% 100%;
-    margin-right: 8px;
-    margin-top: 4px;
-  }
-
-  &:after {
-    content: "";
-    background: url(../../../assets/images/promotion/receive-earn/promo-subtitle-icon.png);
-    width: 25px;
-    height: 9px;
-    display: block;
-    background-size: 100% 100%;
-    transform: scaleX(-1);
-    margin-left: 8px;
-    margin-top: 4px;
-  }
-}
-
 .tnc-content {
   ol li {
     margin-bottom: 8px !important;
