8a8ce12213332cced298435320e1c705576968a9
Sam <sam@flex-df.com>
2025-07-16 14:58:01 +0800

    feat: [lk6 h5] Add weekly reward promo api

---
+++ lk6-static-h5/src/components/hotpromo/weekly-reward/WeeklyReward.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="livepoker-rebate-wrapper">
+    <div class="livepoker-rebate-container">
+      <div class="livepoker-rebate-section section-bg">
+        <div class="livepoker-rebate-section-left">
+          <div class="livepoker-rebate-section-title">
+            <div class="claim-title-icon"></div>
+            {{ $t("promo.weeklyReward.claimSection.title") }}
+          </div>
+          <div class="reward-info">
+            <div class="reward-info-icon claim-coin-icon"></div>
+            <div class="reward-info-content">
+              {{ $t("promo.weeklyReward.claimSection.yesterdayValidBets") }}
+              <span class="amount">{{ totalValidBet }}元</span>
+            </div>
+          </div>
+          <div class="reward-info">
+            <div class="reward-info-icon claim-gift-icon"></div>
+            <div class="reward-info-content">
+              {{ $t("promo.weeklyReward.claimSection.todayBonus") }}
+              <span class="amount">{{ bonus }}元</span>
+            </div>
+          </div>
+        </div>
+        <div class="livepoker-rebate-section-right">
+          <div class="bonus-image" @click="handleClaimBonus" :class="{ disabled: bonus <= 0, loading: loadingClaim }">
+            <img src="../../../assets/images/promotion/hotpromo/common/claim-btn3.png" alt="" width="100%" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, toRefs } from "vue";
+import { initSportWeeklyBonus, claimSportWeeklyBonus } from "../../../api/index/promo";
+import { useNotify } from "src/hooks/notify";
+import { userStore } from "src/stores";
+import { useI18n } from "vue-i18n";
+
+const props = defineProps(["promoCode"]);
+const { promoCode } = toRefs(props);
+
+const notify = useNotify();
+const store = userStore();
+const { t } = useI18n();
+
+const totalValidBet = ref(0);
+const bonus = ref(0);
+const loadingClaim = ref(false);
+
+const handleClaimBonus = () => {
+  loadingClaim.value = true;
+  claimSportWeeklyBonus(promoCode.value)
+    .then((res) => {
+      if (res.code === 0) {
+        notify({
+          type: "success",
+          message: t("common.notification.claimSuccessfully.message")
+        });
+        store.getBalance();
+      } else {
+        notify({
+          type: "error",
+          message: res.message
+        });
+      }
+    })
+    .catch(() => {})
+    .finally(() => {
+      loadingClaim.value = false;
+    });
+};
+
+const fetchData = async () => {
+  loadingClaim.value = true;
+  initSportWeeklyBonus(promoCode.value)
+    .then((res) => {
+      if (res.code === 0) {
+        totalValidBet.value = res.data.totalValidBet;
+        bonus.value = res.data.bonus;
+      }
+    })
+    .catch(() => {})
+    .finally(() => {
+      loadingClaim.value = false;
+    });
+};
+
+onMounted(() => {
+  if (!store.token) {
+    return;
+  }
+  fetchData();
+});
+</script>
+
+<style scoped lang="scss">
+.livepoker-rebate-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.livepoker-rebate-container {
+  width: 100%;
+  height: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.livepoker-rebate-section {
+  box-shadow: 0px 0px 4px 0px #01497b0f;
+  padding: 20px 12px 40px;
+  border-radius: 12px;
+  border: 1px solid #acd4f6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: url("../../../assets/images/promotion/hotpromo/common/section-bg.png");
+  background-size: 100% 100%;
+  align-items: center;
+  width: 100%;
+
+  .livepoker-rebate-section-left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .livepoker-rebate-section-right {
+    width: 180px;
+    margin-top: 20px;
+
+    .bonus-image {
+      width: 100%;
+      cursor: pointer;
+
+      &:active {
+        filter: brightness(0.85);
+        transform: translate(0px, 1px);
+      }
+
+      &.disabled {
+        filter: grayscale(100%);
+        cursor: not-allowed;
+        pointer-events: none;
+      }
+
+      &.loading {
+        cursor: not-allowed;
+        opacity: 0.8;
+      }
+    }
+  }
+
+  .livepoker-rebate-section-title {
+    color: #000000;
+    font-size: 16px;
+    line-height: 1;
+    font-weight: 600;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+}
+
+.livepoker-rebate-game {
+  width: 100%;
+  height: 302px;
+  border-radius: 12px;
+  // border: 1px solid #51acff;
+  background-color: #fff;
+  position: relative;
+  margin-bottom: 12px;
+}
+
+.livepoker-rebate-game-bottom {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .livepoker-rebate-game-bottom-left-title {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 22.4px;
+    color: #ff5d5d !important;
+  }
+  .livepoker-rebate-game-bottom-left-btn {
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 22.4px;
+    color: #ff5d5d !important;
+    cursor: pointer;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+  }
+}
+
+.reward-info {
+  border: 1px solid rgba(215, 235, 255, 1);
+  padding: 8px;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+}
+
+.reward-info-icon {
+  width: 16px;
+  height: 16px;
+  margin-right: 10px;
+}
+
+.reward-info-content {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  color: black;
+
+  .amount {
+    color: #00a1ff;
+    font-weight: 600;
+  }
+}
+</style>
