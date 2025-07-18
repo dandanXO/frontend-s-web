c4622ad20f700da11e108ab7b08851aa703b6c72
zhixuan <zhixuan@preec.net>
2025-07-18 15:22:16 +0800

    update: [tf-manager] add lk6

--- tf-manager-web/src/main.js
+++ tf-manager-web/src/main.js
@@ -1,262 +1,289 @@
-import { createApp } from "vue";
-import vueI18n from "./i18n";
-import App from "./App.vue";
-import router from "./router";
-import { store } from "./store";
-import "@/assets/iconfont/iconfont.css";
-import "@/styles/index.scss";
+import { createApp } from 'vue'
+import vueI18n from './i18n'
+import App from './App.vue'
+import router from './router'
+import { store } from './store'
+import '@/assets/iconfont/iconfont.css'
+import '@/styles/index.scss'
 import 'element-plus/theme-chalk/index.css'
-import "normalize.css";
-import * as directives from "@/directives";
-import "@/permission";
-import "@/icons";
-import SvgIcon from "@/components/SvgIcon/index.vue";
+import 'normalize.css'
+import * as directives from '@/directives'
+import '@/permission'
+import '@/icons'
+import SvgIcon from '@/components/SvgIcon/index.vue'
 import ElementPlus from 'element-plus'
-import { Socket } from "./utils/socket";
-import { createPinia } from "pinia";
-import dayjs from 'dayjs';
-import axios from "axios";
+import { Socket } from './utils/socket'
+import { createPinia } from 'pinia'
+import dayjs from 'dayjs'
+import axios from 'axios'
 
-dayjs.Ls.en.weekStart = 1;
-const app = createApp(App);
+dayjs.Ls.en.weekStart = 1
+const app = createApp(App)
 app.use(ElementPlus)
 
 var currentHost = window.location.host
-let baseApi;
-let baseWss;
+let baseApi
+let baseWss
 
-let siteId;
+let siteId
 
-var imageCDNUrl = process.env.VUE_APP_IMAGE;
+var imageCDNUrl = process.env.VUE_APP_IMAGE
 
-console.log(currentHost);
+console.log(currentHost)
 // currentHost = "ka1-manager.wermop342ox.com";
 switch (currentHost) {
   case process.env.VUE_APP_IW2_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'iw2';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'iw2'
+    break
   case process.env.VUE_APP_IND_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'ind';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'ind'
+    break
   case process.env.VUE_APP_LH_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'lh';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'lh'
+    break
   case process.env.VUE_APP_TH_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'th';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'th'
+    break
   case process.env.VUE_APP_DY_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'dy';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'dy'
+    break
   case process.env.VUE_APP_VNM_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'vnm';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'vnm'
+    break
   case process.env.VUE_APP_KAKA_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'ka1';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'ka1'
+    break
   case process.env.VUE_APP_KRW_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'krw';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'krw'
+    break
   case process.env.VUE_APP_PAK_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'pak';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'pak'
+    break
   case process.env.VUE_APP_PK2_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'pk2';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'pk2'
+    break
   case process.env.VUE_APP_BGD_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'bgd';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'bgd'
+    break
   case process.env.VUE_APP_NGA_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'nga';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'nga'
+    break
   case process.env.VUE_APP_PH1_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'ph1';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'ph1'
+    break
   case process.env.VUE_APP_TH2_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'th2';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'th2'
+    break
   case process.env.VUE_APP_PK3_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'pk3';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'pk3'
+    break
   case process.env.VUE_APP_AU1_HOST:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'au1';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'au1'
+    break
+  case process.env.VUE_APP_LK6_HOST:
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'lk6'
+    break
   case process.env.VUE_APP_BR1_HOST:
-    baseApi = process.env.VUE_APP_BR1_API;
-    baseWss = process.env.VUE_APP_BR1_SOCKET;
-    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE;
-    siteId = 'br1';
-    break;
+    baseApi = process.env.VUE_APP_BR1_API
+    baseWss = process.env.VUE_APP_BR1_SOCKET
+    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE
+    siteId = 'br1'
+    break
   case process.env.VUE_APP_BR2_HOST:
-    baseApi = process.env.VUE_APP_BR1_API;
-    baseWss = process.env.VUE_APP_BR1_SOCKET;
-    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE;
-    siteId = 'br2';
-    break;
+    baseApi = process.env.VUE_APP_BR1_API
+    baseWss = process.env.VUE_APP_BR1_SOCKET
+    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE
+    siteId = 'br2'
+    break
   case process.env.VUE_APP_MX1_HOST:
-    baseApi = process.env.VUE_APP_BR1_API;
-    baseWss = process.env.VUE_APP_BR1_SOCKET;
-    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE;
-    siteId = 'mx1';
-    break;
+    baseApi = process.env.VUE_APP_BR1_API
+    baseWss = process.env.VUE_APP_BR1_SOCKET
+    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE
+    siteId = 'mx1'
+    break
   case process.env.VUE_APP_CO1_HOST:
-    baseApi = process.env.VUE_APP_BR1_API;
-    baseWss = process.env.VUE_APP_BR1_SOCKET;
-    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE;
-    siteId = 'co1';
-    break;
+    baseApi = process.env.VUE_APP_BR1_API
+    baseWss = process.env.VUE_APP_BR1_SOCKET
+    imageCDNUrl = process.env.VUE_APP_BR1_IMAGE
+    siteId = 'co1'
+    break
   default:
-    baseApi = process.env.VUE_APP_BASE_API;
-    baseWss = process.env.VUE_APP_SOCKET;
-    siteId = 'xf';
-    break;
+    baseApi = process.env.VUE_APP_BASE_API
+    baseWss = process.env.VUE_APP_SOCKET
+    siteId = 'xf'
+    break
 }
 
 if (currentHost.indexOf(process.env.VUE_APP_KYQ_HOST) > -1) {
-  console.log("KYQ")
-  baseApi = process.env.VUE_APP_KYQ_API;
-  baseWss = process.env.VUE_APP_KYQ_SOCKET;
-  imageCDNUrl = process.env.VUE_APP_KYQ_IMAGE;
-  siteId = 'lh';
+  console.log('KYQ')
+  baseApi = process.env.VUE_APP_KYQ_API
+  baseWss = process.env.VUE_APP_KYQ_SOCKET
+  imageCDNUrl = process.env.VUE_APP_KYQ_IMAGE
+  siteId = 'lh'
 }
 
 if (currentHost.indexOf(process.env.VUE_APP_BFY3_HOST) > -1) {
-  console.log("BFY3")
-  baseApi = process.env.VUE_APP_BASE_API;
-  baseWss = process.env.VUE_APP_SOCKET;
-  imageCDNUrl = process.env.VUE_APP_BFY3_IMAGE;
-  siteId = 'lh';
+  console.log('BFY3')
+  baseApi = process.env.VUE_APP_BASE_API
+  baseWss = process.env.VUE_APP_SOCKET
+  imageCDNUrl = process.env.VUE_APP_BFY3_IMAGE
+  siteId = 'lh'
 }
 
-sessionStorage.setItem("IMAGE_CDN", imageCDNUrl);
+sessionStorage.setItem('IMAGE_CDN', imageCDNUrl)
 
-var hasUrl = false;
-if (baseApi.indexOf(",") > -1) {
-  const urls = baseApi.split(",");
-  hasUrl = false;
+var hasUrl = false
+if (baseApi.indexOf(',') > -1) {
+  const urls = baseApi.split(',')
+  hasUrl = false
   urls.forEach((url, ind) => {
-    axios.get(url + "/ping").then((res) => {
-      // console.log(res);
-      if (res.status !== 200) {
-      } else {
-        putBaseUrl(url, ind);
-      }
-    }).catch((err) => {
-      console.log(err);
-    })
-  });
+    axios
+      .get(url + '/ping')
+      .then(res => {
+        // console.log(res);
+        if (res.status !== 200) {
+        } else {
+          putBaseUrl(url, ind)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 } else {
-  hasUrl = true;
-  app.config.globalProperties.$baseApi = baseApi;
-  app.config.globalProperties.$baseWss = baseWss;
+  hasUrl = true
+  app.config.globalProperties.$baseApi = baseApi
+  app.config.globalProperties.$baseWss = baseWss
 
-  sessionStorage.setItem("baseApi", baseApi);
+  sessionStorage.setItem('baseApi', baseApi)
 }
 
-var link = document.querySelector("link[rel~='icon']");
-var title = document.querySelector("title");
+var link = document.querySelector("link[rel~='icon']")
+var title = document.querySelector('title')
 if (!link) {
-  link = document.createElement('link');
-  link.rel = 'icon';
-  document.head.appendChild(link);
+  link = document.createElement('link')
+  link.rel = 'icon'
+  document.head.appendChild(link)
 }
 if (!title) {
-  title = document.createElement('title');
-  document.head.appendChild(title);
+  title = document.createElement('title')
+  document.head.appendChild(title)
 }
 
 if (siteId === 'dy') {
-  link.href = '/dy-favicon.ico';
-  title.innerText = '东赢后台';
+  link.href = '/dy-favicon.ico'
+  title.innerText = '东赢后台'
 } else if (siteId === 'xf') {
-  link.href = '/xf-favicon.ico';
-  title.innerText = '兴发后台';
+  link.href = '/xf-favicon.ico'
+  title.innerText = '兴发后台'
 } else if (siteId === 'th') {
-  link.href = '/th-favicon.ico';
-  title.innerText = 'Jolly88';
-} else if (siteId === 'ind' || siteId === "au1" || siteId === 'nga' || siteId === 'ph1' || siteId === 'br1' || siteId === 'mx1' || siteId === 'co1' || siteId === 'pk3' || siteId === 'id1' || siteId === 'th2') {
-  link.href = '/ind-favicon.ico';
-  title.innerText = '55Ace';
+  link.href = '/th-favicon.ico'
+  title.innerText = 'Jolly88'
+} else if (
+  siteId === 'ind' ||
+  siteId === 'au1' ||
+  siteId === 'nga' ||
+  siteId === 'ph1' ||
+  siteId === 'br1' ||
+  siteId === 'mx1' ||
+  siteId === 'co1' ||
+  siteId === 'pk3' ||
+  siteId === 'id1' ||
+  siteId === 'th2'
+) {
+  link.href = '/ind-favicon.ico'
+  title.innerText = '55Ace'
 } else if (siteId === 'pak' || siteId === 'pk2' || siteId === 'bgd') {
-  link.href = '/pak-favicon.ico';
-  title.innerText = 'B9Game';
+  link.href = '/pak-favicon.ico'
+  title.innerText = 'B9Game'
 } else if (siteId === 'lh') {
-  link.href = '/favicon.ico';
-  title.innerText = '雷火后台';
+  link.href = '/favicon.ico'
+  title.innerText = '雷火后台'
 } else if (siteId === 'br2') {
-  link.href = '/akb-favicon.ico';
-  title.innerText = 'AKB188';
+  link.href = '/akb-favicon.ico'
+  title.innerText = 'AKB188'
 } else if (siteId === 'vnm') {
-  link.href = '/favicon.ico';
-  title.innerText = 'TF88';
+  link.href = '/favicon.ico'
+  title.innerText = 'TF88'
 } else if (siteId === 'ka1') {
-  link.href = '/kaka-favicon.ico';
-  title.innerText = 'KAKA GAME';
+  link.href = '/kaka-favicon.ico'
+  title.innerText = 'KAKA GAME'
 } else if (siteId === 'krw') {
-  link.href = '/kr-favicon.ico';
-  title.innerText = 'DIAMOND';
+  link.href = '/kr-favicon.ico'
+  title.innerText = 'DIAMOND'
 } else if (siteId === 'iw2') {
-  link.href = '/iw2-favicon.ico';
-  title.innerText = '789F';
+  link.href = '/iw2-favicon.ico'
+  title.innerText = '789F'
+} else if (siteId === 'lk6') {
+  link.href = '/lk6-favicon.ico'
+  title.innerText = '幸运6'
 } else {
-  link.href = '/favicon.ico';
-  title.innerText = 'TF88';
+  link.href = '/favicon.ico'
+  title.innerText = 'TF88'
 }
 
 function putBaseUrl(url, index) {
   if (hasUrl === false) {
-    console.log(url);
-    const wsss = baseWss.split(",")
-    hasUrl = true;
+    console.log(url)
+    const wsss = baseWss.split(',')
+    hasUrl = true
 
-    sessionStorage.setItem("baseApi", url);
+    sessionStorage.setItem('baseApi', url)
 
-    app.config.globalProperties.$baseApi = url;
-    app.config.globalProperties.$baseWss = wsss[index];
+    app.config.globalProperties.$baseApi = url
+    app.config.globalProperties.$baseWss = wsss[index]
   }
 }
 
-app.component("SvgIcon", SvgIcon);
+app.component('SvgIcon', SvgIcon)
 
 Object.keys(directives).forEach(key => {
-  app.directive(key, directives[key]);
-});
+  app.directive(key, directives[key])
+})
 
-const socket = new Socket(store);
+const socket = new Socket(store)
 app.config.globalProperties.$socket = socket
 
-app.use(createPinia()).use(vueI18n).use(store).use(router).mount("#app");
+app
+  .use(createPinia())
+  .use(vueI18n)
+  .use(store)
+  .use(router)
+  .mount('#app')
 
-const globals = app.config.globalProperties;
+const globals = app.config.globalProperties
 export { globals }
--- tf-manager-web/src/utils/site.js
+++ tf-manager-web/src/utils/site.js
@@ -1,188 +1,206 @@
-const XF_SITE_ID = 1;
-const THAI_SITE_ID = 3;
-const IND_SITE_ID = 5;
-const DY_SITE_ID = 6;
-const LH_SITE_ID = 7;
-const VNM_SITE_ID = 8;
-const IND2_SITE_ID = 9;
-const KRW_SITE_ID = 10;
-const PAK_SITE_ID = 11;
-const BGD_SITE_ID = 12;
-const NGA_SITE_ID = 14;
-const KAKA_SITE_ID = 15;
-const PH1_SITE_ID = 16;
-const ID1_SITE_ID = 17;
-const BR1_SITE_ID = 18;
-const PAK2_SITE_ID = 19;
-const PK3_SITE_ID = 20;
-const MX1_SITE_ID = 21;
-const CO1_SITE_ID = 22;
-const TH2_SITE_ID = 23;
-const BPK_SITE_ID = 24;
-const PK4_SITE_ID = 26;
-const AU1_SITE_ID = 27;
-const BR2_SITE_ID = 28;
+const XF_SITE_ID = 1
+const THAI_SITE_ID = 3
+const IND_SITE_ID = 5
+const DY_SITE_ID = 6
+const LH_SITE_ID = 7
+const VNM_SITE_ID = 8
+const IND2_SITE_ID = 9
+const KRW_SITE_ID = 10
+const PAK_SITE_ID = 11
+const BGD_SITE_ID = 12
+const NGA_SITE_ID = 14
+const KAKA_SITE_ID = 15
+const PH1_SITE_ID = 16
+const ID1_SITE_ID = 17
+const BR1_SITE_ID = 18
+const PAK2_SITE_ID = 19
+const PK3_SITE_ID = 20
+const MX1_SITE_ID = 21
+const CO1_SITE_ID = 22
+const TH2_SITE_ID = 23
+const BPK_SITE_ID = 24
+const PK4_SITE_ID = 26
+const AU1_SITE_ID = 27
+const BR2_SITE_ID = 28
+const LK6_SITE_ID = 30
 
-export const isIndiaSite = (siteId) => {
-  return parseInt(siteId) === IND_SITE_ID || parseInt(siteId) === IND2_SITE_ID;
+export const isIndiaSite = siteId => {
+  return parseInt(siteId) === IND_SITE_ID || parseInt(siteId) === IND2_SITE_ID
 }
 
-export const isCnySite = (siteId) => {
-  return parseInt(siteId) === XF_SITE_ID || parseInt(siteId) === DY_SITE_ID || parseInt(siteId) === LH_SITE_ID;
+export const isCnySite = siteId => {
+  return (
+    parseInt(siteId) === XF_SITE_ID ||
+    parseInt(siteId) === DY_SITE_ID ||
+    parseInt(siteId) === LH_SITE_ID
+  )
 }
 
-export const isVnmSite = (siteId) => {
-  return parseInt(siteId) === VNM_SITE_ID || parseInt(siteId) === KAKA_SITE_ID;
+export const isVnmSite = siteId => {
+  return parseInt(siteId) === VNM_SITE_ID || parseInt(siteId) === KAKA_SITE_ID
 }
 
 export function isInd(siteId) {
-  return parseInt(siteId) === IND_SITE_ID;
+  return parseInt(siteId) === IND_SITE_ID
 }
 
-export const isInd2 = (siteId) => {
-  return parseInt(siteId) === IND2_SITE_ID;
+export const isInd2 = siteId => {
+  return parseInt(siteId) === IND2_SITE_ID
 }
 
-export const isPak = (siteId) => {
-  return parseInt(siteId) === PAK_SITE_ID || parseInt(siteId) === PAK2_SITE_ID || parseInt(siteId) === BGD_SITE_ID || parseInt(siteId) === BPK_SITE_ID || parseInt(siteId) === PK4_SITE_ID;
+export const isPak = siteId => {
+  return (
+    parseInt(siteId) === PAK_SITE_ID ||
+    parseInt(siteId) === PAK2_SITE_ID ||
+    parseInt(siteId) === BGD_SITE_ID ||
+    parseInt(siteId) === BPK_SITE_ID ||
+    parseInt(siteId) === PK4_SITE_ID
+  )
 }
 
-export const isXF = (siteId) => {
-  return parseInt(siteId) === XF_SITE_ID;
+export const isXF = siteId => {
+  return parseInt(siteId) === XF_SITE_ID
 }
 
-export const isDY = (siteId) => {
-  return parseInt(siteId) === DY_SITE_ID;
+export const isDY = siteId => {
+  return parseInt(siteId) === DY_SITE_ID
 }
 
-export const isLH = (siteId) => {
-  return parseInt(siteId) === LH_SITE_ID;
+export const isLH = siteId => {
+  return parseInt(siteId) === LH_SITE_ID
 }
 
-export const isThai = (siteId) => {
-  return parseInt(siteId) === THAI_SITE_ID;
+export const isThai = siteId => {
+  return parseInt(siteId) === THAI_SITE_ID
 }
 
-export const isKorea = (siteId) => {
-  return parseInt(siteId) === KRW_SITE_ID;
+export const isKorea = siteId => {
+  return parseInt(siteId) === KRW_SITE_ID
 }
 
-export const isVnm = (siteId) => {
-  return parseInt(siteId) === VNM_SITE_ID;
+export const isVnm = siteId => {
+  return parseInt(siteId) === VNM_SITE_ID
 }
 
-export const isBgd = (siteId) => {
-  return parseInt(siteId) === BGD_SITE_ID;
+export const isBgd = siteId => {
+  return parseInt(siteId) === BGD_SITE_ID
 }
 
-export const isNga = (siteId) => {
-  return parseInt(siteId) === NGA_SITE_ID;
+export const isNga = siteId => {
+  return parseInt(siteId) === NGA_SITE_ID
 }
 
-export const isKaka = (siteId) => {
-  return parseInt(siteId) === KAKA_SITE_ID;
+export const isKaka = siteId => {
+  return parseInt(siteId) === KAKA_SITE_ID
 }
 
-export const isPh1 = (siteId) => {
-  return parseInt(siteId) === PH1_SITE_ID;
+export const isPh1 = siteId => {
+  return parseInt(siteId) === PH1_SITE_ID
 }
 
-export const isAu1 = (siteId) => {
-  return parseInt(siteId) === AU1_SITE_ID;
+export const isAu1 = siteId => {
+  return parseInt(siteId) === AU1_SITE_ID
 }
 
-export const isBr1 = (siteId) => {
-  return parseInt(siteId) === BR1_SITE_ID;
+export const isLk6 = siteId => {
+  return parseInt(siteId) === LK6_SITE_ID
 }
 
-export const isBr2 = (siteId) => {
-  return parseInt(siteId) === BR2_SITE_ID;
+export const isBr1 = siteId => {
+  return parseInt(siteId) === BR1_SITE_ID
 }
 
-export const isMx1 = (siteId) => {
-  return parseInt(siteId) === MX1_SITE_ID;
+export const isBr2 = siteId => {
+  return parseInt(siteId) === BR2_SITE_ID
 }
 
-export const isId1 = (siteId) => {
-  return parseInt(siteId) === ID1_SITE_ID;
+export const isMx1 = siteId => {
+  return parseInt(siteId) === MX1_SITE_ID
 }
 
-export const isPK4 = (siteId) => {
-  return parseInt(siteId) === PK4_SITE_ID;
+export const isId1 = siteId => {
+  return parseInt(siteId) === ID1_SITE_ID
 }
 
-export const getSiteTitle = (siteId) => {
-  let siteTitle;
+export const isPK4 = siteId => {
+  return parseInt(siteId) === PK4_SITE_ID
+}
+
+export const getSiteTitle = siteId => {
+  let siteTitle
   switch (parseInt(siteId)) {
-    case XF_SITE_ID :
-      siteTitle = "兴发后台 "
-      break;
-    case THAI_SITE_ID :
-      siteTitle = "Jolly88 "
-      break;
-    case IND_SITE_ID :
-      siteTitle = "55Ace "
-      break;
-    case DY_SITE_ID :
-      siteTitle = "东赢后台 "
-      break;
-    case LH_SITE_ID :
-      siteTitle = "雷火后台 "
-      break;
-    case VNM_SITE_ID :
-      siteTitle = "TF88 "
-      break;
-    case IND2_SITE_ID :
-      siteTitle = "789F "
-      break;
-    case KRW_SITE_ID :
-      siteTitle = "DIAMOND "
-      break;
-    case PAK_SITE_ID :
-      siteTitle = "B9.GAME "
-      break;
-    case BGD_SITE_ID :
-      siteTitle = "WIN7.GAME "
-      break;
-    case NGA_SITE_ID :
-      siteTitle = "Nigeria "
-      break;
-    case KAKA_SITE_ID :
-      siteTitle = "KAKA GAME "
-      break;
-    case PH1_SITE_ID :
-      siteTitle = "Philippines "
-      break;
-    case AU1_SITE_ID :
-      siteTitle = "Australia "
-      break;
-    case ID1_SITE_ID :
-      siteTitle = "Indonesia "
-      break;
-    case BR1_SITE_ID :
-      siteTitle = "Brazil "
-      break;
-    case BR2_SITE_ID :
-      siteTitle = "AKB188 "
-      break;
-    case MX1_SITE_ID :
-      siteTitle = "Mexico "
-      break;
-    case PAK2_SITE_ID :
-      siteTitle = "97PKR "
-      break;
-    case PK3_SITE_ID :
-      siteTitle = "Pakistan "
-      break;
-    case CO1_SITE_ID :
-      siteTitle = "Columbia "
-      break;
-    case TH2_SITE_ID :
-      siteTitle = "Thailand "
-      break;
+    case XF_SITE_ID:
+      siteTitle = '兴发后台 '
+      break
+    case THAI_SITE_ID:
+      siteTitle = 'Jolly88 '
+      break
+    case IND_SITE_ID:
+      siteTitle = '55Ace '
+      break
+    case DY_SITE_ID:
+      siteTitle = '东赢后台 '
+      break
+    case LH_SITE_ID:
+      siteTitle = '雷火后台 '
+      break
+    case VNM_SITE_ID:
+      siteTitle = 'TF88 '
+      break
+    case IND2_SITE_ID:
+      siteTitle = '789F '
+      break
+    case KRW_SITE_ID:
+      siteTitle = 'DIAMOND '
+      break
+    case PAK_SITE_ID:
+      siteTitle = 'B9.GAME '
+      break
+    case BGD_SITE_ID:
+      siteTitle = 'WIN7.GAME '
+      break
+    case NGA_SITE_ID:
+      siteTitle = 'Nigeria '
+      break
+    case KAKA_SITE_ID:
+      siteTitle = 'KAKA GAME '
+      break
+    case PH1_SITE_ID:
+      siteTitle = 'Philippines '
+      break
+    case AU1_SITE_ID:
+      siteTitle = 'Australia '
+      break
+    case LK6_SITE_ID:
+      siteTitle = '幸运6 '
+      break
+    case ID1_SITE_ID:
+      siteTitle = 'Indonesia '
+      break
+    case BR1_SITE_ID:
+      siteTitle = 'Brazil '
+      break
+    case BR2_SITE_ID:
+      siteTitle = 'AKB188 '
+      break
+    case MX1_SITE_ID:
+      siteTitle = 'Mexico '
+      break
+    case PAK2_SITE_ID:
+      siteTitle = '97PKR '
+      break
+    case PK3_SITE_ID:
+      siteTitle = 'Pakistan '
+      break
+    case CO1_SITE_ID:
+      siteTitle = 'Columbia '
+      break
+    case TH2_SITE_ID:
+      siteTitle = 'Thailand '
+      break
     default:
-      siteTitle = "后台 "
-      break;
+      siteTitle = '后台 '
+      break
   }
 
   return siteTitle
