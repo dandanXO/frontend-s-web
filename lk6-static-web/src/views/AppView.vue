26800e6414862fc8ee9d21f56bf1d9952588e7af
Kai Wen <kaiwen@caspen-tech.com>
2025-07-18 17:54:33 +0800

    fix: [lk6 web] Add new App route and view

---
+++ lk6-static-web/src/views/AppView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app-download-section">
+        <HomeDownload></HomeDownload>
+    </div>
+</template>
+
+<script setup>
+import HomeDownload from "@/components/home/download/index.vue";
+</script>
+
+<style scoped lang="scss">
+.app-download-section {
+    background-image: url("../assets/images/home/main-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    background-position: top center;
+    padding: 50px;
+    width: 100%;
+}
+</style>
