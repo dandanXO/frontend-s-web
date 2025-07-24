ddc4ee2d3dd96c5bdecde8a6ac35940bd251a4d5
WeiSeng <weiseng@eagrp.net>
2025-07-24 11:37:03 +0800

    feat: TH3 change CW appkey and body id

--- th3-static-v1/src/boot/axios.js
+++ th3-static-v1/src/boot/axios.js
@@ -13,9 +13,9 @@ const rstArray = Object.values(process.env.RST_API);
 const evtArray = Object.values(process.env.EVT_API);
 const crtArray = Object.values(process.env.CR_API);
 
-var rstApi = getInitApi(rstArray, "PAK_RST_URL");
-var crtApi = getInitApi(crtArray, "PAK_CRT_URL");
-var evtApi = getInitApi(evtArray, "PAK_EVT_URL");
+var rstApi = getInitApi(rstArray, "TH3_RST_URL");
+var crtApi = getInitApi(crtArray, "TH3_CRT_URL");
+var evtApi = getInitApi(evtArray, "TH3_EVT_URL");
 
 const api = axios.create({ baseURL: rstApi });
 const cashier = axios.create({ baseURL: crtApi });
