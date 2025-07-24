75332412b4aee990e18b31f1dc4bf51053bddc7b
WeiSeng <weiseng@eagrp.net>
2025-07-24 10:10:41 +0800

    feat: TH3 change CW appKey

--- th3-static-v1/src-capacitor/android/app/src/main/java/com/ind/appb9mega1/MyApplication.java
+++ th3-static-v1/src-capacitor/android/app/src/main/java/com/ind/appb9mega1/MyApplication.java
@@ -7,7 +7,7 @@ import com.cloudwise.agent.app.CWSDK;
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
-        CWSDK.withAppKey("wS0n2SF8WRCb0fAkjFLvksRizrsrej3Y4XuCglHI0M9oZME**mvyF3eC**5bskBqMp", "https://apm-int.cloudwise.com")
+        CWSDK.withAppKey("wS0n2SF8WRCb0fAkjFLvksRizrsrej3YxXOHVsimnu5PgJj4LvvnJBLIRAOEGsg2", "https://apm-int.cloudwise.com")
                 .enableLogging(true) //打开logcat日志开关
                 .start(getApplicationContext());
         super.onCreate();
